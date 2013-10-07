a0435d7f7e02855ded36e8adc59b80fe1968673c
James Laska <jlaska@ansibleworks.com>
2013-10-07 12:42:41 -0400

    Minor crawler changes

--- tests/api/test_crawler.py
+++ tests/api/test_crawler.py
@@ -24,14 +24,14 @@ def assert_response(link, method, response_code=httplib.OK, response_schema='una
         r = func_method(link, data)
 
     # Assert api response code matches expected
-    Assert.equal(r.code, response_code)
+    assert r.code == response_code
 
     # validate api json response matches expected
     if response_schema is not None:
         validate(r.json(), link[7:-1], response_schema)
 
 @pytest.mark.nondestructive
-def test_link_generator():
+def test_crawl_unauthorized():
     global api # yuck
 
     r = api.get('/api/')
@@ -39,13 +39,13 @@ def test_link_generator():
     current_version = data.get('current_version')
     r = api.get(current_version)
     for key, link in r.json().items():
-        for method in ['GET', 'PUT', 'POST', 'HEAD', 'OPTIONS']:
+        for method in ['GET', 'PUT', 'POST', 'PATCH', 'HEAD', 'OPTIONS']:
             expected_response_code = httplib.UNAUTHORIZED
             expected_response_schema = 'unauthorized'
 
             # authtoken only supports POST, OPTIONS
             if key == 'authtoken':
-                if method in ['HEAD', 'GET', 'PUT']:
+                if method in ['HEAD', 'GET', 'PUT', 'PATCH']:
                     expected_response_code = httplib.METHOD_NOT_ALLOWED
                 elif method in ['OPTIONS']:
                     expected_response_code = httplib.OK
@@ -53,8 +53,6 @@ def test_link_generator():
                 elif method in ['POST']:
                     expected_response_code = httplib.BAD_REQUEST
                     expected_response_schema = 'bad_password'
-                else:
-                    expected_response_code = httplib.UNAUTHORIZED
 
             # No json response for 'HEAD'
             if method == 'HEAD':
