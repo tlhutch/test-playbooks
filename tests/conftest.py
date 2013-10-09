22a46a590291f91b7cfa4f9821cfa7a7c9efb0e2
James Laska <jlaska@ansibleworks.com>
2013-10-09 18:49:44 -0400

    Add pytest marker to support test sequences

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import pytest
 import plugins
 import fixtures
 import markers
@@ -14,3 +15,16 @@ def _pytest_plugins_generator(*extension_pkgs):
                 yield modname
 
 pytest_plugins = tuple(_pytest_plugins_generator(fixtures, markers, plugins))
+
+# Add support for test sequences
+# http://stackoverflow.com/questions/12411431/pytest-how-to-skip-the-rest-of-tests-in-the-class-if-one-has-failed/12579625#12579625
+def pytest_runtest_makereport(item, call):
+    if "incremental" in item.keywords:
+        if call.excinfo is not None:
+            parent = item.parent
+            parent._previousfailed = item
+
+def pytest_runtest_setup(item):
+    previousfailed = getattr(item.parent, "_previousfailed", None)
+    if previousfailed is not None:
+        pytest.xfail("previous test failed (%s)" %previousfailed.name)
