22726351260b1e99faa49c1901f721730f49f5e0
James Laska <jlaska@ansibleworks.com>
2013-10-14 16:49:16 -0400

    Remove remaining selenium tendrils

--- tests/lib/plugins/restqa.py
+++ tests/lib/plugins/restqa.py
@@ -1,4 +1,5 @@
 import py
+import pytest
 import re
 import yaml
 import json
@@ -63,6 +64,16 @@ def pytest_sessionstart(session):
             (httplib.OK, session.config.option.base_url, r.status_code)
 
 
+def split_class_and_test_names(nodeid):
+    names = nodeid.split("::")
+    names[0] = names[0].replace("/", '.')
+    names = [x.replace(".py", "") for x in names if x != "()"]
+    classnames = names[:-1]
+    classname = ".".join(classnames)
+    name = names[-1]
+    return (classname, name)
+
+
 def pytest_runtest_setup(item):
     item.debug = {
         'urls': [],
@@ -76,14 +87,9 @@ def pytest_runtest_setup(item):
 
     test_id = '.'.join(split_class_and_test_names(item.nodeid))
 
-    TestSetup.selenium = None
 
-def pytest_runtest_teardown(item):
-    if hasattr(TestSetup, 'selenium') and TestSetup.selenium and 'skip_selenium' not in item.keywords:
-        TestSetup.selenium_client.stop()
-
-
-def pytest_funcarg__testsetup(request):
+@pytest.fixture(scope="session")
+def testsetup(request):
     '''
     Return initialized REST QA TestSetup object
     '''
@@ -93,58 +99,68 @@ def pytest_funcarg__testsetup(request):
 def pytest_addoption(parser):
     group = parser.getgroup('rest', 'rest')
     group._addoption('--baseurl',
-                     action='store',
-                     dest='base_url',
-                     default=None,
-                     metavar='url',
-                     help='base url for the application under test.')
+        action='store',
+        dest='base_url',
+        default=None,
+        metavar='url',
+        help='base url for the application under test.')
     group._addoption('--api-version',
-                     action='store',
-                     dest='api_version',
-                     default='current_version',
-                     metavar='API-VERSION',
-                     help='Choose the API version')
+        action='store',
+        dest='api_version',
+        default='current_version',
+        metavar='API-VERSION',
+        help='Choose the API version')
     group._addoption('--build',
-                     action='store',
-                     dest='build',
-                     metavar='str',
-                     help='build identifier (for continuous integration).')
+        action='store',
+        dest='build',
+        metavar='str',
+        help='build identifier (for continuous integration).')
     group._addoption('--untrusted',
-                     action='store_true',
-                     dest='assume_untrusted',
-                     default=False,
-                     help='assume that all certificate issuers are untrusted. (default: %default)')
+        action='store_true',
+        dest='assume_untrusted',
+        default=False,
+        help='assume that all certificate issuers are untrusted. (default: %default)')
+    # FIXME - make this work (refer to lib/common/api.py)
+    group.addoption('--debug-rest',
+        action="store_true",
+        dest="debug_rest",
+        default=False,
+        help="record REST API calls 'pytest-rest.log'.")
 
     group = parser.getgroup('safety', 'safety')
     group._addoption('--destructive',
-                     action='store_true',
-                     dest='run_destructive',
-                     default=False,
-                     help='include destructive tests (tests not explicitly marked as \'nondestructive\'). (default: %default)')
+        action='store_true',
+        dest='run_destructive',
+        default=False,
+        help='include destructive tests (tests not explicitly marked as \'nondestructive\'). (default: %default)')
 
     group = parser.getgroup('credentials', 'credentials')
     group._addoption("--credentials",
-                     action="store",
-                     dest='credentials_file',
-                     metavar='path',
-                     help="location of yaml file containing user credentials.")
+        action="store",
+        dest='credentials_file',
+        metavar='path',
+        help="location of yaml file containing user credentials.")
 
 
-def split_class_and_test_names(nodeid):
-    names = nodeid.split("::")
-    names[0] = names[0].replace("/", '.')
-    names = [x.replace(".py", "") for x in names if x != "()"]
-    classnames = names[:-1]
-    classname = ".".join(classnames)
-    name = names[-1]
-    return (classname, name)
+def pytest_cmdline_parse(__multicall__):
+    config = __multicall__.execute()
+    if config.option.debug_rest:
+        path = os.path.abspath("pytest-rest.log")
+        f = open(path, 'w')
+        config._debug_rest_file = f
+        f.write("versions pytest-%s, py-%s, python-%s\ncwd=%s\nargs=%s\n\n" %(
+            pytest.__version__, py.__version__, ".".join(map(str, sys.version_info)),
+            os.getcwd(), config._origargs))
+        sys.stderr.write("writing pytest-rest information to %s\n" % path)
+    return config
 
 
-def _debug_summary(debug):
-    summary = []
-    if debug['urls']:
-        summary.append('Failing URL: %s' % debug['urls'][-1])
-    return '\n'.join(summary)
+@pytest.mark.trylast
+def pytest_unconfigure(config):
+    if hasattr(config, '_debug_rest_file'):
+        config._debug_rest_file.close()
+        sys.stderr.write("wrote pytest-rest information to %s\n" %
+            config._debug_rest_file.name)
 
 
 class TestSetup:
