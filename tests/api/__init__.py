8eef81c16bf5b594dfc7f28c105c3966d020f903
James Laska <jlaska@ansibleworks.com>
2013-10-28 14:45:57 -0400

    Disable selenium tests for all API derived tests

--- tests/api/__init__.py
+++ tests/api/__init__.py
@@ -1,5 +1,6 @@
 import pytest
 
+@pytest.mark.skip_selenium
 class Base_Api_Test(object):
     '''
     Base class
@@ -9,7 +10,9 @@ class Base_Api_Test(object):
         """ setup any state specific to the execution of the given class (which
         usually contains tests).
         """
-        self.testsetup = pytest.config.pluginmanager.getplugin("plugins.restqa").TestSetup
+        plugin = pytest.config.pluginmanager.getplugin("plugins.pytest_restqa.pytest_restqa")
+        assert plugin, 'Unable to find pytest_restqa plugin'
+        self.testsetup = plugin.TestSetup
 
     @classmethod
     def teardown_class(self):
