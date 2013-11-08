d71490f11f4398d7bc0a93f3c81631b0d8bf34c7
James Laska <jlaska@ansibleworks.com>
2013-11-08 15:39:45 -0500

    Add credentials and api @properties for convenience
    Also add has_credentials() conventience method to assert whether desired
    credentials are present in self.testsetup.credentials

--- tests/api/__init__.py
+++ tests/api/__init__.py
@@ -14,8 +14,44 @@ class Base_Api_Test(object):
         assert plugin, 'Unable to find pytest_restqa plugin'
         self.testsetup = plugin.TestSetup
 
+    @property
+    def credentials(self):
+        '''convenient access to credentials'''
+        return self.testsetup.credentials
+
+    @property
+    def api(self):
+        '''convenient access to api'''
+        return self.testsetup.api
+
     @classmethod
     def teardown_class(self):
         '''
         Perform any required test teardown
         '''
+
+    def has_credentials(self, ctype, sub_ctype=None, fields=[]):
+        '''
+        assert whether requested credentials are present
+        '''
+
+        # Make sure credentials.yaml has ctype we need
+        assert ctype in self.testsetup.credentials, \
+            "No '%s' credentials defined in credentals.yaml" % ctype
+        creds = self.testsetup.credentials[ctype]
+
+        # Ensure requested sub-type is present
+        if sub_ctype:
+            assert sub_ctype in creds, \
+                "No '%s' '%s' credentials defined in credentals.yaml" % \
+                (ctype, sub_ctype)
+            creds = creds[sub_ctype]
+
+        # Ensure requested fields are present
+        if fields:
+            assert all([field in creds for field in fields]), \
+                "Not all requested credential fields for section '%s' defined credentials.yaml" % \
+                ctype
+
+        return True
+
