a6f64faebc354f11816a6bd72f1661fb694fff6b
James Laska <jlaska@ansibleworks.com>
2013-11-08 15:50:45 -0500

    Add custom response exceptions

---
+++ tests/lib/common/exceptions.py
@@ -0,0 +1,11 @@
+class BadRequest_Exception(Exception):
+    pass
+
+class Duplicate_Exception(Exception):
+    pass
+
+class NoContent_Exception(Exception):
+    pass
+
+class Unknown_Exception(Exception):
+    pass
