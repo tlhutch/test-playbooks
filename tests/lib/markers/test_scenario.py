622305a85d3a65f0494b3284eba3ca40e6f6ba19
James Laska <jlaska@ansibleworks.com>
2013-11-13 10:17:08 -0500

    Only xfail scenario on destructive tests

--- tests/lib/markers/test_scenario.py
+++ tests/lib/markers/test_scenario.py
@@ -3,19 +3,23 @@ import pytest
 
 '''
 Automatically xfail remaining tests if previous test failed.  Tests will
-continue to run if a result is skip or xfail.
+continue to run if:
+ * the result is skip
+ * the result is xfail
+ * the test is marked as nondestructive
 
 For more information, refer to
 http://stackoverflow.com/questions/12411431/pytest-how-to-skip-the-rest-of-tests-in-the-class-if-one-has-failed/12579625#12579625
 '''
 
 def pytest_runtest_makereport(item, call):
-    if "incremental" in item.keywords:
-        if call.excinfo is not None \
-           and not any([call.excinfo.errisinstance(py.test.xfail.Exception),
-                       call.excinfo.errisinstance(py.test.skip.Exception)]):
-            parent = item.parent
-            parent._previousfailed = item
+    if "incremental" in item.keywords \
+       and "destructive" in item.keywords \
+       and call.excinfo is not None \
+       and not any([call.excinfo.errisinstance(py.test.xfail.Exception),
+                    call.excinfo.errisinstance(py.test.skip.Exception)]):
+        parent = item.parent
+        parent._previousfailed = item
 
 def pytest_runtest_setup(item):
     previousfailed = getattr(item.parent, "_previousfailed", None)
