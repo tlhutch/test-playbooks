50b6a546329e383135a50d23b133c18023490a78
James Laska <jlaska@ansibleworks.com>
2014-01-08 16:33:05 -0500

    Add authorized API call tests
    Only GET requests at the moment

--- tests/api/test_crawler.py
+++ tests/api/test_crawler.py
@@ -6,16 +6,14 @@ from plugins.pytest_restqa.rest_client import Connection
 
 api = None
 
+# def setup_module(module):
 def setup_function(function):
     global api # yuck
-
     api = Connection(pytest.config.option.base_url)
 
 @pytest.mark.skip_selenium
 @pytest.mark.nondestructive
-def assert_response(link, method, response_code=httplib.OK, response_schema='unauthorized', data={}):
-    global api # yuck
-
+def assert_response(api, link, method, response_code=httplib.OK, response_schema='unauthorized', data={}):
     # Call requested api method
     method = method.lower()
     func_method = getattr(api, method)
@@ -31,10 +29,13 @@ def assert_response(link, method, response_code=httplib.OK, response_schema='una
     if response_schema is not None:
         validate(r.json(), link[7:-1], response_schema)
 
-@pytest.mark.skip_selenium
 @pytest.mark.nondestructive
+@pytest.mark.skip_selenium
 def test_crawl_unauthorized():
     global api # yuck
+
+    # Clear out any authentication credentials
+    api.auth = None
 
     r = api.get('/api/')
     data = r.json()
@@ -61,4 +62,41 @@ def test_crawl_unauthorized():
                 expected_response_schema = None
 
             # assert!
-            yield "%s:%s" % (method, link), assert_response, link, method, expected_response_code, expected_response_schema
+            yield "%s:%s" % (method, link), assert_response, api, link, method, expected_response_code, expected_response_schema
+
+@pytest.mark.nondestructive
+@pytest.mark.skip_selenium
+@pytest.mark.usefixtures("authtoken")
+def test_crawl_authorized():
+    global api # yuck
+
+    # Login
+    api.login('admin', 'fo0m4nchU')
+
+    r = api.get('/api/')
+    data = r.json()
+    current_version = data.get('current_version')
+    r = api.get(current_version)
+    for key, link in r.json().items():
+        for method in ['GET', ]: # 'PUT', 'POST', 'PATCH', 'HEAD',]:
+            expected_response_code = httplib.OK
+            expected_response_schema = method.lower()
+
+            # authtoken only supports POST, OPTIONS
+            if key == 'authtoken':
+                if method in ['HEAD', 'GET', 'PUT', 'PATCH']:
+                    expected_response_code = httplib.METHOD_NOT_ALLOWED
+                    expected_response_schema = 'unauthorized'
+                elif method in ['OPTIONS']:
+                    # expected_response_code = httplib.OK
+                    expected_response_schema = None
+                elif method in ['POST']:
+                    # expected_response_code = httplib.BAD_REQUEST
+                    expected_response_schema = None
+
+            # No json response for 'HEAD'
+            if method == 'HEAD':
+                expected_response_schema = None
+
+            # assert!
+            yield "%s:%s" % (method, link), assert_response, api, link, method, expected_response_code, expected_response_schema
