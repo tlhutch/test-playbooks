3e12b35fd9b007c30831ea1b7fac6cba92d3ac0e
James Laska <jlaska@ansibleworks.com>
2014-01-11 13:40:15 -0500

    All GET|POST|PUT|PATCH methods handle the API response with handle_response() method
    This includes methods called from a Base_List object, and a Base object.  In
    addition, the property __parent__ has been renamed to __item_class__.  This
    property returns the class representing a singular object.  For example, the
    User_list() class (plural) would have an __item_class__ of User() (singular).

--- tests/lib/common/api/pages/base.py
+++ tests/lib/common/api/pages/base.py
@@ -36,6 +36,11 @@ class Base(Page):
         if kwargs.get('base_url', False):
             self.base_url = kwargs.get('base_url')
 
+    @property
+    def __item_cls__(self):
+        '''Returns the class representing a single 'Base' item'''
+        return self.__class__
+
     def validate_json(self, json=None, request='GET'):
         '''
         Perform JSON validation on JSON response
@@ -45,17 +50,7 @@ class Base(Page):
         else:
             validate(json, self.base_url, request.lower())
 
-    def get(self, **params):
-        ''' disables json validation '''
-        r = self.api.get(self.base_url.format(**self.json), params=params)
-        assert r.status_code == httplib.OK
-        self.json = r.objectify()
-        # TODO - validate schema
-        #self.validate_json('get')
-        return self
-
-    def post(self, payload={}):
-        r = self.api.post(self.base_url, payload)
+    def handle_request(self, r):
         try:
             data = r.json()
         except ValueError, e:
@@ -64,10 +59,31 @@ class Base(Page):
 
         exc_str = "%s (%s) received" % (httplib.responses[r.status_code], r.status_code)
         if r.status_code in (httplib.OK, httplib.CREATED, httplib.ACCEPTED):
-            self.validate_json(json=data, request='post')
-            # FIXME - this should return a populated object
-            # return self.__class__(self.testsetup, base_url=data['url'], json=data)
-            return self.__class__(self.testsetup, base_url=data['url'], json=r.objectify())
+            self.validate_json(json=data, request=r.request.method)
+
+            # Not all JSON responses include a URL.  Grab it from the request
+            # object, if needed.
+            if not 'url' in data:
+                data['url'] = r.request.path_url
+
+            # Also, don't forget to call r.objectify() on the json structure.
+            # Without that call, it will simply be a json dictionary.  By calling
+            # r.objectify(), it behaves like a classic dictionary, but also is
+            # JSON_Wrapped() to allow attribute access (e.g.
+            # self.json['username'] == self.username)
+            self.json = r.objectify()
+
+            # GET requests should return an object of the same type that made
+            # the request.  For example, when performing a GET on /api/v1/users,
+            # return an object of type 'Users'.  A GET on /api/v1/users/4/
+            # would return an object of type 'User'.
+            if r.request.method.lower() == 'get':
+                return self.__class__(self.testsetup, base_url=data['url'], json=self.json)
+            # Other requests (POST, PATCH, PUT etc...) always return a singular
+            # element.  For example, a POST to /api/v1/users will return a 'User'
+            # object.
+            else:
+                return self.__item_class__(self.testsetup, base_url=data['url'], json=self.json)
         elif r.status_code == httplib.NO_CONTENT:
             raise NoContent_Exception(exc_str)
         elif r.status_code == httplib.FORBIDDEN:
@@ -85,23 +101,30 @@ class Base(Page):
         else:
             raise Unknown_Exception(exc_str + ": %s" % data)
 
+    def get(self, **params):
+        ''' disables json validation '''
+        r = self.api.get(self.base_url.format(**self.json), params=params)
+        self.handle_request(r)
+        return self
+
+    def post(self, payload={}):
+        r = self.api.post(self.base_url, payload)
+        return self.handle_request(r)
+
     def put(self):
         r = self.api.put(self.base_url.format(**self.json), self.json)
-        assert r.status_code == httplib.OK
-        self.json = r.objectify()
-        # TODO - validate schema
-        #self.validate_json('put')
+        return self.handle_request(r)
 
     def patch(self, **payload):
         r = self.api.patch(self.base_url.format(**self.json), payload)
-        assert r.status_code == httplib.OK
-        self.json = r.objectify()
-        # TODO - validate schema
-        #self.validate_json('patch')
+        return self.handle_request(r)
 
     def delete(self):
         r = self.api.delete(self.base_url.format(**self.json))
-        assert r.status_code == httplib.NO_CONTENT
+        try:
+            return self.handle_request(r)
+        except NoContent_Exception:
+            pass
 
     def get_related(self, name):
         assert name in self.json['related']
@@ -120,12 +143,15 @@ class Base_List(Base):
     previous = property(json_getter('previous'), json_setter('previous'))
 
     @property
-    def results(self):
-        return [self.__parent__(self.testsetup, base_url=data.get('url',None), json=data) for data in self.json['results']]
+    def __item_class__(self):
+        '''Returns the class representing a single 'Base' item'''
+        # With an inheritence of Org_List -> Org -> Base -> Page, the following
+        # will return the parent class of the current object (e.g. 'Org').
+        return inspect.getmro(self.__class__)[1]
 
     @property
-    def __parent__(self):
-        return inspect.getmro(self.__class__)[1]
+    def results(self):
+        return [self.__item_class__(self.testsetup, base_url=data.get('url',None), json=data) for data in self.json['results']]
 
     def options(self, **kwargs):
         raise NotImplementedError
@@ -134,17 +160,15 @@ class Base_List(Base):
         raise NotImplementedError
 
     def get(self, **params):
-        ''' performs json validation '''
         r = self.api.get(self.base_url.format(**self.json), params=params)
-        assert r.status_code == httplib.OK
-
-        # validate schema
-        self.json = r.objectify()
-        self.validate_json(request='get')
-        return self
+        return self.handle_request(r)
 
     def post(self, payload={}):
         r = self.api.post(self.base_url, payload)
+        return self.handle_request(r)
+
+    # This method id already defined
+    def _OLD_handle_request(self, r):
         try:
             data = r.json()
         except ValueError, e:
@@ -153,7 +177,7 @@ class Base_List(Base):
 
         exc_str = "%s (%s) received" % (httplib.responses[r.status_code], r.status_code)
         if r.status_code in (httplib.OK, httplib.CREATED, httplib.ACCEPTED):
-            self.validate_json(json=data, request='post')
+            self.validate_json(json=data, request=r.request.method)
             # With an inheritence of Org_List -> Org -> Base -> Page, the following
             # will find the parent class of the current object (e.g. 'Org').
 
@@ -162,7 +186,7 @@ class Base_List(Base):
             # r.objectify(), it behaves like a classic dictionary, but also is
             # JSON_Wrapped() to allow attribute access (e.g.
             # self.json['username'] == self.username)
-            return self.__parent__(self.testsetup, base_url=data['url'], json=r.objectify())
+            return self.__item_class__(self.testsetup, base_url=data['url'], json=r.objectify())
         elif r.status_code == httplib.NO_CONTENT:
             raise NoContent_Exception(exc_str)
         elif r.status_code == httplib.FORBIDDEN:
