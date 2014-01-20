2695b1e4ae171ed38b4db46d596ebc3755e483fd
James Laska <jlaska@ansibleworks.com>
2014-01-20 15:16:08 -0500

    Account for other ways to indicate OFFICIAL=false
    Build parameters from AWX_Nightly_Build are passed along.  That build sets
    OFFICIAL=no.

--- playbooks/setup.sh
+++ playbooks/setup.sh
@@ -73,10 +73,10 @@ fi
 # If OFFICIAL=yes, use the public repository. Otherwise, use the nightly
 # repository.
 case "${PLAYBOOK}-${OFFICIAL}" in
-    awx*-false)
+    awx*-false|awx*-no)
         echo "aw_repo_url: http://50.116.42.103/awx_nightlies_RTYUIOPOIUYTYU" >> vars.yaml
         ;;
-    galaxy*-false)
+    galaxy*-false|galaxy*-no)
         echo "aw_repo_url: http://50.116.42.103/galaxy_nightlies_Y6ptm6ES82A5h79V" >> vars.yaml
         ;;
     *)
