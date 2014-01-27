b88b6d74c52ceb560b2b635791a65c8c865348b6
James Laska <jlaska@ansibleworks.com>
2014-01-27 16:33:42 -0500

    Add is_successful @property and wait_until_completed() method

--- tests/lib/common/api/pages/jobs.py
+++ tests/lib/common/api/pages/jobs.py
@@ -1,4 +1,6 @@
+import time
 import base
+import common.utils
 from common.exceptions import *
 
 class Job_Page(base.Base):
@@ -9,6 +11,17 @@ class Job_Page(base.Base):
     result_stdout = property(base.json_getter('result_stdout'), base.json_setter('result_stdout'))
     created = property(base.json_getter('created'), base.json_setter('created'))
     modified = property(base.json_getter('modified'), base.json_setter('failed'))
+
+    @property
+    def is_successful(self):
+        return 'successful' == self.status.lower()
+
+    def wait_until_completed(self):
+        return common.utils.wait_until(self, 'status', ('successful', 'failed', 'error', 'canceled',),
+            interval=5,     # Continously poll the server for status
+            verbose=0,      # Enable verbosity
+            timeout=60*8,   # 8 minutes
+            start_time=time.strptime(self.created, '%Y-%m-%dT%H:%M:%S.%fZ'))
 
 class Jobs_Page(Job_Page, base.Base_List):
     base_url = '/api/v1/jobs/'
