e4786e6f939f1db174bd1b685aa7fb24414053e2
James Laska <jlaska@ansible.com>
2014-02-04 12:10:16 -0500

    Update test_license to verify host counts

--- tests/api/test_license.py
+++ tests/api/test_license.py
@@ -17,9 +17,12 @@
 
 '''
 
+import json
 import pytest
 import jsonschema
+import common.utils
 import common.tower.license
+import common.exceptions
 from tests.api import Base_Api_Test
 
 # The following fixture runs once for this entire module
@@ -39,11 +42,16 @@ def install_demo_license(request, ansible_runner):
     ansible_runner.file(path='/etc/awx/aws', state='absent')
     ansible_runner.file(path='/etc/awx/license', state='absent')
 
+@pytest.fixture(scope='class')
+def license_instance_count(request, ansible_runner):
+    '''Number of host instances permitted by the license'''
+    return 20
+
 # The following fixture runs once for each class that uses it
 @pytest.fixture(scope='class')
-def install_license(request, ansible_runner):
+def install_license(request, ansible_runner, license_instance_count):
 
-    fname = common.tower.license.generate_license_file(instance_count=20, days=31)
+    fname = common.tower.license.generate_license_file(instance_count=license_instance_count, days=31)
     # Using ansible, copy the license to the target system
     ansible_runner.copy(src=fname, dest='/etc/awx/license', owner='awx', group='awx', mode='0600')
 
@@ -63,17 +71,82 @@ def instance_id(ansible_runner):
 
 # The following fixture runs once for each class that uses it
 @pytest.fixture(scope='class')
-def install_aws(request, testsetup, ansible_runner, ami_id, instance_id):
-    fname = common.tower.license.generate_aws_file(instance_count=20, ami_id=ami_id, instance_id=instance_id)
+def install_aws(request, ansible_runner, license_instance_count, ami_id, instance_id):
+    fname = common.tower.license.generate_aws_file(instance_count=license_instance_count, ami_id=ami_id, instance_id=instance_id)
     ansible_runner.copy(src=fname, dest='/etc/awx/aws', owner='awx', group='awx', mode='0600')
 
     def teardown():
         ansible_runner.file(path='/etc/awx/aws', state='absent')
     request.addfinalizer(teardown)
 
+@pytest.fixture(scope="class")
+def organization(request, authtoken, api_organizations_pg):
+    payload = dict(name="org-%s" % common.utils.random_ascii())
+    obj = api_organizations_pg.post(payload)
+    request.addfinalizer(obj.delete)
+    return obj
+
+@pytest.fixture(scope="class")
+def inventory(request, authtoken, api_inventories_pg, api_groups_pg, api_hosts_pg, organization):
+    payload = dict(name="inventory-%s" % common.utils.random_ascii(),
+                   organization=organization.id,
+                   variables=json.dumps(dict(ansible_connection='local')))
+    obj = api_inventories_pg.post(payload)
+    request.addfinalizer(obj.delete)
+    return obj
+
+@pytest.fixture(scope="class")
+def group(request, authtoken, api_groups_pg, inventory):
+    payload = dict(name="group-%s" % common.utils.random_ascii(),
+                   inventory=inventory.id,
+                   variables=json.dumps(dict(ansible_connection='local')))
+    obj = api_groups_pg.post(payload)
+    request.addfinalizer(obj.delete)
+    return obj
+
+class Base_License_Test(Base_Api_Test):
+
+    def test_instance_counts(self, api_config_pg, license_instance_count, inventory, group):
+
+        # Get API resource /groups/N/hosts
+        group_hosts_pg = group.get_related('hosts')
+
+        current_hosts = 0
+        while current_hosts <= license_instance_count + 1:
+            # Get the /config resource
+            conf = api_config_pg.get()
+
+            # Verify instance counts
+            assert conf.license_info.current_instances == current_hosts
+            assert conf.license_info.free_instances == \
+                license_instance_count - current_hosts
+            assert conf.license_info.available_instances == license_instance_count
+            print "current_instances:%s, free_instances:%s, available_instances:%s" % \
+                (conf.license_info.current_instances, conf.license_info.free_instances, \
+                 conf.license_info.available_instances )
+
+            # Add a host to the inventory group
+            payload = dict(name="host-%s" % common.utils.random_ascii(),
+                           inventory=group.inventory)
+            # The first 20 hosts should succeed
+            if current_hosts < license_instance_count:
+                group_hosts_pg.post(payload)
+                current_hosts += 1
+            # Anything more than 'license_instance_count' will raise a 403
+            else:
+                with pytest.raises(common.exceptions.LicenseExceeded_Exception):
+                    result = group_hosts_pg.post(payload)
+                break
+
+        # Verify maximum instances
+        assert current_hosts == license_instance_count
+        assert conf.license_info.current_instances == license_instance_count
+        assert conf.license_info.free_instances == 0
+        assert conf.license_info.available_instances == license_instance_count
+
 @pytest.mark.skip_selenium
 @pytest.mark.nondestructive
-class Test_Demo_License(Base_Api_Test):
+class Test_Demo_License(Base_License_Test):
     @pytest.mark.usefixtures('authtoken', 'backup_license', 'install_demo_license')
     def test_metadata(self, api_config_pg):
         conf = api_config_pg.get()
@@ -86,23 +159,15 @@ class Test_Demo_License(Base_Api_Test):
         assert 'key_present' in conf.license_info
         assert not conf.license_info.key_present
 
-    # Create inventory and hosts
     @pytest.mark.usefixtures('authtoken', 'backup_license', 'install_demo_license')
-    def test_instance_counts(self, api_config_pg):
-        conf = api_config_pg.get()
-
-        # Verify number of instances
-        assert conf.license_info.current_instances == 0
-        assert conf.license_info.free_instances == 10
-        assert conf.license_info.available_instances == 10
-
-        # FIXME - start adding hosts
+    def test_instance_counts(self, api_config_pg, inventory, group):
+        super(Test_Demo_License, self).test_instance_counts(api_config_pg, 10, inventory, group)
 
 @pytest.mark.skip_selenium
 @pytest.mark.nondestructive
-# AWS licensing only works when tested on an ec2 instance
-@pytest.mark.skipif("'ec2' not in pytest.config.getvalue('base_url')")
-class Test_AWS_License(Base_Api_Test):
+class Test_AWS_License(Base_License_Test):
+    # AWS licensing only works when tested on an ec2 instance
+    @pytest.mark.skipif("'ec2' not in pytest.config.getvalue('base_url')")
     @pytest.mark.usefixtures('authtoken', 'backup_license', 'install_aws')
     def test_metadata(self, api_config_pg):
 
@@ -126,23 +191,15 @@ class Test_AWS_License(Base_Api_Test):
         assert 'instance-id' in conf.license_info
         assert 'instance_count' in conf.license_info
 
-    # Create inventory and hosts
+    # AWS licensing only works when tested on an ec2 instance
     @pytest.mark.usefixtures('authtoken', 'backup_license', 'install_aws')
-    def test_instance_counts(self, api_config_pg):
-        conf = api_config_pg.get()
-
-        # Verify number of instances
-        assert conf.license_info.compliant
-        assert conf.license_info.current_instances == 0
-        assert conf.license_info.free_instances == \
-            conf.license_info.instance_count - conf.license_info.current_instances
-        assert conf.license_info.available_instances == conf.license_info.instance_count
-
-        # FIXME - start adding hosts
+    @pytest.mark.skipif("'ec2' not in pytest.config.getvalue('base_url')")
+    def test_instance_counts(self, api_config_pg, license_instance_count, inventory, group):
+        super(Test_AWS_License, self).test_instance_counts(api_config_pg, license_instance_count, inventory, group)
 
 @pytest.mark.skip_selenium
 @pytest.mark.nondestructive
-class Test_License(Base_Api_Test):
+class Test_License(Base_License_Test):
     @pytest.mark.usefixtures('authtoken', 'backup_license', 'install_license')
     def test_metadata(self, api_config_pg):
 
@@ -168,14 +225,5 @@ class Test_License(Base_Api_Test):
 
     # Create inventory and hosts
     @pytest.mark.usefixtures('authtoken', 'backup_license', 'install_license')
-    def test_instance_counts(self, api_config_pg):
-        conf = api_config_pg.get()
-
-        # Verify number of instances
-        assert conf.license_info.compliant
-        assert conf.license_info.current_instances == 0
-        assert conf.license_info.free_instances == \
-            conf.license_info.instance_count - conf.license_info.current_instances
-        assert conf.license_info.available_instances == conf.license_info.instance_count
-
-        # FIXME - start adding hosts
+    def test_instance_counts(self, api_config_pg, license_instance_count, inventory, group):
+        super(Test_License, self).test_instance_counts(api_config_pg, license_instance_count, inventory, group)
