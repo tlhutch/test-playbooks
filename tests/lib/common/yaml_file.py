776bd8a47e55fed8889707f781e31995175e5513
James Laska <jlaska@ansible.com>
2014-02-18 10:14:50 -0500

    Define custom CLoader to handle yaml !include

--- tests/lib/common/yaml_file.py
+++ tests/lib/common/yaml_file.py
@@ -1,7 +1,44 @@
 import os
 import yaml
+import glob
 from py.path import local
 from common.randomness import RandomizeValues
+
+class Loader(yaml.CLoader):
+    def __init__(self, stream):
+        self._root = os.path.split(stream.name)[0]
+        super(Loader, self).__init__(stream)
+        Loader.add_constructor('!include', Loader.include)
+        Loader.add_constructor('!import',  Loader.include)
+
+    def include(self, node):
+        if isinstance(node, yaml.ScalarNode):
+            return self.extractFile(self.construct_scalar(node))
+
+        elif isinstance(node, yaml.SequenceNode):
+            result = []
+            for filename in self.construct_sequence(node):
+                result += self.extractFile(filename)
+            return result
+
+        elif isinstance(node, yaml.MappingNode):
+            result = {}
+            for k,v in self.construct_mapping(node).iteritems():
+                result[k] = self.extractFile(v)
+            return result
+
+        else:
+            logging.error("unrecognised node type in !include statement")
+            raise yaml.constructor.ConstructorError
+
+    def extractFile(self, filename):
+        file_pattern = os.path.join(self._root, filename)
+        data = dict()
+        for filepath in glob.glob(file_pattern):
+            with open(filepath, 'r') as f:
+                data.update(yaml.load(f, Loader))
+        return data
+
 
 def load_file(filename=None):
     """
