413c6036beccb30a337b78d82f5d2397bca2a024
James Laska <jlaska@ansible.com>
2014-03-22 15:10:21 -0400

    Initial /schedules API page

---
+++ tests/lib/common/api/pages/schedules.py
@@ -0,0 +1,23 @@
+import base
+from projects import Project_Page
+from inventories import Inventory_Page
+from job_templates import Job_Template_Page
+from common.exceptions import *
+
+class Schedule_Page(base.Task_Page):
+    base_url = '/api/v1/schedules/{id}/'
+
+    def get_related(self, name, **params):
+        assert name in self.json['related']
+        if name == 'project':
+            related = Project_Page(self.testsetup, base_url=self.json['related'][name])
+        elif name == 'inventory_update':
+            related = Inventory_Page(self.testsetup, base_url=self.json['related'][name])
+        elif name == 'job_template':
+            related = Job_Template_Page(self.testsetup, base_url=self.json['related'][name])
+        else:
+            raise NotImplementedError
+        return related.get(**params)
+
+class Schedules_Page(Schedule_Page, base.Base_List):
+    base_url = '/api/v1/schedules/'
