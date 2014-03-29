0c17a749f3e4c5d62ace72154a072610a43155d6
James Laska <jlaska@ansible.com>
2014-03-29 19:19:22 -0400

    Change api_navigation fixtures to 'module' scope
    This means they'll be refreshed for each *.py test module.  This helps if one
    module logs out or otherwise messes with credentials.  The next module will
    have no difficulty getting an authtoken.

--- tests/lib/fixtures/api_navigation.py
+++ tests/lib/fixtures/api_navigation.py
@@ -21,14 +21,14 @@ def navigate(api, url, field):
     data = api.get(url).json()
     return data.get(field)
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api(testsetup):
     '''
     Convenience fixture that returns api object
     '''
     return testsetup.api
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_home(request, api):
     '''
     Navigate the API and return a link to the base api for the requested version.
@@ -53,11 +53,11 @@ def api_home(request, api):
 #
 # /api/v1/authtoken
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_authtoken_url(api, api_home):
     return navigate(api, api_home, 'authtoken')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_authtoken_pg(testsetup, api_authtoken_url):
     return AuthToken_Page(testsetup, \
         base_url=api_authtoken_url)
@@ -65,11 +65,11 @@ def api_authtoken_pg(testsetup, api_authtoken_url):
 #
 # /api/v1/config
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_config_url(api, api_home):
     return navigate(api, api_home, 'config')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_config_pg(testsetup, api_config_url):
     return Config_Page(testsetup, \
         base_url=api_config_url)
@@ -77,11 +77,11 @@ def api_config_pg(testsetup, api_config_url):
 #
 # /api/v1/me
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_me_url(api, api_home):
     return navigate(api, api_home, 'me')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_me_pg(testsetup, api_me_url):
     return Me_Page(testsetup, \
         base_url=api_me_url)
@@ -89,11 +89,11 @@ def api_me_pg(testsetup, api_me_url):
 #
 # /api/v1/organizations
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_organizations_url(api, api_home):
     return navigate(api, api_home, 'organizations')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_organizations_pg(testsetup, api_organizations_url):
     return Organizations_Page(testsetup, \
         base_url=api_organizations_url)
@@ -101,11 +101,11 @@ def api_organizations_pg(testsetup, api_organizations_url):
 #
 # /api/v1/users
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_users_url(api, api_home):
     return navigate(api, api_home, 'users')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_users_pg(testsetup, api_users_url):
     return Users_Page(testsetup, \
         base_url=api_users_url)
@@ -113,11 +113,11 @@ def api_users_pg(testsetup, api_users_url):
 #
 # /api/v1/teams
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_teams_url(api, api_home):
     return navigate(api, api_home, 'teams')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_teams_pg(testsetup, api_teams_url):
     return Teams_Page(testsetup, \
         base_url=api_teams_url)
@@ -125,11 +125,11 @@ def api_teams_pg(testsetup, api_teams_url):
 #
 # /api/v1/projects
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_projects_url(api, api_home):
     return navigate(api, api_home, 'projects')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_projects_pg(testsetup, api_projects_url):
     return Projects_Page(testsetup, \
         base_url=api_projects_url)
@@ -137,11 +137,11 @@ def api_projects_pg(testsetup, api_projects_url):
 #
 # /api/v1/teams
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_teams_url(api, api_home):
     return navigate(api, api_home, 'teams')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_teams_pg(testsetup, api_teams_url):
     return Teams_Page(testsetup, \
         base_url=api_teams_url)
@@ -149,11 +149,11 @@ def api_teams_pg(testsetup, api_teams_url):
 #
 # /api/v1/credentials
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_credentials_url(api, api_home):
     return navigate(api, api_home, 'credentials')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_credentials_pg(testsetup, api_credentials_url):
     return Credentials_Page(testsetup, \
         base_url=api_credentials_url)
@@ -161,11 +161,11 @@ def api_credentials_pg(testsetup, api_credentials_url):
 #
 # /api/v1/inventory
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_inventories_url(api, api_home):
     return navigate(api, api_home, 'inventory')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_inventories_pg(testsetup, api_inventories_url):
     return Inventories_Page(testsetup, \
         base_url=api_inventories_url)
@@ -173,11 +173,11 @@ def api_inventories_pg(testsetup, api_inventories_url):
 #
 # /api/v1/inventory_sources
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_inventory_sources_url(api, api_home):
     return navigate(api, api_home, 'inventory_sources')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_inventory_sources_pg(testsetup, api_inventory_sources_url):
     return Inventory_Sources_Page(testsetup, \
         base_url=api_inventory_sources_url)
@@ -185,11 +185,11 @@ def api_inventory_sources_pg(testsetup, api_inventory_sources_url):
 #
 # /api/v1/groups
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_groups_url(api, api_home):
     return navigate(api, api_home, 'groups')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_groups_pg(testsetup, api_groups_url):
     return Groups_Page(testsetup, \
         base_url=api_groups_url)
@@ -197,11 +197,11 @@ def api_groups_pg(testsetup, api_groups_url):
 #
 # /api/v1/hosts
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_hosts_url(api, api_home):
     return navigate(api, api_home, 'hosts')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_hosts_pg(testsetup, api_hosts_url):
     return Hosts_Page(testsetup, \
         base_url=api_hosts_url)
@@ -209,11 +209,11 @@ def api_hosts_pg(testsetup, api_hosts_url):
 #
 # /api/v1/job_templates
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_job_templates_url(api, api_home):
     return navigate(api, api_home, 'job_templates')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_job_templates_pg(testsetup, api_job_templates_url):
     return Job_Templates_Page(testsetup, \
         base_url=api_job_templates_url)
@@ -221,16 +221,16 @@ def api_job_templates_pg(testsetup, api_job_templates_url):
 #
 # /api/v1/jobs
 #
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_jobs_url(api, api_home):
     return navigate(api, api_home, 'jobs')
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def api_jobs_pg(testsetup, api_jobs_url):
     return Jobs_Page(testsetup, \
         base_url=api_jobs_url)
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def authtoken(api, testsetup, api_authtoken_url):
     '''
     Logs in to the application with default credentials and returns the
@@ -247,14 +247,14 @@ def authtoken(api, testsetup, api_authtoken_url):
     testsetup.api.login(token=json['token'])
     return json
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def awx_config(api, api_home):
     url = navigate(api, api_home, 'config')
     r = api.get(url)
     assert r.status_code == httplib.OK
     return r.json()
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="module")
 def region_choices(api_inventory_sources_pg):
     options = api_inventory_sources_pg.options()
 
