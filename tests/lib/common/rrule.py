626ce00717cb5d7084688c65cb0b6e399e488dd0
James Laska <jlaska@ansible.com>
2014-04-03 16:55:19 -0400

    Move RRule class to common.rrule

---
+++ tests/lib/common/rrule.py
@@ -0,0 +1,34 @@
+import dateutil.rrule
+
+class RRule(dateutil.rrule.rrule):
+    '''Sub-class rrule to support __str__'''
+
+    FREQNAMES = ['YEARLY','MONTHLY','WEEKLY','DAILY','HOURLY','MINUTELY','SECONDLY']
+    def __str__(self):
+        parts = list()
+        parts.append('FREQ=' + self.FREQNAMES[self._freq])
+        if self._interval:
+            parts.append('INTERVAL=' + str(self._interval))
+        if self._wkst:
+            parts.append('WKST=' + str(self._wkst))
+        if self._count:
+            parts.append('COUNT=' + str(self._count))
+
+        for name, value in [
+                ('BYSETPOS', self._bysetpos),
+                ('BYMONTH', self._bymonth),
+                ('BYMONTHDAY', self._bymonthday),
+                ('BYYEARDAY', self._byyearday),
+                ('BYWEEKNO', self._byweekno),
+                ('BYWEEKDAY', self._byweekday),
+                # ('BYWEEKDAY', (dateutil.rrule.weekdays[num] for num in self._byweekday)),
+                ('BYHOUR', self._byhour),
+                ('BYMINUTE', self._byminute),
+                ('BYSECOND', self._bysecond),
+                ]:
+            if name == "BYWEEKDAY" and value:
+                value = (dateutil.rrule.weekdays[num] for num in value)
+            if value:
+                parts.append(name + '=' + ','.join(str(v) for v in value))
+
+        return '''DTSTART:%s RRULE:%s ''' % (re.sub(r'[:-]', '', self._dtstart.strftime("%Y%m%dT%H%M%SZ")), ';'.join(parts))
