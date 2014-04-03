24cdeebb15be79aaaaa2c9bddb2f4baa81037761
James Laska <jlaska@ansible.com>
2014-04-03 15:17:28 -0400

    Add related->unified_jobs resource

--- tests/lib/common/api/pages/schedules.py
+++ tests/lib/common/api/pages/schedules.py
@@ -2,6 +2,7 @@ import base
 from projects import Project_Page
 from inventory import Inventory_Page
 from job_templates import Job_Template_Page
+from jobs import Jobs_Page
 from common.exceptions import *
 
 class Schedule_Page(base.Task_Page):
@@ -20,6 +21,8 @@ class Schedule_Page(base.Task_Page):
             related = Project_Page(self.testsetup, base_url=self.json['related'][name])
         elif name == 'inventory_update':
             related = Inventory_Page(self.testsetup, base_url=self.json['related'][name])
+        elif name == 'unified_jobs':
+            related = Jobs_Page(self.testsetup, base_url=self.json['related'][name])
         elif name == 'job_template':
             related = Job_Template_Page(self.testsetup, base_url=self.json['related'][name])
         else:
