0316f0cdd28280a241e88346cbd30bd97409f5e2
James Laska <jlaska@ansible.com>
2014-04-04 14:58:30 -0400

    Support running script from multiple locations

--- scripts/jenkins_runtest.sh
+++ scripts/jenkins_runtest.sh
@@ -9,7 +9,15 @@ set -- ${INVENTORY_LINE}
 BASEURL="https://$1"
 
 # Create and modify credentials.yaml
-python scripts/create_credentials.py tests/credentials.template tests/credentials.yaml
+SCRIPT_NAME="create_credentials.py"
+# Look for script in multple locations (this allows for backwards compat with
+# jenkins)
+for SCRIPT_DIR in "scripts" "." ; do
+    if [ -f "${SCRIPT_DIR}/${SCRIPT_NAME}" ]; then
+        break
+    fi
+done
+echo python ${SCRIPT_DIR}/${SCRIPT_NAME} tests/credentials.template tests/credentials.yaml
 
 export ANSIBLE_NOCOLOR=True
 export ANSIBLE_HOST_KEY_CHECKING=False
