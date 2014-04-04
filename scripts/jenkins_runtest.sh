9246f50cb86aaa98d9f8057ddbf29cc80e5a6692
James Laska <jlaska@ansible.com>
2014-04-04 11:48:38 -0400

    Updated scripts to improve jenkins build process

--- scripts/jenkins_runtest.sh
+++ scripts/jenkins_runtest.sh
@@ -8,22 +8,6 @@ INVENTORY_LINE=$(grep "^\[ ${PLATFORM} \]" -A1 ${ANSIBLE_INVENTORY} | tail -n1)
 set -- ${INVENTORY_LINE}
 BASEURL="https://$1"
 
-# Gather SCM private key credentials
-if [ -z "${SCM_KEY_DATA}" ]; then
-    SCM_KEY_DATA="$HOME/.ssh/id_rsa.github-ansible-jenkins-nopassphrase"
-fi
-if [ -z "${SCM_KEY_DATA_ENCRYPTED}" ]; then
-    SCM_KEY_DATA_ENCRYPTED="$HOME/.ssh/id_rsa.github-ansible-jenkins-passphrase"
-fi
-
-# Gather SSH private key credentials
-if [ -z "${SSH_KEY_DATA}" ]; then
-    SSH_KEY_DATA="$HOME/.ssh/id_rsa.jenkins-nopassphrase"
-fi
-if [ -z "${SSH_KEY_DATA_ENCRYPTED}" ]; then
-    SSH_KEY_DATA_ENCRYPTED="$HOME/.ssh/id_rsa.jenkins-passphrase"
-fi
-
 # Create and modify credentials.yaml
 python scripts/create_credentials.py tests/credentials.template tests/credentials.yaml
 
