dc1b1e4e1a5ede233249cf0ccf18028e4d1adc83
James Laska <jlaska@ansible.com>
2014-08-06 12:22:42 -0400

    Update logging to include filename

--- tests/lib/fixtures/license.py
+++ tests/lib/fixtures/license.py
@@ -3,73 +3,80 @@ import logging
 import pytest
 import common.tower.license
 
-# The following fixture runs once for this entire module
+
+log = logging.getLogger(__name__)
+
+
 @pytest.fixture(scope='class')
 def backup_license(request, ansible_runner):
     '''Backup and existing license files. The files will be restored upon teardown.
     '''
-    logging.debug("calling fixture backup_license")
+    log.debug("calling fixture backup_license")
     ansible_runner.shell('test -f /etc/awx/aws && mv /etc/awx/aws /etc/awx/.aws', creates='/etc/awx/.aws', removes='/etc/awx/aws')
     ansible_runner.shell('test -f /etc/awx/license && mv /etc/awx/license /etc/awx/.license', creates='/etc/awx/.license', removes='/etc/awx/license')
 
     def teardown():
-        logging.debug("calling teardown backup_license")
+        log.debug("calling teardown backup_license")
         ansible_runner.shell('test -f /etc/awx/.aws && mv /etc/awx/.aws /etc/awx/aws', creates='/etc/awx/aws', removes='/etc/awx/.aws')
         ansible_runner.shell('test -f /etc/awx/.license && mv /etc/awx/.license /etc/awx/license', creates='/etc/awx/license', removes='/etc/awx/.license')
     request.addfinalizer(teardown)
+
 
 @pytest.fixture(scope='class')
 def install_license_100(request, ansible_runner):
     '''Install a license where instance_count=100
     '''
 
-    logging.debug("calling fixture install_license_100")
+    log.debug("calling fixture install_license_100")
     fname = common.tower.license.generate_license_file(instance_count=100, days=365)
     ansible_runner.copy(src=fname, dest='/etc/awx/license', owner='awx', group='awx', mode='0600')
 
     def teardown():
-        logging.debug("calling teardown install_license_100")
+        log.debug("calling teardown install_license_100")
         ansible_runner.file(path='/etc/awx/license', state='absent')
     request.addfinalizer(teardown)
+
 
 @pytest.fixture(scope='class')
 def install_license_1000(request, ansible_runner):
     '''Install a license where instance_count=1000
     '''
 
-    logging.debug("calling fixture install_license_1000")
+    log.debug("calling fixture install_license_1000")
     fname = common.tower.license.generate_license_file(instance_count=1000, days=365)
     ansible_runner.copy(src=fname, dest='/etc/awx/license', owner='awx', group='awx', mode='0600')
 
     def teardown():
-        logging.debug("calling teardown install_license_1000")
+        log.debug("calling teardown install_license_1000")
         ansible_runner.file(path='/etc/awx/license', state='absent')
     request.addfinalizer(teardown)
+
 
 @pytest.fixture(scope='class')
 def install_license_10000(request, ansible_runner):
     '''Install a license where instance_count=10000
     '''
 
-    logging.debug("calling fixture install_license_10000")
+    log.debug("calling fixture install_license_10000")
     fname = common.tower.license.generate_license_file(instance_count=10000, days=365)
     ansible_runner.copy(src=fname, dest='/etc/awx/license', owner='awx', group='awx', mode='0600')
 
     def teardown():
-        logging.debug("calling teardown install_license_10000")
+        log.debug("calling teardown install_license_10000")
         ansible_runner.file(path='/etc/awx/license', state='absent')
     request.addfinalizer(teardown)
+
 
 @pytest.fixture(scope='class')
 def install_license_unlimited(request, ansible_runner):
     '''Install a license where instance_count=unlimited
     '''
 
-    logging.debug("calling fixture install_license_unlimited")
+    log.debug("calling fixture install_license_unlimited")
     fname = common.tower.license.generate_license_file(instance_count=sys.maxint, days=365)
     ansible_runner.copy(src=fname, dest='/etc/awx/license', owner='awx', group='awx', mode='0600')
 
     def teardown():
-        logging.debug("calling teardown install_license_unlimited")
+        log.debug("calling teardown install_license_unlimited")
         ansible_runner.file(path='/etc/awx/license', state='absent')
     request.addfinalizer(teardown)
