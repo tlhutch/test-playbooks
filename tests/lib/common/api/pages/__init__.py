4bf834f5aebaf53d2fe93866798a0aecbd36913e
James Laska <jlaska@ansible.com>
2014-08-11 13:51:16 -0400

    Import the unified_job_template pages

--- tests/lib/common/api/pages/__init__.py
+++ tests/lib/common/api/pages/__init__.py
@@ -6,6 +6,7 @@ from teams import *
 from credentials import *
 from projects import *
 from inventory import *
+from unified_job_templates import *
 from job_templates import *
 from jobs import *
 from config import *
