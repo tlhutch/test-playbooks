2cd1c89ef8d76a55bb519d535dca63305a127991
James Laska <jlaska@ansible.com>
2014-08-21 16:33:22 -0400

    Assertions in test_dashboard

--- tests/ui/test_dashboard.py
+++ tests/ui/test_dashboard.py
@@ -5,5 +5,11 @@ from tests.ui import Base_UI_Test
 @pytest.mark.nondestructive
 class Test_Dashboard(Base_UI_Test):
     def test_current_tab(self, ui_dashboard_pg):
-        assert ui_dashboard_pg.is_the_current_tab
+        assert ui_dashboard_pg.is_the_current_tab # FIXME
+
         assert ui_dashboard_pg.has_activity_stream_button, "Unable to locate activity stream button"
+
+        assert ui_dashboard_pg.has_job_status_graph
+        assert ui_dashboard_pg.has_host_status_graph
+        assert ui_dashboard_pg.has_jobs_list
+        assert ui_dashboard_pg.has_host_count_graph
