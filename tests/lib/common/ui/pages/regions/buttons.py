421411f9cbfb4eab763899ddb856e424ac4d7da4
James Laska <jlaska@ansible.com>
2014-08-29 15:27:20 -0400

    Add generate buttons objects

---
+++ tests/lib/common/ui/pages/regions/buttons.py
@@ -0,0 +1,64 @@
+import logging
+import types
+from selenium.webdriver.common.by import By
+from common.ui.pages import *
+
+
+log = logging.getLogger(__name__)
+
+
+class Base_Button(BaseRegion):
+    _root_locator = None  # provided by caller
+    _item_class = None  # provided by caller
+    _click_post = None  # provided by caller
+
+    def is_displayed(self):
+        return self._root_element.is_displayed()
+
+    def click(self):
+        self._root_element.click()
+        obj = None
+        # FIXME - assert it's a class
+        if self._item_class is not None:
+            obj = self._item_class(self.testsetup)
+        if hasattr(self, '_click_post') and isinstance(self._click_post, (types.MethodType, types.InstanceType)):
+            self._click_post()
+        return obj
+
+
+class Add_Button(Base_Button):
+    '''Region describing a add [+] button'''
+    _root_locator = (By.CSS_SELECTOR, '#add_btn')
+
+
+class Close_Button(Base_Button):
+    '''Region describing a close [x] button'''
+    _root_locator = (By.CSS_SELECTOR, '#close_btn')
+
+
+class Refresh_Button(Base_Button):
+    '''Region describing a [refresh] button'''
+    _root_locator = (By.CSS_SELECTOR, '#refresh_btn')
+
+
+class Help_Button(Base_Button):
+    '''Region describing a help [?] button'''
+    _root_locator = (By.CSS_SELECTOR, '#help_btn')
+
+
+class Activity_Stream_Button(Base_Button):
+    '''Region describing the activity stream [clock] button'''
+    _root_locator = (By.CSS_SELECTOR, '#stream_btn')
+
+    def __init__(self, testsetup, **kwargs):
+        super(Activity_Stream_Button, self).__init__(testsetup, **kwargs)
+        self._click_post = self.wait_for_spinny
+
+
+class Activity_Stream_Refresh_Button(Base_Button):
+    '''Region describing an activity_stream [refresh] button'''
+    # The following works for post-2.0.0
+    # _root_locator = (By.CSS_SELECTOR, '#activity_stream_refresh_btn')
+    _root_locator = (By.CSS_SELECTOR, '#stream-container > #refresh_btn')
+
+
--- tests/lib/common/ui/pages/regions/stream_container.py
+++ tests/lib/common/ui/pages/regions/stream_container.py
@@ -1,30 +1,25 @@
 from selenium.webdriver.common.by import By
-from common.ui.pages import *
+from common.ui.pages import BaseRegion
+from common.ui.pages.regions.buttons import Close_Button, Activity_Stream_Refresh_Button
 
 
 class Activity_Stream_Region(BaseRegion):
     '''Base activity stream page'''
     _breadcrumb_title = 'Activity Stream'
-    _parent_object = Base
     _root_locator = (By.CSS_SELECTOR, '#stream-container')
-    _refresh_btn_locator = (By.CSS_SELECTOR, '#refresh_btn')
-    _close_btn_locator = (By.CSS_SELECTOR, '#close_btn')
+    _item_class = None
 
     @property
-    def refresh_button(self):
-        return self.find_visible_element(*self._refresh_btn_locator)
+    def refresh_btn(self):
+        return Activity_Stream_Refresh_Button(self.testsetup, _click_post=self.wait_for_spinny)
 
     @property
-    def has_refresh_button(self):
-        try:
-            return self.refresh_button.is_displayed()
-        except NoSuchElementException:
-            return False
-
     def click_refresh(self):
-        self.refresh_button.click()
-        self.wait_for_spinny()
-        return self
+        self.find_element(*(By.CSS_SELECTOR, '#refresh_btn')).click()
+
+    @property
+    def close_btn(self):
+        return Close_Button(self.testsetup, _item_class=self._item_class, _click_post=self.wait_for_slideout)
 
     def wait_for_slidein(self):
         '''wait for activity stream to appear'''
@@ -33,34 +28,3 @@ class Activity_Stream_Region(BaseRegion):
     def wait_for_slideout(self):
         '''wait for activity stream to disappear'''
         self.wait_for_element_not_present(*self._root_locator)
-
-    @property
-    def close_button(self):
-        return self.find_visible_element(*self._close_btn_locator)
-
-    @property
-    def has_close_button(self):
-        try:
-            return self.close_button.is_displayed()
-        except NoSuchElementException:
-            return False
-
-    def click_close(self):
-        self.close_button.click()
-        self.wait_for_slideout()
-        return self._parent_object(self.testsetup)
-
-
-class Activity_Stream_Button(BaseRegion):
-    '''Region describing the activity stream button'''
-    _root_locator = (By.CSS_SELECTOR, '#stream_btn')
-    _item_class = None  # provided by caller (must be a subclass of Activity_Stream_Region)
-
-    def is_displayed(self):
-        return self._root_element.is_displayed()
-
-    def click(self):
-        self._root_element.click()
-        obj = self._item_class(self.testsetup)
-        obj.wait_for_slidein()
-        return obj
