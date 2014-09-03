bc3e29d2efd6f0966c0d2b462719c77dccd7eaeb
James Laska <jlaska@ansible.com>
2014-09-03 15:52:30 -0400

    Include random unicode in org fixture description

--- tests/lib/fixtures/api/organizations.py
+++ tests/lib/fixtures/api/organizations.py
@@ -4,7 +4,7 @@ import common.utils
 @pytest.fixture(scope="class")
 def organization(request, authtoken, api_organizations_pg):
     payload = dict(name="org-%s" % common.utils.random_unicode(),
-                   description="Random organization",)
+                   description="Random organization - %s" % common.utils.random_unicode())
     obj = api_organizations_pg.post(payload)
     request.addfinalizer(obj.delete)
     return obj
