5a84da3d66a71fa1295ffa92b424729cc7ef5a56
James Laska <jlaska@ansible.com>
2014-09-10 12:11:44 -0400

    Make SearchRegion more generic for other pages

--- tests/lib/common/ui/pages/regions/search.py
+++ tests/lib/common/ui/pages/regions/search.py
@@ -1,41 +1,38 @@
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
 from common.ui.pages import PageRegion
-from common.ui.pages.forms import input_getter, input_setter, Form_Page
+from common.ui.pages.forms import input_getter, input_setter
 from common.ui.pages.regions.buttons import Base_Button, Search_Button, Reset_Button
 from common.ui.pages.regions.lists import List_Region
 
 
 class Search_Region(PageRegion):
     '''Represents the search filter region'''
-    _root_locator = (By.CSS_SELECTOR, "#search-widget-container")
-
-    # Drop-down list
-    _search_type_locator = (By.CSS_SELECTOR, "#search_field_ddown")
-    _search_type_lookup_locator = (By.CSS_SELECTOR, "#organizationSearchDropdown")
+    _root_locator = (By.CSS_SELECTOR, '#search-widget-container')
+    _locators = {
+        'search_type': (By.CSS_SELECTOR, '#search_field_ddown'),
+        'search_type_dropdown': (By.CSS_SELECTOR, 'ul.dropdown-menu'),
+        'search_value': (By.CSS_SELECTOR, '#search_value_input'),
+        'search_btn': (By.CSS_SELECTOR, '#search-submit-button'),
+        'reset_btn': (By.CSS_SELECTOR, '#search-reset-button'),
+    }
 
     # Textbox
-    _search_value_locator = (By.CSS_SELECTOR, "#search_value_input")
-    search_value = property(input_getter(_search_value_locator), input_setter(_search_value_locator))
-
-    # Buttons
-    _search_btn_locator = (By.CSS_SELECTOR, "a.search-reset-start > i.fa-search")  # TODO: change to #search-submit-button
-    _reset_btn_locator = (By.CSS_SELECTOR, "a.search-reset-start > i.fa-times")  # TODO: change to #search-reset-button
+    search_value = property(input_getter(_locators['search_value']), input_setter(_locators['search_value']))
 
     @property
     def search_type(self):
         return Search_DropDown(self.testsetup,
                                _item_class=self._item_class,
-                               _root_element=self.find_element(*self._search_type_locator),
-                               _options_element=self.find_element(*self._search_type_lookup_locator))
+                               _root_element=self.find_element(*self._locators['search_type']),
+                               _options_element=self.find_element(*self._locators['search_type_dropdown']))
 
     @property
     def search_btn(self):
-        return Search_Button(self.testsetup, _root_element=self.find_element(*self._search_btn_locator), _item_class=self._item_class)
+        return Search_Button(self.testsetup, _root_element=self.find_element(*self._locators['search_btn']), _item_class=self._item_class)
 
     @property
     def reset_btn(self):
-        return Reset_Button(self.testsetup, _root_element=self.find_element(*self._reset_btn_locator), _item_class=self._item_class)
+        return Reset_Button(self.testsetup, _root_element=self.find_element(*self._locators['reset_btn']), _item_class=self._item_class)
 
 
 class Search_DropDown(PageRegion):
