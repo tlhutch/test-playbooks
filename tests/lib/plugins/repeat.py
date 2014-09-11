bbe88545a4c864c3a158210a5758ec4a3cd9c993
James Laska <jlaska@ansible.com>
2014-09-11 16:48:40 -0400

    Only use repeat when the option is provided

--- tests/lib/plugins/repeat.py
+++ tests/lib/plugins/repeat.py
@@ -4,9 +4,10 @@ Repeat tests a specified number of times.
 
 
 def pytest_addoption(parser):
-    parser.addoption('--repeat', default=1, type='int', metavar='count', help='Run each test the specified number of times')
+    parser.addoption('--repeat', default=None, type='int', metavar='count', help='Run each test the specified number of times')
 
 
-def pytest_generate_tests (metafunc):
-    for i in range (metafunc.config.option.repeat):
-        metafunc.addcall()
+def pytest_generate_tests(metafunc):
+    if metafunc.config.option.repeat is not None:
+        for i in range (metafunc.config.option.repeat):
+            metafunc.addcall()
