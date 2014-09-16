b94cc80e83b5ded9d0114898e5f9eeae639a394a
James Laska <jlaska@ansible.com>
2014-09-16 15:57:26 -0400

    Surface user and team credential attributes

--- tests/lib/common/api/pages/credentials.py
+++ tests/lib/common/api/pages/credentials.py
@@ -7,6 +7,8 @@ class Credential_Page(base.Base):
     name = property(base.json_getter('name'), base.json_setter('name'))
     description = property(base.json_getter('description'), base.json_setter('description'))
     kind = property(base.json_getter('kind'), base.json_setter('kind'))
+    user = property(base.json_getter('user'), base.json_setter('user'))
+    team = property(base.json_getter('team'), base.json_setter('team'))
 
 class Credentials_Page(Credential_Page, base.Base_List):
     base_url = '/api/v1/credentials/'
