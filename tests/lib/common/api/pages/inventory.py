fadb8a55cd908076f8b1371de5b311ae462ab7ca
James Laska <jlaska@ansible.com>
2014-09-25 09:10:59 -0400

    pep8 fixes

--- tests/lib/common/api/pages/inventory.py
+++ tests/lib/common/api/pages/inventory.py
@@ -1,12 +1,12 @@
-import base
-from common.exceptions import *
+from common.api.pages import Base, Base_List, Task_Page, json_setter, json_getter
 
-class Inventory_Page(base.Base):
+
+class Inventory_Page(Base):
     # FIXME - it would be nice for base_url to always return self.json.url.
     base_url = '/api/v1/inventory/{id}/'
-    name = property(base.json_getter('name'), base.json_setter('name'))
-    description = property(base.json_getter('description'), base.json_setter('description'))
-    variables = property(base.json_getter('variables'), base.json_setter('variables'))
+    name = property(json_getter('name'), json_setter('name'))
+    description = property(json_getter('description'), json_setter('description'))
+    variables = property(json_getter('variables'), json_setter('variables'))
 
     def get_related(self, attr, **kwargs):
         assert attr in self.json['related']
@@ -17,7 +17,7 @@ class Inventory_Page(base.Base):
         elif attr == 'root_groups':
             related = Groups_Page(self.testsetup, base_url=self.json['related'][attr])
         elif attr == 'script':
-            related = base.Base(self.testsetup, base_url=self.json['related'][attr])
+            related = Base(self.testsetup, base_url=self.json['related'][attr])
         else:
             raise NotImplementedError
         return related.get(**kwargs)
@@ -56,24 +56,26 @@ class Inventory_Page(base.Base):
 
         print '\n'.join(output)
 
-class Inventories_Page(Inventory_Page, base.Base_List):
+
+class Inventories_Page(Inventory_Page, Base_List):
     base_url = '/api/v1/inventory/'
 
     def get_related(self, attr, **kwargs):
         assert attr in self.json['related']
         if attr == 'variable_data':
-            related = base.Base(self.testsetup, base_url=self.json['related'][attr])
+            related = Base(self.testsetup, base_url=self.json['related'][attr])
         else:
             raise NotImplementedError
         return related.get(**kwargs)
 
-class Group_Page(base.Base):
+
+class Group_Page(Base):
     # FIXME - it would be nice for base_url to always return self.json.url.
     base_url = '/api/v1/groups/{id}/'
-    name = property(base.json_getter('name'), base.json_setter('name'))
-    description = property(base.json_getter('description'), base.json_setter('description'))
-    inventory = property(base.json_getter('inventory'), base.json_setter('inventory'))
-    variables = property(base.json_getter('variables'), base.json_setter('variables'))
+    name = property(json_getter('name'), json_setter('name'))
+    description = property(json_getter('description'), json_setter('description'))
+    inventory = property(json_getter('inventory'), json_setter('inventory'))
+    variables = property(json_getter('variables'), json_setter('variables'))
 
     def get_related(self, attr, **kwargs):
         assert attr in self.json['related']
@@ -88,7 +90,7 @@ class Group_Page(base.Base):
         elif attr == 'children':
             related = Groups_Page(self.testsetup, base_url=self.json['related'][attr])
         elif attr == 'variable_data':
-            related = base.Base(self.testsetup, base_url=self.json['related'][attr])
+            related = Base(self.testsetup, base_url=self.json['related'][attr])
         else:
             raise NotImplementedError
         return related.get(**kwargs)
@@ -111,21 +113,23 @@ class Group_Page(base.Base):
                 parents.append(candidate.id)
         return parents
 
-class Groups_Page(Group_Page, base.Base_List):
+
+class Groups_Page(Group_Page, Base_List):
     base_url = '/api/v1/groups/'
 
-class Host_Page(base.Base):
+
+class Host_Page(Base):
     # FIXME - it would be nice for base_url to always return self.json.url.
     base_url = '/api/v1/hosts/{id}/'
-    name = property(base.json_getter('name'), base.json_setter('name'))
-    description = property(base.json_getter('description'), base.json_setter('description'))
-    inventory = property(base.json_getter('inventory'), base.json_setter('inventory'))
-    variables = property(base.json_getter('variables'), base.json_setter('variables'))
+    name = property(json_getter('name'), json_setter('name'))
+    description = property(json_getter('description'), json_setter('description'))
+    inventory = property(json_getter('inventory'), json_setter('inventory'))
+    variables = property(json_getter('variables'), json_setter('variables'))
 
     def get_related(self, attr, **kwargs):
         assert attr in self.json['related']
         if attr == 'variable_data':
-            related = base.Base(self.testsetup, base_url=self.json['related'][attr])
+            related = Base(self.testsetup, base_url=self.json['related'][attr])
         elif attr == 'inventory':
             related = Inventory_Page(self.testsetup, base_url=self.json['related'][attr])
         elif attr == 'groups':
@@ -134,22 +138,24 @@ class Host_Page(base.Base):
             raise NotImplementedError
         return related.get(**kwargs)
 
-class Hosts_Page(Host_Page, base.Base_List):
+
+class Hosts_Page(Host_Page, Base_List):
     base_url = '/api/v1/hosts/'
 
-class Inventory_Source_Page(base.Base):
+
+class Inventory_Source_Page(Base):
     # FIXME - it would be nice for base_url to always return self.json.url.
     base_url = '/api/v1/inventory_sources/{id}/'
-    name = property(base.json_getter('name'), base.json_setter('name'))
-    source = property(base.json_getter('source'), base.json_setter('source'))
-    status = property(base.json_getter('status'), base.json_setter('status'))
-    description = property(base.json_getter('description'), base.json_setter('description'))
-    last_updated = property(base.json_getter('last_updated'), base.json_setter('last_updated'))
-    last_update_failed = property(base.json_getter('last_update_failed'), base.json_setter('last_update_failed'))
-    last_job_run = property(base.json_getter('last_job_run'), base.json_setter('last_job_run'))
-    update_cache_timeout = property(base.json_getter('update_cache_timeout'), base.json_setter('update_cache_timeout'))
-    update_on_launch = property(base.json_getter('update_on_launch'), base.json_setter('update_on_launch'))
-    inventory = property(base.json_getter('inventory'), base.json_setter('inventory'))
+    name = property(json_getter('name'), json_setter('name'))
+    source = property(json_getter('source'), json_setter('source'))
+    status = property(json_getter('status'), json_setter('status'))
+    description = property(json_getter('description'), json_setter('description'))
+    last_updated = property(json_getter('last_updated'), json_setter('last_updated'))
+    last_update_failed = property(json_getter('last_update_failed'), json_setter('last_update_failed'))
+    last_job_run = property(json_getter('last_job_run'), json_setter('last_job_run'))
+    update_cache_timeout = property(json_getter('update_cache_timeout'), json_setter('update_cache_timeout'))
+    update_on_launch = property(json_getter('update_on_launch'), json_setter('update_on_launch'))
+    inventory = property(json_getter('inventory'), json_setter('inventory'))
 
     def get_related(self, attr, **kwargs):
         assert attr in self.json['related']
@@ -161,7 +167,7 @@ class Inventory_Source_Page(base.Base):
             related = Inventory_Updates_Page(self.testsetup, base_url=self.json['related'][attr])
         elif attr == 'update':
             # FIXME - this should have it's own object
-            related = base.Base(self.testsetup, base_url=self.json['related'][attr])
+            related = Base(self.testsetup, base_url=self.json['related'][attr])
         elif attr == 'schedules':
             from schedules import Schedules_Page
             related = Schedules_Page(self.testsetup, base_url=self.json['related'][attr])
@@ -182,11 +188,14 @@ class Inventory_Source_Page(base.Base):
             not self.last_update_failed and \
             self.last_updated is not None
 
-class Inventory_Sources_Page(Inventory_Source_Page, base.Base_List):
+
+class Inventory_Sources_Page(Inventory_Source_Page, Base_List):
     base_url = '/api/v1/inventory_sources/'
 
-class Inventory_Update_Page(base.Task_Page):
+
+class Inventory_Update_Page(Task_Page):
     base_url = '/api/v1/inventory_updates/{id}/'
 
-class Inventory_Updates_Page(Inventory_Update_Page, base.Base_List):
+
+class Inventory_Updates_Page(Inventory_Update_Page, Base_List):
     base_url = '/api/v1/inventory_sources/{inventory_source}/inventory_updates/'
