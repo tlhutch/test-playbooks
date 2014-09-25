47a7cfc6f8187a35803ae5916d7999072f60acb6
James Laska <jlaska@ansible.com>
2014-09-25 15:36:19 -0400

    Add test_projects
    Currently verifies...
     * expected responses when accessing 'related' on a deleted project
     * project_updates are canceled when a project is deleted

---
+++ tests/api/test_projects.py
@@ -0,0 +1,69 @@
+'''
+# Create projects in /var/lib/awx/projects and verify
+# 1) projects starting with '.' or '_' are excluded from config['project_local_paths']
+# 2) project appears under config['project_local_paths']
+'''
+
+import re
+import pytest
+import common.utils
+import common.exceptions
+from tests.api import Base_Api_Test
+
+
+@pytest.mark.api
+@pytest.mark.destructive
+@pytest.mark.skip_selenium
+class Test_Projects(Base_Api_Test):
+    '''
+    Verify various activities on /projects endpoint
+    '''
+    pytestmark = pytest.mark.usefixtures('authtoken')
+
+    def test_delete_related_fields(self, project_ansible_playbooks_git):
+        '''Verify that related fields on a deleted resource respond as expected'''
+
+        # delete all the projects
+        project_ansible_playbooks_git.delete()
+
+        # assert some related resources are okay
+        for related in ('last_job', 'last_update'):
+            assert project_ansible_playbooks_git.get_related(related)
+
+        # assert specific related resources are forbidden (403)
+        for related in ('schedules', 'activity_stream', 'project_updates', 'teams', ):
+            print related
+            with pytest.raises(common.exceptions.Forbidden_Exception):
+                assert project_ansible_playbooks_git.get_related(related)
+
+        # assert specific related resources are notfound (404)
+        for related in ('playbooks', 'update'):
+            print related
+            with pytest.raises(common.exceptions.NotFound_Exception):
+                assert project_ansible_playbooks_git.get_related(related)
+
+    def test_cancel_on_delete(self, project_ansible_playbooks_git_nowait, api_unified_jobs_pg, ansible_runner):
+        '''Verify that queued project_updates are canceled when a project is deleted.
+
+        This test is kind of ugly since we call out to 'tower-manage shell' to
+        determine whether queued jobs persist.  We do this the project is
+        marked as active=False, and therefore not visible through the API.
+        '''
+
+        # delete all the projects
+        project_ansible_playbooks_git_nowait.delete()
+
+        # assert no unified_jobs remain running
+        result = ansible_runner.shell(
+            "echo \"from awx.main.models import *; "
+            "print UnifiedJob.objects.filter(status__in=['running','waiting','pending'], unified_job_template=%d).count();\" "
+            "| tower-manage shell" % project_ansible_playbooks_git_nowait.id
+        )
+        assert 'stdout' in result, "Unexpected response from ansible_runner.shell"
+        match = re.search(r'>>> (\d)\n', result['stdout'], re.MULTILINE)
+        if match is None or not match.group(1).isdigit():
+            raise Exception("Unhandled response from tower-manage: %s" % result['stdout'])
+        num_pending = match.group(1)
+        assert int(num_pending) == 0, \
+            "A project (id:%d) was deleted, but a project_update (%s) remains queued/waiting/running" % \
+            (project_ansible_playbooks_git_nowait.id, num_pending)
