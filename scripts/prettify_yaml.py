e2f2f9ef0fead518de23184128026b04474a1d90
James Laska <jlaska@ansible.com>
2014-10-03 14:59:17 -0400

    Convenience script to prettify YAML files

---
+++ scripts/prettify_yaml.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+'''
+Prettify a YAML document
+'''
+
+import yaml
+import sys
+import os
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        print "Usage: %s [<filename>, ...]" % sys.argv[0]
+        sys.exit(1)
+
+    for filename in sys.argv[1:]:
+        assert os.path.isfile(filename)
+        assert os.path.splitext(filename)[1] in ['.yaml', '.yml']
+
+        # open for read
+        stream = file(filename, 'r')
+        data = yaml.load(stream)
+        stream.close()
+
+        print yaml.dump(data)
+        yesno = raw_input("\nSave changes? [y|n] ").lower() == 'y'
+        if yesno:
+            stream = file(filename, 'w')
+            stream.write(yaml.dump(data))
+            stream.close()
