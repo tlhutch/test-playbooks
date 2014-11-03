489623c50ef835902f3b1fa53ac062ea51536b8d
James Laska <jlaska@ansible.com>
2014-11-03 11:24:07 -0500

    Remove endpoint_visibility test
    The /api/v1/system_job{s,_templates} endpoint is always visible,
    regardless of which user has authenticated.

--- tests/api/test_system_job_templates.py
+++ tests/api/test_system_job_templates.py
@@ -58,22 +58,6 @@ class Test_System_Job_Template(Base_Api_Test):
                 with pytest.raises(common.exceptions.NotFound_Exception):
                     api_system_job_templates_pg.get()
 
-    def test_endpoint_visibility(self, non_superusers, user_password, api_v1_pg, api_system_job_templates_url):
-        '''
-        Verify endpoint /api/v1/system_job_templates visibility for various
-        user accounts
-        '''
-
-        assert 'system_job_templates' in api_v1_pg.json, "Superuser " \
-            "account unable to see endpoint %s" % \
-            api_system_job_templates_url
-
-        for non_superuser in non_superusers:
-            with self.current_user(non_superuser.username, user_password):
-                assert 'system_job_templates' not in api_v1_pg.json, "Non-superuser " \
-                    "account is able to see endpoint %s" % \
-                    api_system_job_templates_url
-
     def test_method_not_allowed(self, api_system_job_templates_pg):
         '''
         Verify that PUT, POST and PATCH are unsupported request methods
