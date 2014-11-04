b78266714e45c5880287b4a18b64a71868ec686c
James Laska <jlaska@ansible.com>
2014-11-04 10:38:22 -0500

    Update test_dashboard for Portal Mode link

--- tests/ui/test_dashboard.py
+++ tests/ui/test_dashboard.py
@@ -42,7 +42,8 @@ class Test_Dashboard(Base_UI_Test):
 
         ui_dashboard_pg.account_menu.show()
         actual_items = ui_dashboard_pg.account_menu.keys()
-        expected_items = [u'About Tower', u'Account Settings', u'View License', u'Contact Support', u'Monitor Tower', u'Logout']
+        expected_items = [u'About Tower', u'Account Settings', u'Contact Support', u'Monitor Tower', u'Portal Mode', u'View License', u'Logout']
+
         assert actual_items == expected_items, "Missing expected menu items (%s != %s)" % (actual_items, expected_items)
 
     def test_account_menu_non_superuser(self, ui_dashboard_pg, anonymous_user, admin_user, user_password):
@@ -54,7 +55,8 @@ class Test_Dashboard(Base_UI_Test):
 
             ui_dashboard_pg.account_menu.show()
             actual_items = ui_dashboard_pg.account_menu.keys()
-            expected_items = [u'About Tower', u'Account Settings', u'View License', u'Contact Support', u'Logout']
+            expected_items = [u'About Tower', u'Account Settings', u'Contact Support', u'Portal Mode', u'View License', u'Logout']
+
             assert actual_items == expected_items, "Missing expected menu items (%s != %s)" % (actual_items, expected_items)
 
     def test_activity_stream(self, ui_dashboard_pg):
