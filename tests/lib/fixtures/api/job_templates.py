104b029a02bde3f264fd3dc8e7fee865789548ca
James Laska <jlaska@ansible.com>
2014-11-25 16:31:35 -0500

    Add fixture job_template_multi_ask

--- tests/lib/fixtures/api/job_templates.py
+++ tests/lib/fixtures/api/job_templates.py
@@ -53,6 +53,21 @@ def job_template_ask(request, authtoken, api_job_templates_pg, project, inventor
 
 
 @pytest.fixture(scope="function")
+def job_template_multi_ask(request, authtoken, api_job_templates_pg, project, inventory, ssh_credential_multi_ask):
+    '''Create a job_template with a valid machine credential, but a limit parameter that matches nothing'''
+    payload = dict(name="job_template-%s" % common.utils.random_unicode(),
+                   description="Random job_template with multiple ASK credential - %s" % common.utils.random_unicode(),
+                   inventory=inventory.id,
+                   job_type='run',
+                   project=project.id,
+                   credential=ssh_credential_multi_ask.id,
+                   playbook='site.yml', )  # This depends on the project selected
+    obj = api_job_templates_pg.post(payload)
+    request.addfinalizer(obj.delete)
+    return obj
+
+
+@pytest.fixture(scope="function")
 def job_template_ansible_playbooks_git(request, authtoken, api_job_templates_pg, project_ansible_playbooks_git, inventory, ssh_credential):
     '''Define a job_template with a valid machine credential'''
 
