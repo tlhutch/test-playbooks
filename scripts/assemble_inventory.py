4571eaa42763f19b93783582d9e5f2a5a8a152e8
James Laska <jlaska@ansible.com>
2014-12-04 09:58:18 -0500

    Add optparse support for jenkins user/token

--- scripts/assemble_inventory.py
+++ scripts/assemble_inventory.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
+import os
 import sys
 import requests
 import StringIO
 import ConfigParser
+import optparse
+from urlparse import urljoin
+
 
 class AnsibleInventory(ConfigParser.SafeConfigParser):
     def write(self, fp):
         """Write an .ini-format representation of the configuration state."""
         if self._defaults:
-            fp.write("[%s]\n" % DEFAULTSECT)
+            fp.write("[%s]\n" % self.DEFAULTSECT)
             for (key, value) in self._defaults.items():
                 fp.write("%s=%s\n" % (key, str(value).replace('\n', '\n\t')))
             fp.write("\n")
@@ -23,27 +27,57 @@ class AnsibleInventory(ConfigParser.SafeConfigParser):
                 fp.write("%s\n" % (key))
             fp.write("\n")
 
-user = 'jlaska'
-password = 'd6733c68ed00072ee47822c16af14662'
-base_url = 'http://50.116.42.103/'
-job_path = 'job/Test_Tower_Install/CLOUD_PROVIDER={cloud_provider},PLATFORM={platform},label={label}/lastBuild/'
-artifact_path = 'artifact/playbooks/inventory.log/*view*/'
 
-cfg = AnsibleInventory()
+def parse_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-j", "--jenkins", dest="jenkins",
+                      default=os.getenv('JENKINS_URL', 'http://50.116.42.103'),
+                      help="URL to Jenkins (default: %default).")
+    parser.add_option("-u", "--user", dest="user",
+                      default=os.getenv('JENKINS_USER', os.getenv('USER')),
+                      help="Jenkins username (default: %default).")
+    parser.add_option("-t", "--token", dest="token",
+                      default=os.getenv('JENKINS_TOKEN'),
+                      help="Jenkins API-Token.")
 
-for cloud_provider in ['rax', 'ec2']:
-    for platform in ['rhel-6.5-x86_64', 'centos-6.5-x86_64', 'centos-7.0-x86_64', 'rhel-7.0-x86_64', 'ubuntu-12.04-x86_64', 'ubuntu-14.04-x86_64']:
-        for label in ['test']:
-            url = base_url + job_path + artifact_path
-            url = url.format(**dict(cloud_provider=cloud_provider, platform=platform, label=label))
-            r = requests.get(url, verify=False, auth=(user, password))
-            try:
-                cfg.readfp(StringIO.StringIO(r.text))
-            except (ConfigParser.MissingSectionHeaderError, ConfigParser.ParsingError) as e:
-                sys.stderr.write("Failed to download inventory.log: %s\n" % url)
-                continue
+    (opts, args) = parser.parse_args()
 
-if cfg.sections():
-    cfg.write(sys.stdout)
-else:
-    print "No usable inventory found."
+    if opts.jenkins is None or opts.jenkins == "":
+        parser.error("Must specify [-j|--jenkins].")
+    if opts.user is None or opts.user == "":
+        parser.error("Must specify [-u|--user].")
+    if opts.token is None or opts.token == "":
+        parser.error("Must specify [-t|--token].")
+
+    return (opts, args)
+
+
+job_path = '/job/Test_Tower_Install/CLOUD_PROVIDER={cloud_provider},PLATFORM={platform},label={label}/lastBuild'
+artifact_path = '/artifact/playbooks/inventory.log/*view*/'
+supported_cloud_providers = ['rax', 'ec2']
+supported_platforms = ['rhel-6.5-x86_64', 'centos-6.5-x86_64',
+                       'centos-7.0-x86_64', 'rhel-7.0-x86_64',
+                       'ubuntu-12.04-x86_64', 'ubuntu-14.04-x86_64']
+
+
+if __name__ == "__main__":
+    (opts, args) = parse_args()
+
+    cfg = AnsibleInventory()
+
+    for cloud_provider in supported_cloud_providers:
+        for platform in supported_platforms:
+            for label in ['test']:
+                url = urljoin(opts.jenkins, job_path + artifact_path)
+                url = url.format(**dict(cloud_provider=cloud_provider, platform=platform, label=label))
+                r = requests.get(url, verify=False, auth=(opts.user, opts.token))
+                try:
+                    cfg.readfp(StringIO.StringIO(r.text))
+                except (ConfigParser.MissingSectionHeaderError, ConfigParser.ParsingError) as e:
+                    sys.stderr.write("Failed to download inventory.log: %s\n" % url)
+                    continue
+
+    if cfg.sections():
+        cfg.write(sys.stdout)
+    else:
+        print "No usable inventory found."
