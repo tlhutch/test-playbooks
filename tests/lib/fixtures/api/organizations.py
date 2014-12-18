b481161bdf9486a0ddc9165a521bcdab524c096d
James Laska <jlaska@ansible.com>
2014-12-17 20:02:18 -0500

    Quietly delete org fixtures

--- tests/lib/fixtures/api/organizations.py
+++ tests/lib/fixtures/api/organizations.py
@@ -7,7 +7,7 @@ def organization(request, authtoken, api_organizations_pg):
     payload = dict(name="org-%s" % common.utils.random_unicode(),
                    description="Random organization - %s" % common.utils.random_unicode())
     obj = api_organizations_pg.post(payload)
-    request.addfinalizer(obj.delete)
+    request.addfinalizer(obj.silent_delete)
     return obj
 
 
@@ -16,7 +16,7 @@ def another_organization(request, authtoken, api_organizations_pg):
     payload = dict(name="org-%s" % common.utils.random_unicode(),
                    description="Another random organization - %s" % common.utils.random_unicode())
     obj = api_organizations_pg.post(payload)
-    request.addfinalizer(obj.delete)
+    request.addfinalizer(obj.silent_delete)
     return obj
 
 
@@ -28,6 +28,6 @@ def many_organizations(request, authtoken, api_organizations_pg):
         payload = dict(name="%s random %s organization" % (i, common.utils.random_unicode()),
                        description="Random organization %s %s" % (i, common.utils.random_unicode()))
         obj = api_organizations_pg.post(payload)
-        request.addfinalizer(obj.delete)
+        request.addfinalizer(obj.silent_delete)
         obj_list.append(obj)
     return obj_list
