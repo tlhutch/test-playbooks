a511bc032c2177cdd0c3261d4227bafb81664d89
James Laska <jlaska@ansible.com>
2014-12-19 11:44:38 -0500

    Improve change detection

--- tests/lib/fixtures/api/config.py
+++ tests/lib/fixtures/api/config.py
@@ -15,20 +15,21 @@ def AWX_PROOT_ENABLED(request, ansible_runner, tower_settings_path):
     # update settings.py
     result = ansible_runner.lineinfile(state='present', dest=tower_settings_path, regexp='^\s*AWX_PROOT_ENABLED\s*=', line='AWX_PROOT_ENABLED = True')
     assert 'failed' not in result, "Failure while setting AWX_PROOT_ENABLED\n%s" % json.dumps(result, indent=2)
+    changed = 'changed' in result and result['changed']
 
     # restart ansible-tower (if changes were necesary)
-    if result['changed']:
+    if changed:
         result = ansible_runner.service(name='ansible-tower', state='restarted')
         assert 'failed' not in result, "Failure restarting ansible-tower\n%s" % json.dumps(result, indent=2)
 
     def fin():
         # restore settings.py (if changes were necesary)
-        if result['changed']:
+        if changed:
             result = ansible_runner.lineinfile(state='absent', dest=tower_settings_path, regexp='^\s*AWX_PROOT_ENABLED\s*=')
             assert 'failed' not in result, "Failure while removing AWX_PROOT_ENABLED\n%s" % json.dumps(result, indent=2)
 
         # restart ansible-tower (if changes were necesary)
-        if result['changed']:
+        if changed:
             result = ansible_runner.service(name='ansible-tower', state='restarted')
             assert 'failed' not in result, "Failure restarting ansible-tower\n%s" % json.dumps(result, indent=2)
     request.addfinalizer(fin)
@@ -39,20 +40,21 @@ def ORG_ADMINS_CANNOT_SEE_ALL_USERS(request, ansible_runner, tower_settings_path
     # update settings.py
     result = ansible_runner.lineinfile(state='present', dest=tower_settings_path, regexp='^\s*ORG_ADMINS_CAN_SEE_ALL_USERS\s*=', line='ORG_ADMINS_CAN_SEE_ALL_USERS = False')
     assert 'failed' not in result, "Failure while setting ORG_ADMINS_CAN_SEE_ALL_USERS\n%s" % json.dumps(result, indent=2)
+    changed = 'changed' in result and result['changed']
 
     # restart ansible-tower (if changes were necesary)
-    if result['changed']:
+    if changed:
         result = ansible_runner.service(name='ansible-tower', state='restarted')
         assert 'failed' not in result, "Failure restarting ansible-tower\n%s" % json.dumps(result, indent=2)
 
     def fin():
         # restore settings.py (if changes were necesary)
-        if result['changed']:
+        if changed:
             result = ansible_runner.lineinfile(state='absent', dest=tower_settings_path, regexp='^\s*ORG_ADMINS_CAN_SEE_ALL_USERS\s*=')
             assert 'failed' not in result, "Failure while removing ORG_ADMINS_CAN_SEE_ALL_USERS\n%s" % json.dumps(result, indent=2)
 
         # restart ansible-tower (if changes were necesary)
-        if result['changed']:
+        if changed:
             result = ansible_runner.service(name='ansible-tower', state='restarted')
             assert 'failed' not in result, "Failure restarting ansible-tower\n%s" % json.dumps(result, indent=2)
     request.addfinalizer(fin)
