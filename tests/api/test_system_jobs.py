e4c79f2d4118ac7d3a0a6b5ce1f3bcc379715dda
James Laska <jlaska@ansible.com>
2015-01-26 10:48:36 -0500

    Stub out test_system_jobs

---
+++ tests/api/test_system_jobs.py
@@ -0,0 +1,105 @@
+import json
+import pytest
+import common.tower.inventory
+import common.utils
+from tests.api import Base_Api_Test
+
+
+@pytest.fixture(scope="function")
+def cleanup_jobs_template(request, api_system_job_templates_pg):
+    '''
+    Return a System_Job_Template object representing the 'cleanup_jobs' system
+    job template.
+    '''
+    matches = api_system_job_templates_pg.get(job_type='cleanup_jobs')
+    assert matches.count == 1, "Unexpected number of results (%s) when querying " \
+        "for system_job_template job_type:cleanup_jobs" % matches.count
+    return matches.results[0]
+
+
+@pytest.fixture(scope="function", params=['cleanup_jobs', 'cleanup_deleted', 'cleanup_activitystream'])
+def system_job_template(request, api_system_job_templates_pg):
+    matches = api_system_job_templates_pg.get(job_type=request.param)
+    assert matches.count == 1, "No matching system_job_template found with job_type=%s" % request.param
+    return matches.results[0]
+
+
+@pytest.mark.api
+@pytest.mark.skip_selenium
+@pytest.mark.destructive
+@pytest.mark.skipif(True, reason="not yet implemented")
+class Test_System_Jobs(Base_Api_Test):
+    '''
+    Verify actions with system_job_templates
+    '''
+
+    pytestmark = pytest.mark.usefixtures('authtoken')
+
+    def test_get_as_superuser(self, api_system_job_templates_pg):
+        '''
+        Verify that a superuser account is able to GET the system_jobs
+        resource.
+        '''
+        results = api_system_job_templates_pg.get()
+        # NOTE: validation on the system_job_template name+description happens
+        # during JSON schema validation
+        assert results.count > 0, "Unexpected system_job_template count (%s)" % \
+            results.count
+
+    def test_delete_as_superuser(self, api_system_job_templates_pg):
+        '''
+        Verify that a superuser account is able to GET the system_jobs
+        resource.
+        '''
+        results = api_system_job_templates_pg.get()
+        # NOTE: validation on the system_job_template name+description happens
+        # during JSON schema validation
+        assert results.count > 0, "Unexpected system_job_template count (%s)" % \
+            results.count
+
+    def test_get_as_non_superuser(self, api_system_job_templates_pg, non_superusers, user_password):
+        '''
+        Verify that non-superuser accounts are unable to access the
+        system_job_template endpoint
+        '''
+        for non_superuser in non_superusers:
+            with self.current_user(non_superuser.username, user_password):
+                with pytest.raises(common.exceptions.NotFound_Exception):
+                    api_system_job_templates_pg.get()
+
+    def test_method_not_allowed(self, api_system_job_templates_pg):
+        '''
+        Verify that PUT, POST and PATCH are unsupported request methods
+        '''
+        with pytest.raises(common.exceptions.Method_Not_Allowed_Exception):
+            api_system_job_templates_pg.post()
+
+        system_job_template = api_system_job_templates_pg.get(id=1)
+        with pytest.raises(common.exceptions.Method_Not_Allowed_Exception):
+            system_job_template.put()
+
+        with pytest.raises(common.exceptions.Method_Not_Allowed_Exception):
+            system_job_template.patch()
+
+    def test_relaunch_as_superuser(self, system_job_template):
+        '''
+        Verify successful launch of a system_job_template
+        '''
+
+        result = system_job_template.get_related('launch').post()
+        assert 'system_job' in result.json, "Unexpected JSON response when " \
+            "launching system_job_template\n%s" % json.dumps(result.json, indent=2)
+
+        job_pg = system_job_template.get_related('jobs', id=result.json['system_job']).results[0].wait_until_completed()
+        assert job_pg.is_successful, job_pg
+
+    def test_relaunch_as_non_superuser(self, system_job_template, non_superusers, user_password):
+        '''
+        Verify launch fails when attempted by a non-superuser
+        '''
+
+        launch_pg = system_job_template.get_related('launch')
+        for non_superuser in non_superusers:
+            with self.current_user(non_superuser.username, user_password):
+                with pytest.raises(common.exceptions.Forbidden_Exception):
+                    launch_pg.post()
