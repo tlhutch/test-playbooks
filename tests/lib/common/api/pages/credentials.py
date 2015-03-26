a6ff53968523bba831def4f6bf4c0f71700f4820
James Laska <jlaska@ansible.com>
2015-03-26 16:42:00 -0400

    Expose credential attributes

--- tests/lib/common/api/pages/credentials.py
+++ tests/lib/common/api/pages/credentials.py
@@ -9,6 +9,10 @@ class Credential_Page(base.Base):
     kind = property(base.json_getter('kind'), base.json_setter('kind'))
     user = property(base.json_getter('user'), base.json_setter('user'))
     team = property(base.json_getter('team'), base.json_setter('team'))
+    su_username = property(base.json_getter('su_username'), base.json_setter('su_username'))
+    su_password = property(base.json_getter('su_password'), base.json_setter('su_password'))
+    sudo_username = property(base.json_getter('sudo_username'), base.json_setter('sudo_username'))
+    sudo_password = property(base.json_getter('sudo_password'), base.json_setter('sudo_password'))
 
 
 class Credentials_Page(Credential_Page, base.Base_List):
