191cd99d1e6c8bb66ac8b47b94afe8fcfcd83c58
James Laska <jlaska@ansible.com>
2015-04-21 15:36:20 -0400

    Add AD_HOC_COMMANDS class-scoped fixture

--- tests/lib/fixtures/license.py
+++ tests/lib/fixtures/license.py
@@ -9,11 +9,6 @@ log = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope='session')
-def tower_config_dir(request):
-    return '/etc/tower'
-
-
-@pytest.fixture(scope='session')
 def tower_license_path(request, tower_config_dir):
     return os.path.join(tower_config_dir, 'license')
 
