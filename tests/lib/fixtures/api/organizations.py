7ef64f79a87d90fc3d707ed922b9ad1ce05efaab
James Laska <jlaska@ansible.com>
2015-05-31 13:31:11 -0400

    default_organization fixture should never create
    The default_organization fixture will only return the default organization.  It
    will assert if no such organization exists.

--- tests/lib/fixtures/api/organizations.py
+++ tests/lib/fixtures/api/organizations.py
@@ -5,22 +5,17 @@ import fauxfactory
 @pytest.fixture(scope="function")
 def default_organization(authtoken, api_organizations_pg):
     match = api_organizations_pg.get(name='Default')
-    if match.count == 1:
-        return match.results.pop()
-    else:
-        return None
+    assert match.count == 1, "No default organization found (%s)" % match.count
+    return match.results.pop()
 
 
 @pytest.fixture(scope="function")
-def organization(request, authtoken, api_organizations_pg, default_organization):
-    if default_organization is not None:
-        return default_organization
-    else:
-        payload = dict(name="Default Organization %s" % fauxfactory.gen_utf8(),
-                       description="Default organization - %s" % fauxfactory.gen_utf8())
-        obj = api_organizations_pg.post(payload)
-        request.addfinalizer(obj.silent_delete)
-        return obj
+def organization(request, authtoken, api_organizations_pg):
+    payload = dict(name="Random organization %s" % fauxfactory.gen_utf8(),
+                   description="Random organization - %s" % fauxfactory.gen_utf8())
+    obj = api_organizations_pg.post(payload)
+    request.addfinalizer(obj.silent_delete)
+    return obj
 
 
 @pytest.fixture(scope="function")
@@ -34,7 +29,6 @@ def another_organization(request, authtoken, api_organizations_pg):
 
 @pytest.fixture(scope="function")
 def many_organizations(request, authtoken, api_organizations_pg):
-
     obj_list = list()
     for i in range(55):
         payload = dict(name="%s random %s organization" % (i, fauxfactory.gen_utf8()),
