d840c0508b34a5c79bab7c65c0b7f26eb3727874
James Laska <jlaska@ansible.com>
2015-06-07 09:00:29 -0400

    Improve debugging of empty json responses

--- tests/lib/common/api/pages/base.py
+++ tests/lib/common/api/pages/base.py
@@ -65,7 +65,7 @@ class Base(Page):
             data = r.json()
         except ValueError, e:
             '''If there was no json to parse'''
-            log.warn("Unable to parse JSON response: %s - %s" % (e, r.text))
+            log.warn("Unable to parse JSON response (%s): %s - '%s'" % (r.status_code, e, r.text))
             data = dict()
 
         exc_str = "%s (%s) received" % (httplib.responses[r.status_code], r.status_code)
