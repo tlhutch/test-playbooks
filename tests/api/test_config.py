b952477d708bd39d6c0030eae64b1e40fd065286
James Laska <jlaska@ansible.com>
2015-06-12 16:13:58 -0400

    User current_user fixture

--- tests/api/test_config.py
+++ tests/api/test_config.py
@@ -10,20 +10,20 @@ class Test_Config(Base_Api_Test):
     '''
     pytestmark = pytest.mark.usefixtures('authtoken', 'install_enterprise_license_unlimited')
 
-    def test_privileged_user(self, api_config_pg, privileged_user, user_password):
+    def test_privileged_user(self, api_config_pg, privileged_user, user_password, current_user):
         '''Verify the project_local_paths and project_base_dir fields are present'''
         if privileged_user.username == 'admin':
             user_password = self.credentials['default']['password']
 
-        with self.current_user(privileged_user.username, user_password):
+        with current_user(username=privileged_user.username, password=user_password):
             conf = api_config_pg.get()
             assert 'project_local_paths' in conf.json
             assert 'project_base_dir' in conf.json
 
-    def test_unprivileged_user(self, api_config_pg, unprivileged_user, user_password):
+    def test_unprivileged_user(self, api_config_pg, unprivileged_user, user_password, current_user):
         '''Verify the project_local_paths and project_base_dir fields are absent'''
 
-        with self.current_user(unprivileged_user.username, user_password):
+        with current_user(username=unprivileged_user.username, password=user_password):
             conf = api_config_pg.get()
             assert 'project_local_paths' not in conf.json
             assert 'project_base_dir' not in conf.json
