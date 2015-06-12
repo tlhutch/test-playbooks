3ac887cecb8de45a87f1e4e4a13b53cac4b39f5d
James Laska <jlaska@ansible.com>
2015-06-12 16:07:02 -0400

    Properly assign the project to an organization
    The previous method didn't actually do anything.

--- tests/lib/fixtures/api/projects.py
+++ tests/lib/fixtures/api/projects.py
@@ -22,17 +22,16 @@ def project_status_choices(request, authtoken, api_projects_pg):
 
 
 @pytest.fixture(scope="function")
-def project_ansible_helloworld_hg(request, authtoken, api_projects_pg, organization):
+def project_ansible_helloworld_hg(request, authtoken, organization):
     # Create project
     payload = dict(name="project-%s" % fauxfactory.gen_utf8(),
-                   organization=organization.id,
                    scm_type='hg',
                    scm_url='https://bitbucket.org/jlaska/ansible-helloworld',
                    scm_clean=False,
                    scm_delete_on_update=False,
                    scm_update_on_launch=False,)
 
-    obj = api_projects_pg.post(payload)
+    obj = organization.get_related('projects').post(payload)
     request.addfinalizer(obj.delete)
 
     # Wait for project update to complete
@@ -53,7 +52,7 @@ def project_update_with_status_completed(request, project_ansible_playbooks_git)
 
 
 @pytest.fixture(scope="function")
-def project_ansible_playbooks_manual(request, authtoken, ansible_runner, awx_config, api_projects_pg, organization):
+def project_ansible_playbooks_manual(request, authtoken, ansible_runner, awx_config, organization):
     '''
     Create a manual project associated with ansible-playbooks.git
     '''
@@ -74,7 +73,6 @@ def project_ansible_playbooks_manual(request, authtoken, ansible_runner, awx_con
     # Initialize the project payload
     payload = dict(name="ansible-playbooks.manual - %s" % local_path,
                    description="manual project - %s" % fauxfactory.gen_utf8(),
-                   organization=organization.id,
                    local_path=local_path,
                    scm_type='')
 
@@ -83,7 +81,7 @@ def project_ansible_playbooks_manual(request, authtoken, ansible_runner, awx_con
         payload.update(fixture_args.kwargs)
 
     try:
-        obj = api_projects_pg.post(payload)
+        obj = organization.get_related('projects').post(payload)
     except common.exceptions.Duplicate_Exception:
         log.debug("POST failed - %s" % json.dumps(payload, indent=2))
         raise
@@ -104,16 +102,15 @@ def project_manual(request, project_ansible_playbooks_manual):
 
 
 @pytest.fixture(scope="function")
-def project_ansible_playbooks_git_nowait(request, authtoken, api_projects_pg, organization):
+def project_ansible_playbooks_git_nowait(request, authtoken, organization):
     # Create project
     payload = dict(name="ansible-playbooks.git - %s" % fauxfactory.gen_utf8(),
-                   organization=organization.id,
                    scm_type='git',
                    scm_url='https://github.com/jlaska/ansible-playbooks.git',
                    scm_clean=False,
                    scm_delete_on_update=False,
                    scm_update_on_launch=False,)
-    obj = api_projects_pg.post(payload)
+    obj = organization.get_related('projects').post(payload)
     request.addfinalizer(obj.silent_delete)
     return obj
 
@@ -130,16 +127,15 @@ def project_ansible_playbooks_git(request, project_ansible_playbooks_git_nowait)
 
 
 @pytest.fixture(scope="function")
-def project_ansible_git_nowait(request, authtoken, api_projects_pg, organization):
+def project_ansible_git_nowait(request, authtoken, organization):
     # Create project
     payload = dict(name="ansible-playbooks.git - %s" % fauxfactory.gen_utf8(),
-                   organization=organization.id,
                    scm_type='git',
                    scm_url='https://github.com/ansible/ansible.git',
                    scm_clean=False,
                    scm_delete_on_update=False,
                    scm_update_on_launch=False,)
-    obj = api_projects_pg.post(payload)
+    obj = organization.get_related('projects').post(payload)
     request.addfinalizer(obj.silent_delete)
     return obj
 
@@ -156,40 +152,40 @@ def project_ansible_git(request, project_ansible_git_nowait):
 
 
 @pytest.fixture(scope="function")
-def project_with_credential_prompt(request, authtoken, api_projects_pg, organization, scm_credential_key_unlock_ASK):
+def project_with_credential_prompt(request, authtoken, organization, scm_credential_key_unlock_ASK):
     # Create project
     payload = dict(name="project-%s" % fauxfactory.gen_utf8(),
-                   organization=organization.id,
                    scm_type='git',
                    scm_url='git@github.com:ansible/ansible-examples.git',
                    scm_key_unlock='ASK',
                    credential=scm_credential_key_unlock_ASK.id,)
-    obj = api_projects_pg.post(payload)
+    obj = organization.get_related('projects').post(payload)
     request.addfinalizer(obj.delete)
     return obj
 
 
 @pytest.fixture(scope="function")
-def many_git_projects(request, authtoken, api_projects_pg, organization):
+def many_git_projects(request, authtoken, organization):
     obj_list = list()
+    related = organization.get_related('projects')
     for i in range(55):
         payload = dict(name="project-%d-%s" % (i, fauxfactory.gen_utf8()),
                        description="random project %d - %s" % (i, fauxfactory.gen_utf8()),
-                       organization=organization.id,
                        scm_type='git',
                        scm_url='https://github.com/jlaska/ansible-playbooks.git',
                        scm_clean=False,
                        scm_delete_on_update=False,
                        scm_update_on_launch=False,)
-        obj = api_projects_pg.post(payload)
+        obj = related.post(payload)
         request.addfinalizer(obj.silent_delete)
         obj_list.append(obj)
     return obj_list
 
 
 @pytest.fixture(scope="function")
-def many_manual_projects(request, authtoken, ansible_runner, awx_config, api_projects_pg, organization):
+def many_manual_projects(request, authtoken, ansible_runner, awx_config, organization):
     obj_list = list()
+    related = organization.get_related('projects')
     for i in range(55):
         # create project path
         local_path = "project_dir_%s" % fauxfactory.gen_utf8()
@@ -198,10 +194,9 @@ def many_manual_projects(request, authtoken, ansible_runner, awx_config, api_pro
         # create manual project
         payload = dict(name="project-%d-%s" % (i, local_path),
                        description="random project %d - %s" % (i, fauxfactory.gen_utf8()),
-                       organization=organization.id,
                        scm_type=None,
                        local_path=local_path)
-        obj = api_projects_pg.post(payload)
+        obj = related.post(payload)
         obj_list.append(obj)
         request.addfinalizer(obj.silent_delete)
 
