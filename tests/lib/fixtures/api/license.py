604d636aa18a5d8b4588e18e3e8d8a1e38492f9e
James Ladd <jladd@redhat.com>
2016-01-29 14:44:28 -0500

    Update license tests  - Tower now stores license in database instead of using    /etc/tower/license

--- tests/lib/fixtures/api/license.py
+++ tests/lib/fixtures/api/license.py
@@ -19,32 +19,61 @@ def no_license(request, authtoken, api_config_pg):
 @pytest.fixture(scope='function')
 def install_legacy_license(request, api_config_pg, ansible_runner):
     '''Install an legacy license where instance_count=unlimited'''
-
+    # Apply license
     log.debug("calling fixture install_legacy_license_unlimited")
     license_info = common.tower.license.generate_license(instance_count=sys.maxint, days=365, license_type='legacy')
     api_config_pg.post(license_info)
     request.addfinalizer(api_config_pg.delete)
 
+    # Confirm that license is present
+    conf = api_config_pg.get()
+    assert conf.is_valid_license, 'Expected valid license, invalid license found'
+
+    # Confirm license type, license_key have expected values
+    assert conf.is_legacy_license, \
+        "Expected legacy license, found %s." % conf.license_info.license_type
+    assert conf.license_info.license_key == license_info['license_key'], \
+        "License found differs from license applied"
+
 
 @pytest.fixture(scope='function')
 def install_basic_license(request, api_config_pg, ansible_runner):
     '''Install an basic license where instance_count=unlimited'''
-
+    # Apply license
     log.debug("calling fixture install_basic_license_unlimited")
     license_info = common.tower.license.generate_license(instance_count=sys.maxint, days=365, license_type='basic')
     api_config_pg.post(license_info)
     request.addfinalizer(api_config_pg.delete)
 
+    # Confirm that license is present
+    conf = api_config_pg.get()
+    assert conf.is_valid_license, 'Expected valid license, invalid license found'
+
+    # Confirm license type, license_key have expected values
+    assert conf.is_basic_license, \
+        "Expected basic license, found %s." % conf.license_info.license_type
+    assert conf.license_info.license_key == license_info['license_key'], \
+        "License found differs from license applied"
+
 
 @pytest.fixture(scope='function')
 def install_enterprise_license(request, api_config_pg, ansible_runner):
     '''Install an enterprise license where instance_count=unlimited'''
-
     log.debug("calling fixture install_enterprise_license")
 
     # Post the license
     license_info = common.tower.license.generate_license(instance_count=sys.maxint, days=365, license_type='enterprise')
     api_config_pg.post(license_info)
+
+    # Confirm that license is present
+    conf = api_config_pg.get()
+    assert conf.is_valid_license, 'Expected valid license, invalid license found'
+
+    # Confirm license type, license_key have expected values
+    assert conf.is_enterprise_license, \
+        "Expected enterprise license, found %s." % conf.license_info.license_type
+    assert conf.license_info.license_key == license_info['license_key'], \
+        "License found differs from license applied"
 
     # Determine if mongo is needed
     contacted = ansible_runner.command("tower-manage uses_mongo --local")
