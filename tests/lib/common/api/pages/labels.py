0459497eb287fef8a376acfd602fe50f5c26a715
Christopher Wang <cwang@ansible.com>
2016-03-29 10:14:57 -0400

    Added schema and page objects for labels

---
+++ tests/lib/common/api/pages/labels.py
@@ -0,0 +1,32 @@
+from common.api.pages import Base, Base_List, json_setter, json_getter
+
+
+class Label_Page(Base):
+    base_url = '/api/v1/labels/{id}/'
+    type = property(json_getter('type'), json_setter('type'))
+    created = property(json_getter('created'), json_setter('created'))
+    modified = property(json_getter('modified'), json_setter('modified'))
+    name = property(json_getter('name'), json_setter('name'))
+    organization = property(json_getter('organization'), json_setter('organization'))
+
+
+class Labels_Page(Label_Page, Base_List):
+    base_url = '/api/v1/labels/'
+
+    def get_related(self, attr, **kwargs):
+        assert attr in self.json['related'], \
+            "No such related attribute '%s'" % attr
+
+        if attr == 'created_by':
+            from users import User_Page
+            cls = User_Page
+        elif attr == 'modified_by':
+            from users import User_Page
+            cls = User_Page
+        elif attr == 'organization':
+            from organizations import Organization_Page
+            cls = Organization_Page
+        else:
+            raise NotImplementedError("No related class found for '%s'" % attr)
+
+        return cls(self.testsetup, base_url=self.json['related'][attr]).get(**kwargs)
