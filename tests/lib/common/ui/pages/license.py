8fc06b3d350ce0049cf06da75bfdf70eeccd5c84
Shane McDonald <smcdonal@redhat.com>
2016-04-27 08:44:22 -0400

    Add UI test for license expiration date / time remaining.

--- tests/lib/common/ui/pages/license.py
+++ tests/lib/common/ui/pages/license.py
@@ -1,10 +1,44 @@
+from selenium.webdriver.common.by import By
+
 from base import TowerPage
+from common.ui.pages.forms import Checkbox
+from common.ui.pages.page import Region
+from common.ui.pages.regions import Clickable
 
 
 class License(TowerPage):
 
     _path = '/#/license'
 
-    def open(self, *args, **kwargs):
-        self.wait_for_spinny()
-        return super(License, self).open(*args, **kwargs)
+    _agree_eula = (
+        (By.CSS_SELECTOR, '[class="License-details--label"]'),
+        (By.CSS_SELECTOR, '[type="checkbox'))
+
+    _license_upload = (By.ID, 'License-file')
+    _submit = (By.CSS_SELECTOR, '[class="btn btn-success pull-right"]')
+
+    _expires_on = (By.XPATH, '//*[text()="Expires On"]/following::div')
+    _time_remaining = (By.XPATH, '//*[text()="Time Remaining"]/following::div')
+
+    @property
+    def agree_eula(self):
+        return Checkbox(self, root_locator=self._agree_eula)
+
+    @property
+    def license_upload(self):
+        return Region(self, root_locator=self._license_upload)
+
+    @property
+    def submit(self):
+        return Clickable(self, root_locator=self._submit, spinny=True)
+
+    @property
+    def expires_on(self):
+        return Region(self, root_locator=self._expires_on)
+
+    @property
+    def time_remaining(self):
+        return Region(self, root_locator=self._time_remaining)
+
+    def upload(self, file_path):
+        self.license_upload.root.send_keys(file_path)
---
+++ tests/ui/test_license.py
@@ -0,0 +1,43 @@
+import pytest
+import json
+
+from time import time
+from random import randint
+from datetime import datetime, timedelta
+from common.tower.license import generate_license_file
+
+pytestmark = [
+    pytest.mark.ui,
+    pytest.mark.nondestructive,
+    pytest.mark.usefixtures(
+        'authtoken',
+        'maximized_window_size'
+    )
+]
+
+
+@pytest.mark.github('https://github.com/ansible/ansible-tower/issues/1250')
+@pytest.mark.usefixtures('no_license')
+def test_expiration_date(ui_license):
+    """Check that the correct expiration date and time remaining are
+    displayed on the license page.
+    """
+    license_path = generate_license_file(
+        days=randint(30, 999),
+        license_type='enterprise')
+
+    with open(license_path) as license:
+        license = json.load(license)
+
+    ui_license.upload(license_path)
+    ui_license.agree_eula.click()
+
+    ui_license.submit.click()
+
+    license_date = int(license['license_date'])
+    current_date = int(time())
+    time_remaining = timedelta(seconds=license_date - current_date).days
+    expires_on = datetime.utcnow() + timedelta(days=time_remaining)
+
+    assert ui_license.time_remaining.text == "%s Days" % time_remaining
+    assert ui_license.expires_on.text == expires_on.strftime("%m/%d/%Y")
