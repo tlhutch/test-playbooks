47103980777cb2425caa2143ffd70132fe8b8f68
Christopher Wang <cwang@ansible.com>
2016-05-09 10:15:14 -0400

    Removed cleanup_deleted from tower-qa

--- tests/lib/fixtures/api/system_job_templates.py
+++ tests/lib/fixtures/api/system_job_templates.py
@@ -1,7 +1,7 @@
 import pytest
 
 
-@pytest.fixture(scope="function", params=['cleanup_jobs', 'cleanup_deleted', 'cleanup_activitystream', 'cleanup_facts'])
+@pytest.fixture(scope="function", params=['cleanup_jobs', 'cleanup_activitystream', 'cleanup_facts'])
 def system_job_template(request, api_system_job_templates_pg):
     return request.getfuncargvalue(request.param + '_template')
 
@@ -15,18 +15,6 @@ def cleanup_jobs_template(request, api_system_job_templates_pg):
     matches = api_system_job_templates_pg.get(job_type='cleanup_jobs')
     assert matches.count == 1, "Unexpected number of results (%s) when querying " \
         "for system_job_template job_type:cleanup_jobs" % matches.count
-    return matches.results[0]
-
-
-@pytest.fixture(scope="function")
-def cleanup_deleted_template(request, api_system_job_templates_pg):
-    '''
-    Return a System_Job_Template object representing the 'cleanup_deleted' system
-    job template.
-    '''
-    matches = api_system_job_templates_pg.get(job_type='cleanup_deleted')
-    assert matches.count == 1, "Unexpected number of results (%s) when querying " \
-        "for system_job_template job_type:cleanup_deleted" % matches.count
     return matches.results[0]
 
 
