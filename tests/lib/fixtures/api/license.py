6b3b5faaad9ebdce7a80440bc93e9808c93215c1
Jake McDermott <jmcdermott@ansible.com>
2016-07-25 19:50:23 -0400

    add module scoped enterprise license fixture - use it if you're using module scoped factories

--- tests/lib/fixtures/api/license.py
+++ tests/lib/fixtures/api/license.py
@@ -1,101 +1,103 @@
-import sys
 import logging
+import sys
+import time
+
 import pytest
-import common.tower.license
+
+from common.tower.license import generate_license
 
 
 log = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope='function')
-def no_license(request, authtoken, api_config_pg):
-    '''Remove an active license'''
-    log.debug("deleting any active license")
+def install_license(request, api_config_pg, **kwargs):
+    # generate license
+    license_info = generate_license(**kwargs)
+    # install license
+    api_config_pg.post(license_info)
+    request.addfinalizer(api_config_pg.delete)
+    # Confirm that license is present
+    conf = api_config_pg.get()
+    assert conf.is_valid_license, 'Invalid license found'
+    # Confirm license type
+    expected_license_type = license_info['license_type']
+    assert conf.license_info.license_type == expected_license_type, (
+        'expected {0} license, found {1}'.format(
+            expected_license_type, conf.license_info.license_type))
+    # Confirm license key
+    expected_license_key = license_info['license_key']
+    assert conf.license_info.license_key == expected_license_key, (
+        'License found differs from license applied')
+
+
+@pytest.fixture
+def no_license(request, api_config_pg):
+    """Remove an active license
+    """
+    log.debug('deleting any active license')
     api_config_pg.delete()
     request.addfinalizer(api_config_pg.delete)
 
 
-@pytest.fixture(scope='function')
-def install_legacy_license(request, api_config_pg, ansible_runner):
-    '''Install an legacy license where instance_count=unlimited'''
-    # Apply license
-    log.debug("calling fixture install_legacy_license_unlimited")
-    license_info = common.tower.license.generate_license(instance_count=sys.maxint, days=365, license_type='legacy')
-    api_config_pg.post(license_info)
-    request.addfinalizer(api_config_pg.delete)
-
-    # Confirm that license is present
-    conf = api_config_pg.get()
-    assert conf.is_valid_license, 'Expected valid license, invalid license found'
-
-    # Confirm license type, license_key have expected values
-    assert conf.is_legacy_license, \
-        "Expected legacy license, found %s." % conf.license_info.license_type
-    assert conf.license_info.license_key == license_info['license_key'], \
-        "License found differs from license applied"
+@pytest.fixture
+def install_legacy_license(request, api_config_pg):
+    """Install legacy license
+    """
+    log.debug('calling fixture install_legacy_license_unlimited')
+    install_license(request,
+                    api_config_pg,
+                    days=365,
+                    instance_count=sys.maxint,
+                    license_type='legacy')
 
 
-@pytest.fixture(scope='function')
-def install_basic_license(request, api_config_pg, ansible_runner):
-    '''Install an basic license where instance_count=unlimited'''
-    # Apply license
-    log.debug("calling fixture install_basic_license_unlimited")
-    license_info = common.tower.license.generate_license(instance_count=sys.maxint, days=365, license_type='basic')
-    api_config_pg.post(license_info)
-    request.addfinalizer(api_config_pg.delete)
-
-    # Confirm that license is present
-    conf = api_config_pg.get()
-    assert conf.is_valid_license, 'Expected valid license, invalid license found'
-
-    # Confirm license type, license_key have expected values
-    assert conf.is_basic_license, \
-        "Expected basic license, found %s." % conf.license_info.license_type
-    assert conf.license_info.license_key == license_info['license_key'], \
-        "License found differs from license applied"
+@pytest.fixture
+def install_basic_license(request, api_config_pg):
+    """Install basic license
+    """
+    log.debug('calling fixture install_basic_license_unlimited')
+    install_license(request,
+                    api_config_pg,
+                    days=365,
+                    instance_count=sys.maxint,
+                    license_type='basic')
 
 
-@pytest.fixture(scope='function')
-def install_enterprise_license(request, api_config_pg, ansible_runner):
-    '''Install an enterprise license where instance_count=unlimited'''
-    log.debug("calling fixture install_enterprise_license")
-
-    # Post the license
-    license_info = common.tower.license.generate_license(instance_count=sys.maxint, days=365, license_type='enterprise')
-    api_config_pg.post(license_info)
-
-    # Confirm that license is present
-    conf = api_config_pg.get()
-    assert conf.is_valid_license, 'Expected valid license, invalid license found'
-
-    # Confirm license type, license_key have expected values
-    assert conf.is_enterprise_license, \
-        "Expected enterprise license, found %s." % conf.license_info.license_type
-    assert conf.license_info.license_key == license_info['license_key'], \
-        "License found differs from license applied"
-
-    def teardown():
-        # Delete the license
-        api_config_pg.delete()
-
-    request.addfinalizer(teardown)
+@pytest.fixture
+def install_enterprise_license(request, api_config_pg):
+    """Install enterprise license
+    """
+    log.debug('calling fixture install_enterprise_license')
+    install_license(request,
+                    api_config_pg,
+                    days=365,
+                    instance_count=sys.maxint,
+                    license_type='enterprise')
 
 
 @pytest.fixture(scope='class')
-def install_enterprise_license_unlimited(request, api_config_pg, ansible_runner):
-    '''Install an enterprise license where instance_count=unlimited'''
+def install_enterprise_license_unlimited(request, api_config_pg):
+    """Install enterprise license at the class fixture scope
+    """
+    log.debug('calling fixture install_enterprise_license_unlimited')
+    install_license(request,
+                    api_config_pg,
+                    days=365,
+                    instance_count=sys.maxint,
+                    license_type='enterprise')
+    # Pause to allow tower to do it's thing
+    request.addfinalizer(lambda: time.sleep(15))
 
-    log.debug("calling fixture install_enterprise_license_unlimited")
 
-    # Post the license
-    license_info = common.tower.license.generate_license(instance_count=sys.maxint, days=365, license_type='enterprise')
-    api_config_pg.post(license_info)
-
-    def teardown():
-        # Delete the license
-        api_config_pg.delete()
-
-        # Pause to allow tower to do it's thing
-        ansible_runner.pause(seconds=15)
-
-    request.addfinalizer(teardown)
+@pytest.fixture(scope='module')
+def module_install_enterprise_license(authtoken, request, api_config_pg):
+    """Install enterprise license at the module fixture scope
+    """
+    log.debug('calling fixture module_install_enterprise_license')
+    install_license(request,
+                    api_config_pg,
+                    days=365,
+                    instance_count=sys.maxint,
+                    license_type='enterprise')
+    # Pause to allow tower to do it's thing
+    request.addfinalizer(lambda: time.sleep(15))
