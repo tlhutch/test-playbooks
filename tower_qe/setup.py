6632f0b21e0a19563b3d197678eff4024b800cdc
Ryan Fitzpatrick <rmfitzpatrick@users.noreply.github.com>
2016-09-07 15:53:30 -0400

    Install tower_qe from tests/requirements.txt (#754)
    * updated installe
    
    * remove tower_qe git repo to prevent ui breakage

--- tower_qe/setup.py
+++ tower_qe/setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+requirements = [str(r.req) for r in parse_requirements('requirements.txt', session=False)]
 
 version = '0.1.0'
 setup(name='tower_qe',
       version=version,
       description='Ansible Tower QE Libraries',
       packages=find_packages(exclude=['test']),
-      include_package_data=True)
+      include_package_data=True,
+      install_requires=requirements)
