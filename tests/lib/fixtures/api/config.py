a1ea6b4262861e404aa1a67911ff1bf5fb112745
Christopher Wang <cwang@ansible.com>
2016-09-13 11:43:56 -0400

    Added stub custom console logo fixture

--- tests/lib/fixtures/api/config.py
+++ tests/lib/fixtures/api/config.py
@@ -245,3 +245,39 @@ AD_HOC_COMMANDS = %s
                 assert result['rc'] == 0, \
                     "Failure restarting ansible-tower\n%s" % json.dumps(result, indent=2)
     request.addfinalizer(fin)
+
+
+@pytest.fixture
+def CUSTOM_CONSOLE_LOGO(request, ansible_runner):
+    """Installs a custom console logo on our target host. Files transferred include the image itself
+    and a settings file. Files is removed upon teardown.
+    """
+    # install custom_console_logo.png
+    contacted = ansible_runner.copy(
+        src='tests/lib/fixtures/api/static/custom_console_logo.png',
+        dest='/var/lib/awx/public/static/assets/',
+        owner='awx',
+        group='awx',
+        mode='0755'
+    )
+    # install local_settings.json
+    contacted = ansible_runner.copy(
+        src='tests/lib/fixtures/api/static/local_settings.json',
+        dest='/var/lib/awx/public/static/',
+        owner='awx',
+        group='awx',
+        mode='0755'
+    )
+    import pdb; pdb.set_trace()
+    def fin():
+        # remove custom_console_logo.png
+        contacted = ansible_runner.file(
+            path='/var/lib/awx/public/static/assets/custom_console_logo.png',
+            state='absent'
+        )
+        contacted = ansible_runner.file(
+            path='/var/lib/awx/public/static/local_settings.json',
+            state='absent'
+        )
+        import pdb; pdb.set_trace()
+    request.addfinalizer(fin)
