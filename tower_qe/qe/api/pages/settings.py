7795e1d9c1953885deb1af5d72fca4dbf929032d
Christopher Wang <cwang@ansible.com>
2016-09-23 10:53:17 -0400

    Renamed helper method

--- tower_qe/qe/api/pages/settings.py
+++ tower_qe/qe/api/pages/settings.py
@@ -3,10 +3,11 @@ import base
 
 
 class Settings_Page(base.Base):
-    def get_setting(self, setting):
-        """Helper method used to navigate to a specific settings page.
+    def get_endpoint(self, endpoint):
+        """Helper method used to navigate to a specific settings endpoint.
+        (Pdb) settings_pg.get_endpoint('all')
         """
-        base_url = '/api/v1/settings/%s/' % setting
+        base_url = '/api/v1/settings/%s/' % endpoint
         return self.walk(base_url)
 
 base.register_page(resources.v1_settings, Settings_Page)
