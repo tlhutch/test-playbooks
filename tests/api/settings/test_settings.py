eca55e4806ce6b34a0d9cac47caa6b702d0f4779
Christopher Wang <cwang@ansible.com>
2016-11-15 16:58:14 -0500

    Add GitHub marker for test_stdout_max_bytes_display

--- tests/api/settings/test_settings.py
+++ tests/api/settings/test_settings.py
@@ -180,6 +180,7 @@ class Test_Setting(Base_Api_Test):
             "Unexpected response when relaunching ad hoc command whose module " \
             "has been removed from AD_HOC_COMMANDS: %s." % json.dumps(result)
 
+    @pytest.mark.github("https://github.com/ansible/ansible-tower/issues/4030")
     def test_stdout_max_bytes_display(self, unified_job_with_stdout, update_setting_pg, ansible_runner):
         '''
         Assert that all of our unified jobs include result_stdout by default. Then assert that
