c0b2537ec1fbe6dded48b3cf06ddb6e7a7f5b217
Jim Ladd <jladd@ansible.com>
2016-11-17 18:18:46 -0500

    Adding WorkflowTree and WorkflowTreeMapper

---
+++ tests/lib/helpers/workflow_utils.py
@@ -0,0 +1,320 @@
+import logging
+
+# Utilities for testing workflows
+
+log = logging.getLogger(__name__)
+
+
+class WorkflowTree(object):
+    """
+    Represents workflow tree structure. Can be built manually, but also has
+    a constructor that supports building a `WorkflowTree` using a
+    `WorkflowJobTemplate` or `WorkflowJob` page.
+
+    ex:
+    # Create tree based on WFJT page object
+    >>> wfjt = v1.workflow_job_templates.get(id=1).results.pop()
+    >>> tree1 = WorkflowTree(wfjt)
+
+    # Create tree by hand
+    >>> tree2 = WorkflowTree()
+    >>> tree2.add_nodes(1, 2, 3)
+    >>> tree2.add_edge(1, 2, 'success')
+
+    # Assert equality
+    >>> assert tree1 == tree2
+    """
+
+    def __init__(self, workflow=None):
+        """Initialize `WorkflowTree`. May optionally provide reference to a
+           `WorkflowJobTemplate` or `WorkflowJob` object."""
+        self._graph = {}
+
+        if workflow:
+            # Get nodes
+            try:
+                workflow.get()
+                nodes = workflow.get_related('workflow_nodes').results
+            except:
+                log.error("Unable to get 'workflow_nodes' from:\n{}".format(workflow))
+                raise
+
+            # Process nodes
+            for node in nodes:
+                self._graph[node.id] = {}
+                for condition in ('always', 'failure', 'success'):
+                    triggered_node_ids = getattr(node, condition + '_nodes', set())
+                    if len(triggered_node_ids):
+                        self._graph[node.id][condition] = set(triggered_node_ids)
+
+    def __repr__(self):
+        return str(self._graph)
+
+    def __str__(self):
+        output = ['Nodes  Edges', '-----  -----------------']
+        if not len(self._graph):
+            output.append('(No nodes)')
+            return '\n'.join(output)
+
+        nodes = self._graph.keys()
+        nodes.sort()
+        for node in nodes:
+            line = '{0:<5}  '.format(node)
+            conditions = self._graph[node].keys()
+            conditions.sort()
+            for condition in conditions:
+                line += '{0}: {1} '.format(condition, list(self._graph[node][condition]))
+            output.append(line)
+        return '\n'.join(output)
+
+    def __eq__(self, workflow_tree):
+        return self._graph == workflow_tree._graph
+
+    def __ne__(self, workflow_tree):
+        return self._graph != workflow_tree._graph
+
+    def get_node(self, id):
+        """Returns a node. If node does not exist, returns `None`."""
+        return self._graph.get(id)
+
+    def add_node(self, id, always_nodes=[], failure_nodes=[], success_nodes=[]):
+        """Add single node to workflow tree. Requires id of node.
+           May optionally specify triggered nodes using lists of integers.
+
+           Raises `Exception` if node with `id` already exists.
+           Raises `Exception` if node includes edges to nodes that do not exist.
+
+           ex:
+           >>> tree = WorkflowTree()
+           >>> tree.add_nodes(3)
+           >>> tree.add_node(2, always_nodes=[3])
+           >>> tree.add_node(1, sucess_nodes=[2,3])
+        """
+        # Node should not already exist
+        if id in self._graph:
+            raise Exception("Cannot create node '{}'; node already exists.".format(id))
+
+        # Edge nodes should already exist
+        edge_nodes = set(always_nodes + failure_nodes + success_nodes)
+        for node in edge_nodes:
+            if node not in self._graph:
+                error = "Cannot create node '{}'. Includes edge to node '{}', which does not exist.".format(id, node)
+                raise Exception(error)
+
+        self._graph[id] = {}
+
+        if always_nodes:
+            self._graph[id]['always'] = set(always_nodes)
+        if failure_nodes:
+            self._graph[id]['failure'] = set(failure_nodes)
+        if success_nodes:
+            self._graph[id]['success'] = set(success_nodes)
+
+    def add_nodes(self, *ids):
+        """Add one or more nodes to workflow tree.
+
+           Raises `Exception` if node with id found in `ids` already exists.
+           (If this happens, no nodes are created).
+
+           ex:
+           >>> tree = WorkflowTree()
+           >>> tree.add_nodes(1)
+           >>> tree.add_nodes(2, 3, 4)
+        """
+        # Cannot reference same node more than once
+        if len(ids) != len(set(ids)):
+            raise Exception('Cannot reference same id more than once. Given: {}'.format(ids))
+
+        # Cannot already have any nodes with id found in `ids`
+        for id in ids:
+            if id in self._graph:
+                raise Exception("Cannot create node '{}'; node already exists.".format(id))
+
+        for id in ids:
+            self._graph[id] = {}
+
+    def remove_nodes(self, *ids):
+        """Remove one or more nodes from workflow tree.
+
+           Automatically removes any edges that referred to deleted nodes.
+
+           Note that nodes must already exist, otherwise raises `Exception`.
+           If ids contains a reference to a node that is not in the
+           `WorkflowTree`, no nodes will be deleted.
+        """
+        if not len(ids):
+            return
+
+        # All nodes must exist
+        for id in ids:
+            if id not in self._graph:
+                raise Exception('Cannot delete node {}. Node does not exist.'.format(id))
+
+        # Delete nodes
+        for id in ids:
+            del self._graph[id]
+
+        # Clean-up any edges that referred to deleted nodes
+        ids = set(ids)
+        for id in self._graph:
+            # Get all keys in advance (otherwise, size of dict may change, interrupting iteration)
+            conditions = self._graph[id].keys()
+            for condition in conditions:
+                self._graph[id][condition] = self._graph[id][condition] - ids
+                if not len(self._graph[id][condition]):
+                    del self._graph[id][condition]
+
+    def add_edge(self, node, triggered_node, condition):
+        """Adds an edge from `node` to `triggered_node` of type `condition`.
+
+           Note that both `node` ande `triggered_node` must both exist, otherwise
+           raises `Exception`.
+
+           `condition` must be one of 'always', 'failure', or 'success', otherwise
+           raises `Exception`.
+
+           >>> tree = WorkflowTree()
+           >>> tree.add_nodes(1)
+           >>> tree.add_nodes(2)
+           >>> tree.add_edge(1, 2, 'always')
+        """
+        if condition not in ('always', 'failure', 'success'):
+            raise Exception("Node {0}: cannot add edge of type '{1}'.".format(node, condition))
+        if node not in self._graph:
+            raise Exception("Cannot add edge; node '{}' does not exist.".format(node))
+        if triggered_node not in self._graph:
+            raise Exception("Cannot add edge; second node '{}' does not exist.".format(triggered_node))
+        if condition not in self._graph[node]:
+            self._graph[node][condition] = set([])
+
+        self._graph[node][condition].add(triggered_node)
+
+    def remove_edge(self, node, triggered_node, condition):
+        """Removes an edge from `node` to `triggered_node` of type `condition`.
+
+           * `condition` must be one of 'always', 'failure', or 'success', otherwise raises `Exception`.
+           * If edge does not exist, raises `ExceptionError`.
+        """
+        if condition not in ('always', 'failure', 'success'):
+            raise Exception("Node {0}: cannot remove edge of type '{1}'.".format(node, condition))
+        if node not in self._graph:
+            raise Exception('Cannot remove edge from node {0}; node does not exist.'.format(node))
+        if condition not in self._graph[node] or triggered_node not in self._graph[node][condition]:
+            raise Exception("Cannot remove edge of type '{0}' from node '{1}'; edge does not exist.".format(condition, node))
+
+        self._graph[node][condition].remove(triggered_node)
+
+        if not len(self._graph[node][condition]):
+            del self._graph[node][condition]
+
+
+class WorkflowTreeMapper(object):
+    """Finds a mapping of one graph's nodes onto the other's.
+    (i.e. a graph homomorphism)."""
+
+    def __init__(self, tree1, tree2):
+        """Create `WorkflowTreeMapper` with references
+        to two `WorkflowTree`s being compared."""
+        self._tree1 = tree1
+        self._tree2 = tree2
+
+    def _get_root_nodes(self, tree):
+        """Gets the root nodes for a given tree."""
+        nodes = tree._graph.keys()
+
+        # Node is root node if it is not triggered by any
+        # other node
+        triggered_nodes = []
+        for node in nodes:
+            for condition in tree._graph[node]:
+                triggered_nodes.extend(tree._graph[node][condition])
+
+        return list(set(nodes) - set(triggered_nodes))
+
+    def _match(self, node1, node2):
+        """Two nodes match if they have the same number
+        of edges (for each edge type)."""
+        for condition in node1:
+            if condition not in node2:
+                return False
+            if len(node1[condition]) != len(node2[condition]):
+                return False
+        return True
+
+    def map(self):
+        """If mapping exists, returns list of tuples
+        representing mapping of nodes from first graph
+        to second graph.
+
+        If no mapping exists, returns None.
+
+        Note that function does not guarantee that
+        mapping is unique.
+        """
+        root_nodes1 = self._get_root_nodes(self._tree1)
+        root_nodes2 = self._get_root_nodes(self._tree2)
+        return self._map(root_nodes1, root_nodes2)
+
+    def _map(self, root_nodes1, root_nodes2):
+        """If mapping exists, returns list of tuples.
+        Otherwise, returns None."""
+
+        # If number of root nodes differ on each side,
+        # then no mapping exists
+        if len(root_nodes1) != len(root_nodes2):
+            return None
+
+        # If there are no nodes, nothing to be done
+        if not len(root_nodes1):
+            return []
+
+        # Map first root node
+        node1 = root_nodes1[0]
+
+        # ..to one of the root nodes in second graph
+        for node2 in root_nodes2:
+            temp_mapping = []
+
+            # If the two nodes are a match..
+            if self._match(self._tree1._graph[node1], self._tree2._graph[node2]):
+                temp_mapping = [(node1, node2)]
+
+                # Recursively map each set of trigger nodes
+                mapping_successful = True
+                for condition in self._tree1._graph[node1]:
+                    trigger_nodes1 = list(self._tree1._graph[node1][condition])
+                    trigger_nodes2 = list(self._tree2._graph[node2][condition])
+                    mapping = self._map(trigger_nodes1, trigger_nodes2)
+
+                    if mapping:
+                        temp_mapping.extend(mapping)
+                    else:
+                        mapping_successful = False
+                        break
+
+                # If recursive mapping not successful,
+                # attempt to map node1 to different node
+                if not mapping_successful:
+                    continue
+
+                # Attempt to map remaining root nodes
+                if len(root_nodes1) == 1:
+                    # This was the last root node, return results
+                    return temp_mapping
+
+                # (Use list to create copy)
+                new_root_nodes1 = list(root_nodes1)
+                new_root_nodes2 = list(root_nodes2)
+                new_root_nodes1.remove(node1)
+                new_root_nodes2.remove(node2)
+                mapping = self._map(new_root_nodes1, new_root_nodes2)
+
+                # If mapping successful, then the mapping is complete
+                if mapping:
+                    temp_mapping.extend(mapping)
+                    return temp_mapping
+
+                # Otherwise, try to map node1 to next node
+
+        # No mapping found
+        return None
---
+++ tests/lib/test/test_workflowtree.py
@@ -0,0 +1,428 @@
+import pytest
+
+from tests.lib.helpers.workflow_utils import WorkflowTree, WorkflowTreeMapper
+
+
+class MockNode:
+
+    def __init__(self, id, always_nodes=[], failure_nodes=[], success_nodes=[]):
+        self.id = id
+        self.always_nodes = set(always_nodes)
+        self.failure_nodes = set(failure_nodes)
+        self.success_nodes = set(success_nodes)
+
+
+class MockNodes:
+    """Wraps `MockNode` objects to provide `results` attribute."""
+
+    def __init__(self, nodes=[]):
+        self.results = nodes
+
+
+class MockWorkflowJobTemplate:
+    """Mocks WorkflowJobTemplate page object"""
+
+    def __init__(self, nodes=[]):
+        """Optionally takes list of `MockNode` objects"""
+        self.nodes = MockNodes(nodes)
+
+    def get(self):
+        return self
+
+    def get_related(self, item):
+        """ignore `item`. WorkflowTree only uses `get_related`
+           for getting 'workflow_nodes'."""
+        return self.nodes
+
+######################
+# WorkflowTree tests #
+######################
+
+
+@pytest.mark.parametrize('node', [0, -1, 5])
+def test_get_node_from_empty_tree(node):
+    'Confirms getting node that does not exist returns None'
+    t = WorkflowTree()
+    assert not len(t._graph), 'Tree should be empty when first created'
+    assert t.get_node(node) is None, 'Tree empty, but found node ({})'.format(id)
+
+
+def test_add_nodes_without_edges():
+    """Create single node. Shouldn't find any edges on node."""
+    t = WorkflowTree()
+    t.add_nodes(1)
+    n1 = t.get_node(1)
+
+    # Assert dictionary representing node is empty (i.e. has no edges)
+    assert not len(n1), 'Found edges (non expected): {}'.format(n1)
+
+
+def test_add_node():
+    """Confirm can successfully add single node using `add_nodes`"""
+    t = WorkflowTree()
+    t.add_nodes(1)
+    assert len(t._graph) == 1, 'WorkflowTree should have single node'
+    assert 1 in t._graph, 'Expected node 1 to be in WorkflowTree'
+    assert not len(t._graph[1]), 'Node 1 should not have any edges:\n{}'.format(t._graph[1])
+
+
+def test_add_nodes():
+    """Confirm can successfully add multiple single node using `add_nodes`"""
+    t = WorkflowTree()
+    nodes = [1, 2, 3, 4]
+    t.add_nodes(*nodes)
+    assert len(t._graph) == len(nodes),\
+        'WorkflowTree should have {0} nodes, found {1}'.format(len(nodes), len(t._graph))
+    for node in nodes:
+        assert node in t._graph, 'Expected node {} to be in WorkflowTree'.format(node)
+
+
+def test_add_node_with_edge_to_node_that_does_not_exist():
+    """Confirm that attempting to create a node with an edge
+    to a node that does not exist, `Exception` is raised."""
+    t = WorkflowTree()
+    error = 'Should not be able to create node with edge to node that does not exist.'
+    with pytest.raises(Exception, msg=error):
+        t.add_node(1, always_nodes=[2])
+
+    with pytest.raises(Exception, msg=error):
+        t.add_node(1, always_nodes=[2, 3])
+
+    t.add_nodes(2)
+    with pytest.raises(Exception, msg=error):
+        t.add_node(1, always_nodes=[2, 3])  # Still missing node 3
+
+    with pytest.raises(Exception, msg=error):
+        t.add_node(1, always_nodes=[2], success_nodes=[3])
+
+    t.add_nodes(3)
+    with pytest.raises(Exception, msg=error):
+        t.add_node(1, always_nodes=[2], success_nodes=[3, 4])  # Missing node 4
+
+
+def test_add_node_with_edges():
+    """Confirm that adding a node with edges is successful."""
+    t = WorkflowTree()
+    t.add_nodes(2)
+    t.add_nodes(3)
+    t.add_nodes(4)
+    t.add_node(1, success_nodes=[2], failure_nodes=[3, 4])
+    n1 = t.get_node(1)
+
+    # Assert node has expected edges
+    assert n1['success'] == set([2]), \
+        "Expected 'success' edge to node 2, found edge(s) to: {}".format(n1['success'])
+    assert n1['failure'] == set([3, 4]), \
+        "Expected 'failure' edges to nodes 3 and 4, found edge(s) to: {}".format(n1['failure'])
+    assert 'always' not in n1, "Did not expect 'always' edges"
+
+
+@pytest.mark.parametrize('node', [-1, 0, 5])
+def test_remove_node_that_does_not_exist(node):
+    """Confirm that removing node that does not exist raises `Exception`"""
+    t = WorkflowTree()
+    with pytest.raises(Exception, msg='Expect `Exception` when deleting node that does not exist'):
+        t.remove_nodes(node)
+
+
+def test_remove_node():
+    """Confirm can successfully remove node."""
+    t = WorkflowTree()
+    t.add_nodes(1)
+    assert len(t._graph) == 1, 'Expected single node in graph'
+    assert 1 in t._graph, 'Expected node 1 in graph'
+
+    t.remove_nodes(1)
+    assert not len(t._graph), 'Expected graph to be empty after removing node.'
+
+
+def test_remove_node_with_edges():
+    """Confirm can successfully remove nodes."""
+    t = WorkflowTree()
+    t.add_nodes(4, 5)
+    t.add_node(1, always_nodes=[4])
+    t.add_node(2, success_nodes=[4])
+    t.add_node(3, failure_nodes=[4, 5])
+
+    t.remove_nodes(4)
+    assert 'always' not in t._graph[1]
+    assert 'success' not in t._graph[2]
+    assert 'failure' in t._graph[3]
+    assert 5 in t._graph[3]['failure']
+
+
+def test_add_edge_when_nodes_missing():
+    """Confirms cannot add edge when either node is missing"""
+    t = WorkflowTree()
+
+    with pytest.raises(Exception, msg='Should not be able to add edge when both nodes missing.'):
+        t.add_edge(1, 2, 'always')
+
+    t.add_nodes(2)
+    with pytest.raises(Exception, msg='Should not be able to add edge when first node missing.'):
+        t.add_edge(1, 2, 'always')
+
+    t.remove_nodes(2)
+    t.add_nodes(1)
+    with pytest.raises(Exception, msg='Should not be able to add edge when second node missing.'):
+        t.add_edge(1, 2, 'always')
+
+
+def test_add_edge():
+    """Confirms can successfully add edge"""
+    t = WorkflowTree()
+    t.add_nodes(1, 2)
+    t.add_edge(1, 2, 'always')
+
+    # Confirm edge created
+    assert 1 in t._graph
+    assert 'always' in t._graph[1]
+    assert 'success' not in t._graph[1]
+    assert 'failure' not in t._graph[1]
+    assert t._graph[1]['always'] == set([2])
+
+
+def test_remove_edge_that_does_not_exist():
+    """Confirm that removing edge that does not exist raises `Exception`"""
+    t = WorkflowTree()
+
+    # Remove edge when nodes do not exist
+    with pytest.raises(Exception, msg='Removing edge that does not exist should raise `Exception`'):
+        t.remove_edge(1, 2, 'always')
+
+    t.add_nodes(1)
+    # Remove edge when second node do not exist
+    with pytest.raises(Exception, msg='Removing edge that does not exist should raise `Exception`'):
+        t.remove_edge(1, 2, 'always')
+
+    t.add_nodes(2)
+    # Remove edge when edge does not exist
+    with pytest.raises(Exception, msg='Removing edge that does not exist should raise `Exception`'):
+        t.remove_edge(1, 2, 'always')
+
+
+def test_remove_edges():
+    """Confirm removing edge that exists is successful"""
+    t = WorkflowTree()
+    t.add_nodes(2, 3)
+    t.add_node(1, success_nodes=[2, 3])
+    n1 = t.get_node(1)
+    assert n1['success'] == set([2, 3]), \
+        'Created node with 2 edges, but cannot find edges. Edge(s) found: {}'.format(n1['success'])
+
+    t.remove_edge(1, 3, 'success')
+    assert n1['success'] == set([2]), \
+        'Removed edge (to node 3), expected to find one remaining edge (to node 2). Found: {}'\
+        .format(n1['success'])
+
+    t.remove_edge(1, 2, 'success')
+    assert 'success' not in n1, \
+        "Removed last 'success' edge, but still found edges. Found: {}"\
+        .format(n1['success'])
+
+
+def fixture_for_equality_tests():
+    """Builds:
+       - `WorkflowTree` constructed based on (mock) `WorkflowJobTemplate` object, and
+       - Manually constructed `WorkflowTree`
+
+        Workflow:
+         n1:
+          - always: n2
+         n3:
+          - success: n4
+          - failure: n5
+         n6:
+          - always: n7
+            - always: n8
+              - always: n9
+         n10
+    """
+    # Build WorkflowTree from (mock) WorkflowJobTemplate
+    n1 = MockNode(1, always_nodes=[2])
+    n2 = MockNode(2)
+    n3 = MockNode(3, success_nodes=[4], failure_nodes=[5])
+    n4 = MockNode(4)
+    n5 = MockNode(5)
+    n6 = MockNode(6, always_nodes=[7])
+    n7 = MockNode(7, always_nodes=[8])
+    n8 = MockNode(8, always_nodes=[9])
+    n9 = MockNode(9)
+    n10 = MockNode(10)
+
+    nodes = [n1, n2, n3, n4, n5, n6, n7, n8, n9, n10]
+    wfjt = MockWorkflowJobTemplate(nodes)
+    wfjt_tree = WorkflowTree(wfjt)
+
+    # Build WorkflowTree 'manually'
+    tree = WorkflowTree()
+    tree.add_nodes(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    tree.add_edge(1, 2, 'always')
+    tree.add_edge(3, 4, 'success')
+    tree.add_edge(3, 5, 'failure')
+    tree.add_edge(6, 7, 'always')
+    tree.add_edge(7, 8, 'always')
+    tree.add_edge(8, 9, 'always')
+
+    return wfjt_tree, tree
+
+
+def test_equality_when_trees_are_equal():
+    """Test equality when trees have same structure."""
+    wfjt_tree, tree = fixture_for_equality_tests()
+
+    # Test (in)equality when trees are same
+    assert wfjt_tree == tree,\
+        ('Expected trees to be equal.' +
+         '\n\nWFJT Tree:\n\n{0}\n\n(Manually-built) Tree:\n\n{1}').format(wfjt_tree, tree)
+    assert not wfjt_tree != tree,\
+        ('Expected inequality to be false (i.e. trees to be same).' +
+         '\n\nWFJT Tree:\n\n{0}\n\n(Manually-built) Tree:\n\n{1}').format(wfjt_tree, tree)
+
+
+def test_equality_when_one_edge_missing():
+    """Test equality when one tree is missing an edge."""
+    wfjt_tree, tree = fixture_for_equality_tests()
+
+    # Trees should be different if edge is missing
+    tree.remove_edge(1, 2, 'always')
+    assert not wfjt_tree == tree,\
+        ('Expected trees to *not* be equal (missing edge).' +
+         '\n\nWFJT Tree:\n\n{0}\n\n(Manually-built) Tree:\n\n{1}').format(wfjt_tree, tree)
+    assert wfjt_tree != tree,\
+        ('Expected inequality to be true (i.e. trees to be different) due to missing edge.' +
+         '\n\nWFJT Tree:\n\n{0}\n\n(Manually-built) Tree:\n\n{1}').format(wfjt_tree, tree)
+
+
+def test_equality_when_extra_edge_present():
+    """Test equality when one tree has an extra edge."""
+    wfjt_tree, tree = fixture_for_equality_tests()
+
+    # Trees should be different if extra edge is present
+    tree.add_edge(9, 10, 'success')
+    assert not wfjt_tree == tree,\
+        ('Expected trees to *not* be equal (extra edge).' +
+         '\n\nWFJT Tree:\n\n{0}\n\n(Manually-built) Tree:\n\n{1}').format(wfjt_tree, tree)
+    assert wfjt_tree != tree,\
+        ('Expected inequality to be true (i.e. trees to be different) due to extra edge.' +
+         '\n\nWFJT Tree:\n\n{0}\n\n(Manually-built) Tree:\n\n{1}').format(wfjt_tree, tree)
+
+
+def test_equality_when_node_missing():
+    """Test equality when one tree is missing a node."""
+    wfjt_tree, tree = fixture_for_equality_tests()
+
+    # Trees should be different if node is missing
+    tree.remove_nodes(10)
+    assert not wfjt_tree == tree,\
+        ('Expected trees to *not* be equal (missing node).' +
+         '\n\nWFJT Tree:\n\n{0}\n\n(Manually-built) Tree:\n\n{1}').format(wfjt_tree, tree)
+    assert wfjt_tree != tree,\
+        ('Expected inequality to be true (i.e. trees to be different) due to missing node.' +
+         '\n\nWFJT Tree:\n\n{0}\n\n(Manually-built) Tree:\n\n{1}').format(wfjt_tree, tree)
+
+
+def test_equality_when_extra_node_present():
+    """Test equality when one tree has an extra node."""
+    wfjt_tree, tree = fixture_for_equality_tests()
+
+    # Trees should be different if extra node is present
+    tree.add_nodes(11)
+    assert not wfjt_tree == tree,\
+        ('Expected trees to *not* be equal (extra node).' +
+         '\n\nWFJT Tree:\n\n{0}\n\n(Manually-built) Tree:\n\n{1}').format(wfjt_tree, tree)
+    assert wfjt_tree != tree,\
+        ('Expected inequality to be true (i.e. trees to be different) due to extra.' +
+         '\n\nWFJT Tree:\n\n{0}\n\n(Manually-built) Tree:\n\n{1}').format(wfjt_tree, tree)
+
+
+############################
+# WorkflowTreeMapper tests #
+############################
+
+def fixture_for_simple_mapping_tests():
+    """Builds two trees of form:
+
+    Workflow:
+     n1:
+      - (always) n2
+     n3
+    """
+    tree1 = WorkflowTree()
+    tree1.add_nodes(1, 2, 3)
+    tree1.add_edge(1, 2, 'success')
+
+    tree2 = WorkflowTree()
+    tree2.add_nodes(11, 12, 13)
+    tree2.add_edge(11, 12, 'success')
+
+    return tree1, tree2
+
+
+def test_map_simple_trees():
+    """
+    Forms mapping between two simple trees
+    (that are identical in structure, but
+    have different node ids).
+    """
+    tree1, tree2 = fixture_for_simple_mapping_tests()
+    mapping = WorkflowTreeMapper(tree1, tree2).map()
+
+    assert mapping == [(1, 11), (2, 12), (3, 13)]
+
+
+def test_map_simple_trees_differing_by_single_node():
+    """Tests mapping trees that differ by single node."""
+    tree1, tree2 = fixture_for_simple_mapping_tests()
+    tree1.add_nodes(4)
+
+    mapping = WorkflowTreeMapper(tree1, tree2).map()
+    assert mapping is None
+
+
+def test_map_simple_trees_differing_by_single_edge():
+    """Tests mapping trees that differ by single edge."""
+    tree1, tree2 = fixture_for_simple_mapping_tests()
+    tree1.add_edge(1, 3, 'failure')
+
+    mapping = WorkflowTreeMapper(tree1, tree2).map()
+    assert mapping is None
+
+
+def test_map_trees_with_similar_root_nodes():
+    """
+    Forms mapping between two trees with similar
+    root nodes.
+
+    Workflow:
+     n1:
+      - (always) n2
+       - (success) n3
+       - (success) n4
+        - (failure) n5
+     n6:
+      - (always) n7
+       - (failure) n8
+    """
+    tree1 = WorkflowTree()
+    tree1.add_nodes(1, 2, 3, 4, 5, 6, 7, 8)
+    tree1.add_edge(1, 2, 'always')
+    tree1.add_edge(2, 3, 'success')
+    tree1.add_edge(2, 4, 'success')
+    tree1.add_edge(4, 5, 'failure')
+    tree1.add_edge(6, 7, 'always')
+    tree1.add_edge(7, 8, 'failure')
+
+    tree2 = WorkflowTree()
+    tree2.add_nodes(11, 12, 13, 14, 15, 16, 17, 18)
+    tree2.add_edge(11, 12, 'always')
+    tree2.add_edge(12, 13, 'success')
+    tree2.add_edge(12, 14, 'success')
+    tree2.add_edge(14, 15, 'failure')
+    tree2.add_edge(16, 17, 'always')
+    tree2.add_edge(17, 18, 'failure')
+
+    mapping = WorkflowTreeMapper(tree1, tree2).map()
+
+    assert mapping == [(1, 11), (2, 12), (3, 13), (4, 14),
+                       (5, 15), (6, 16), (7, 17), (8, 18)]
