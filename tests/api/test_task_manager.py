3de2a5097f4949398bc9ad9b38775bca8d55bec8
Christopher Wang <cwang@ansible.com>
2016-12-13 17:03:28 -0500

    Corrected faulty test logic. Added related project/inventory tests.

--- tests/api/test_task_manager.py
+++ tests/api/test_task_manager.py
@@ -6,16 +6,22 @@ from tests.api import Base_Api_Test
 
 def check_sequential_jobs(jobs):
     """Helper function that will check that jobs ran sequentially. How this works:
+    * Wait for jobs to finish
     * Sort jobs by start time.
     * Check that each job finishes before the next job starts.
 
     :jobs: A list of job page objects.
     """
+    # wait for jobs to finish
+    for job in jobs:
+        job.wait_until_completed()
+
+    # check that jobs ran sequentially
     intervals = [[job.started, job.finished] for job in jobs]
     sorted_intervals = sorted(intervals, key=lambda x: x[0])
     for i in range(1, len(sorted_intervals)):
-        assert sorted_intervals[i-1][1] <= sorted_intervals[i][0], \
-            "We have overlapping jobs. Format: [job.started, job.finished]."
+        assert sorted_intervals[i-1][1] < sorted_intervals[i][0], \
+            "Job overlap found: we have an instance where one job starts before previous job finishes."
 
 
 @pytest.mark.api
@@ -32,7 +38,8 @@ class Test_Task_Manager(Base_Api_Test):
         """
         # launch three updates
         for _ in range(3):
-            project.update().wait_until_completed()
+            project.update()
+        updates = project.related.project_updates.get()
 
         # check that we have no overlapping project updates
         updates = project.related.project_updates.get()
@@ -45,22 +52,11 @@ class Test_Task_Manager(Base_Api_Test):
         """
         # launch three updates
         for _ in range(3):
-            custom_inventory_source.update().wait_until_completed()
+            custom_inventory_source.update()
 
         # check that we have no overlapping inventory updates
         updates = custom_inventory_source.related.inventory_updates.get()
         check_sequential_jobs(updates.results)
-
-    def test_system_job(self, system_jobs):
-        """Launch all three of our system jobs. Assert no system job was running when another system
-        job was running.
-        """
-        # wait for system jobs to finish
-        for job in system_jobs:
-            job.wait_until_completed()
-
-        # check that we have no overlapping system jobs
-        check_sequential_jobs(system_jobs)
 
     def test_job_template(self, job_template):
         """Launch several jobs using the same JT. Assert no job was running when another job was
@@ -68,15 +64,15 @@ class Test_Task_Manager(Base_Api_Test):
         """
         # launch three jobs
         for _ in range(3):
-            job_template.launch().wait_until_completed()
+            job_template.launch()
 
         # check that we have no overlapping jobs
         jobs = job_template.related.jobs.get()
         check_sequential_jobs(jobs.results)
 
     def test_job_template_with_allow_simultaneous(self, job_template):
-        """Launch two jobs using the same JT with allow_simultaneous. Assert that we have overlapping
-        jobs.
+        """Launch two jobs using the same JT with allow_simultaneous enabled. Assert that we have
+        overlapping jobs.
         """
         job_template.patch(allow_simultaneous=True, playbook="sleep.yml", extra_vars=dict(sleep_interval=10))
 
@@ -87,8 +83,42 @@ class Test_Task_Manager(Base_Api_Test):
         job_2.wait_until_completed()
 
         # check that we have overlapping jobs
-        # assert that job#1 started before job#2 finished
+        # assert that job_1 started before job_2 finished
         assert du_parse(job_1.started) < du_parse(job_2.finished)
 
-        # assert that job#1 finished after job#2 started
+        # assert that job_1 finished after job_2 started
         assert du_parse(job_1.finished) > du_parse(job_2.started)
+
+    def test_system_job(self, system_jobs):
+        """Launch all three of our system jobs. Assert no system job was running when another system
+        job was running.
+        """
+        # check that we have no overlapping system jobs
+        check_sequential_jobs(system_jobs)
+
+    def test_related_project_update(self, job_template):
+        """If a project is used in a JT, then spawned jobs and updates must run sequentially.
+        """
+        project = job_template.related.project.get()
+
+        # launch jobs
+        job = job_template.launch()
+        update = project.update()
+
+        # check that our update and job ran sequentially
+        jobs = [job, update]
+        check_sequential_jobs(jobs)
+
+    def test_related_inventory_update(self, job_template, custom_group):
+        """If an inventory is used in a JT and has a group that allows for updates, then spawned
+        jobs and updates must run sequentially.
+        """
+        inv_source = custom_group.related.inventory_source.get()
+
+        # launch jobs
+        job = job_template.launch()
+        update = inv_source.update()
+
+        # check that our update and job ran sequentially
+        jobs = [job, update]
+        check_sequential_jobs(jobs)
