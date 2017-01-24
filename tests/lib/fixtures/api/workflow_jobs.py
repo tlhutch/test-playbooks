0699d99e62506ef3776bafb23803fc6ea28fe5d6
Christopher Wang <cwang@ansible.com>
2017-01-24 14:36:02 -0500

    Return WFJ in WFJ fixture

--- tests/lib/fixtures/api/workflow_jobs.py
+++ tests/lib/fixtures/api/workflow_jobs.py
@@ -3,4 +3,4 @@ import pytest
 
 @pytest.fixture
 def workflow_job_with_status_completed(request, workflow_job_template):
-    workflow_job_template.launch().wait_until_completed()
+    return workflow_job_template.launch().wait_until_completed()
