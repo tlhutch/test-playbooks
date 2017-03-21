89b8782f7f23930c233f07bc57c50ec2e699322e
Christopher Wang <cwang@ansible.com>
2017-03-21 17:36:20 -0400

    Apply factories to team fixtures

--- tests/lib/fixtures/api/teams.py
+++ tests/lib/fixtures/api/teams.py
@@ -1,23 +1,17 @@
 import pytest
 import fauxfactory
-import towerkit.exceptions
 
 
 @pytest.fixture(scope="function")
-def team(request, authtoken, organization):
-    payload = dict(name="team_%s" % fauxfactory.gen_utf8(),
-                   description="Random Team (%s)" % fauxfactory.gen_utf8(),
-                   organization=organization.id,)
-    obj = organization.get_related('teams').post(payload)
-    request.addfinalizer(obj.silent_delete)
-    return obj
+def team(organization, factories):
+    team = factories.team(name="team_%s" % fauxfactory.gen_utf8(),
+                          description="Random Team (%s)" % fauxfactory.gen_utf8(),
+                          organization=organization)
+    return team
 
 
 @pytest.fixture(scope="function")
-def team_with_org_admin(request, team, org_admin):
+def team_with_org_admin(team, org_admin):
     """A team with an org_admin as a member"""
-    users_pg = team.get_related('users')
-    payload = dict(id=org_admin.id)
-    with pytest.raises(towerkit.exceptions.NoContent):
-        users_pg.post(payload)
+    team.set_object_roles(org_admin, "member")
     return team
