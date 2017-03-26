7e481d8b1a91e8fcd500ca457246497c2ffeaa6b
Jake McDermott <jmcdermott@ansible.com>
2017-03-26 15:56:55 -0400

    update jenkins runtest..

--- scripts/jenkins_runtest.sh
+++ scripts/jenkins_runtest.sh
@@ -19,29 +19,9 @@ ANSIBLE_VERSION=$(python -c 'import ansible; print ansible.__version__;')
 JUNIT_XML=${JUNIT_XML:-tests/results.xml}
 HTML_REPORT=${HTML_REPORT:-tests/results/index.html}
 
-# Determine --base-url parameter using `ansible --list-hosts`
-if [ -f "${ANSIBLE_INVENTORY}" ]; then
-    INVENTORY_HOST=$(ansible "${PLATFORM}:&${CLOUD_PROVIDER}" -i ${ANSIBLE_INVENTORY} --list-hosts)
-    if [ $? -ne 0 ] || [ "${INVENTORY_HOST}" == *"provided host list is empty"* ]; then
-        echo "Unable to find a matching host: ${PLATFORM}:&${CLOUD_PROVIDER}"
-        exit 1
-    fi
-    IFSBAK="${IFS}" IFS=$'\n'
-    INVENTORY_HOST_ARRAY=(${INVENTORY_HOST})            # Parse lines into array
-    if [[ "${ANSIBLE_VERSION}" = 2* ]]; then            #Ansible v2 includes summary line (remove if present)
-        INVENTORY_HOST_ARRAY=("${INVENTORY_HOST_ARRAY[@]:1}")
-    fi
-    if [ ${#INVENTORY_HOST_ARRAY[@]} -ne 1 ]; then      # Confirm one host listed 
-        echo "Host list does not contain exactly one host"
-        exit 1
-    fi
-    INVENTORY_HOST="${INVENTORY_HOST_ARRAY[0]}"
-    INVENTORY_HOST="${INVENTORY_HOST//[[:space:]]/}"    # remove whitespace
-    IFS="${IFSBAK}"
-else
-    echo "Ansible inventory file not found: ${ANSIBLE_INVENTORY}"
-    exit 1
-fi
+DEFAULT_INVENTORY_GROUP="${PLATFORM}:&${CLOUD_PROVIDER}"
+INVENTORY_GROUP=${INVENTORY_GROUP:-$DEFAULT_INVENTORY_GROUP}
+INVENTORY_HOST=$(ansible -i ${ANSIBLE_INVENTORY} --list-hosts ${INVENTORY_GROUP} | tail -n 1 | awk 'NR==1{print $1}')
 
 export ANSIBLE_NOCOLOR=True
 export ANSIBLE_HOST_KEY_CHECKING=False
