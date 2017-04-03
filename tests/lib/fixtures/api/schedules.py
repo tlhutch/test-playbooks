608fe763888f278e92145c91034c328cd579d6c6
Christopher Wang <cwang@ansible.com>
2017-04-03 15:44:52 -0400

    Update schedule fixtures for add_schedule

--- tests/lib/fixtures/api/schedules.py
+++ tests/lib/fixtures/api/schedules.py
@@ -1,5 +1,4 @@
 import pytest
-import fauxfactory
 
 
 @pytest.fixture(scope="function", params=["project", "custom_inventory_source", "job_template", "cleanup_jobs_template"])
@@ -15,22 +14,16 @@ def schedulable_resource_as_org_admin(request):
 @pytest.fixture(scope="function", params=["project", "custom_inventory_source", "job_template", "cleanup_jobs_template"])
 def resource_with_schedule(request):
     """Returns all schedulable Tower resources with a built-in schedule."""
-    payload = dict(name="Schedule - %s" % fauxfactory.gen_utf8(),
-                   rrule="DTSTART:20160926T040000Z RRULE:FREQ=HOURLY;INTERVAL=1")
     resource = request.getfuncargvalue(request.param)
-    schedules_pg = resource.get_related('schedules')
-    schedule_pg = schedules_pg.post(payload)
-    request.addfinalizer(schedule_pg.silent_delete)
+    schedule = resource.add_schedule()
+    if request.param == "cleanup_jobs_template":
+        request.addfinalizer(schedule.silent_delete)
     return resource
 
 
 @pytest.fixture(scope="function", params=["project", "custom_inventory_source", "job_template"])
 def organization_resource_with_schedule(request):
     """Returns all organization-scoped schedulable Tower resources with a built-in schedule."""
-    payload = dict(name="Schedule - %s" % fauxfactory.gen_utf8(),
-                   rrule="DTSTART:20160926T040000Z RRULE:FREQ=HOURLY;INTERVAL=1")
     resource = request.getfuncargvalue(request.param)
-    schedules_pg = resource.get_related('schedules')
-    schedule_pg = schedules_pg.post(payload)
-    request.addfinalizer(schedule_pg.silent_delete)
+    resource.add_schedule()
     return resource
