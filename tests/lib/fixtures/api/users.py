8425b3d5ece1f839036d3678f849f43aeaf965a7
Christopher Wang <cwang@ansible.com>
2017-04-04 21:19:38 -0400

    Changes: * Refactor test_credentials_rbac.py * Fix test_user_credential_role_assignment

--- tests/lib/fixtures/api/users.py
+++ tests/lib/fixtures/api/users.py
@@ -7,7 +7,7 @@ import pytest
 
 @pytest.fixture(scope="class")
 def admin_user(authtoken, api_users_pg):
-    return api_users_pg.get(username__iexact='admin').results[0]
+    return api_users_pg.get(username__iexact='admin').results.pop()
 
 
 @pytest.fixture(scope="function")
