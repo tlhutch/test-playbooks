c71b8c0bb6a3f2ac1b8d51a0b6a6b24c490c818a
Christopher Wang <cwang@ansible.com>
2017-04-04 21:40:29 -0400

    Refactor test_system_jobs_rbac.py

--- tests/api/rbac/test_system_jobs_rbac.py
+++ tests/api/rbac/test_system_jobs_rbac.py
@@ -18,34 +18,34 @@ class Test_System_Jobs_RBAC(Base_Api_Test):
         system_job.get()
 
     @pytest.mark.fixture_args(days=1000, granularity='1y', older_than='1y')
-    def test_get_list_view_as_non_superuser(self, non_superusers, user_password, api_system_jobs_pg):
+    def test_get_list_view_as_non_superuser(self, non_superusers, api_system_jobs_pg):
         """Verify that non-superuser accounts are unable to access the top-level
         system jobs endpoint (/api/v1/system_jobs/).
         """
         for non_superuser in non_superusers:
-            with self.current_user(non_superuser.username, user_password):
+            with self.current_user(username=non_superuser.username, password=non_superuser.password):
                 with pytest.raises(towerkit.exceptions.Forbidden):
                     api_system_jobs_pg.get()
 
     @pytest.mark.fixture_args(days=1000, granularity='1y', older_than='1y')
-    def test_get_detail_view_as_non_superuser(self, non_superusers, user_password, system_job):
+    def test_get_detail_view_as_non_superuser(self, non_superusers, system_job):
         """Verify that non-superuser accounts are unable to access nested system
         job endpoints (/api/v1/system_jobs/N/).
         """
         for non_superuser in non_superusers:
-            with self.current_user(non_superuser.username, user_password):
+            with self.current_user(username=non_superuser.username, password=non_superuser.password):
                 with pytest.raises(towerkit.exceptions.Forbidden):
                     system_job.get()
 
     def test_user_capabilities_as_superuser(self, cleanup_jobs_with_status_completed, api_system_jobs_pg):
         """Verify 'user_capabilities' with a superuser."""
-        check_user_capabilities(cleanup_jobs_with_status_completed.get(), "superuser")
+        check_user_capabilities(cleanup_jobs_with_status_completed, "superuser")
         check_user_capabilities(api_system_jobs_pg.get(id=cleanup_jobs_with_status_completed.id).results.pop(), "superuser")
 
     def test_launch_as_auditor(self, factories, system_job_template):
         """Confirms that a system auditor cannot launch system jobs"""
         user = factories.user()
         user.is_system_auditor = True
-        with self.current_user(user.username, user.password):
+        with self.current_user(username=user.username, password=user.password):
             with pytest.raises(towerkit.exceptions.Forbidden):
                 system_job_template.launch().wait_until_completed()
