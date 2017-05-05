dc142bd44b9cfc86a8685ad3d469e1df454967b9
Christopher Wang <cwang@ansible.com>
2017-05-05 16:35:34 -0400

    Changes: * Rework our /api/v1/auth/ test module. * Create new update auth settings fixture.

--- tests/lib/fixtures/api/settings.py
+++ tests/lib/fixtures/api/settings.py
@@ -1,4 +1,5 @@
 import pytest
+import fauxfactory
 
 
 @pytest.fixture
@@ -19,12 +20,33 @@ def update_setting_pg(request):
     return func
 
 
+@pytest.fixture(scope="function", params=["api_settings_azuread_pg",
+                                          "api_settings_github_pg",
+                                          "api_settings_github_org_pg",
+                                          "api_settings_github_team_pg",
+                                          "api_settings_google_pg"])
+def sso_settings_pg(request):
+    """Returns each of our SSO settings pages."""
+    return request.getfuncargvalue(request.param)
+
+
 @pytest.fixture
-def oauth_settings_pgs(api_settings_github_pg, api_settings_github_org_pg, api_settings_github_team_pg, api_settings_google_pg,
-                       api_settings_azuread_pg):
-    """Returns a list of all of our oauth settings pages. Fixture current with Tower-3.1.1."""
-    return [api_settings_github_pg, api_settings_github_org_pg, api_settings_github_team_pg, api_settings_google_pg,
-            api_settings_azuread_pg]
+def configure_auth(update_setting_pg):
+    """Configure an authentication service with dummy settings."""
+    def func(settings_pg):
+        v = fauxfactory.gen_utf8()
+        if settings_pg.endpoint.endswith("azuread-oauth2/"):
+            payload = {k: v for k in ["SOCIAL_AUTH_AZUREAD_OAUTH2_KEY", "SOCIAL_AUTH_AZUREAD_OAUTH2_SECRET"]}
+        elif settings_pg.endpoint.endswith("github/"):
+            payload = {k: v for k in ["SOCIAL_AUTH_GITHUB_KEY", "SOCIAL_AUTH_GITHUB_SECRET"]}
+        elif settings_pg.endpoint.endswith("github-org/"):
+            payload = {k:v for k in ["SOCIAL_AUTH_GITHUB_ORG_KEY", "SOCIAL_AUTH_GITHUB_ORG_SECRET", "SOCIAL_AUTH_GITHUB_ORG_NAME"]}
+        elif settings_pg.endpoint.endswith("github-team/"):
+            payload = {k:v for k in ["SOCIAL_AUTH_GITHUB_TEAM_KEY", "SOCIAL_AUTH_GITHUB_TEAM_SECRET", "SOCIAL_AUTH_GITHUB_TEAM_ID"]}
+        elif settings_pg.endpoint.endswith("google-oauth2/"):
+            payload = {k:v for k in ["SOCIAL_AUTH_GOOGLE_OAUTH2_KEY", "SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET"]}
+        update_setting_pg(settings_pg, payload)
+    return func
 
 
 @pytest.fixture(scope="function", params=["all", "auth", "azuread", "changed", "github_org", "github_team", "google", "jobs",
