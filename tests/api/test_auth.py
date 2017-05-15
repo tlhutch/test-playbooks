81416bc12240bdd7c0b2dee361f50cd9ed2c6557
Christopher Wang <cwang@ansible.com>
2017-05-15 15:35:05 -0400

    Update test_auth.py for non-existent fixture.

--- tests/api/test_auth.py
+++ tests/api/test_auth.py
@@ -25,22 +25,20 @@ class Test_Auth(Base_Api_Test):
         assert auth.json == {}, \
             "Unexpected value for /api/v1/auth/."
 
-    def test_updated_entries(self, v1, sso_settings_pg, configure_auth):
+    def test_updated_entries(self, v1, configured_auth):
         """Update sso settings page and verify that /api/v1/auth/ updates accordingly."""
-        configure_auth(sso_settings_pg)
-
         # assert expected entry found
         auth = v1.walk('/api/v1/auth/')
         assert len(auth.json) == 1, \
             "Expected to have one result under /api/v1/auth/."
-        endpoint_name = sso_settings_pg.endpoint.replace('/api/v1/settings/', '').strip('/')
+        endpoint_name = configured_auth.endpoint.replace('/api/v1/settings/', '').strip('/')
         assert endpoint_name in auth.json, \
             "Unexpected result found with /api/v1/auth/. Expected '{0}.'".format(endpoint_name)
         # assert entry contains expected contents
         assert auth.json[endpoint_name]["login_url"] == "/sso/login/" + endpoint_name + "/", \
             "Unexpected value for login_url."
-        callback_setting = find_settings(sso_settings_pg, ['CALLBACK']).pop()
-        assert auth.json[endpoint_name]["complete_url"] == sso_settings_pg.json[callback_setting]
+        callback_setting = find_settings(configured_auth, ['CALLBACK']).pop()
+        assert auth.json[endpoint_name]["complete_url"] == configured_auth.json[callback_setting]
 
     def test_reset_entries(self, v1):
         """Our /api/v1/auth/ endpoint should not contain any entries after settings reset."""
