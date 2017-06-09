e92f8d1d23bb30f4db67d09db7764ea6884d5959
Jim Ladd <jladd@ansible.com>
2017-06-09 16:22:01 -0400

    Test instance group creation (#1286)

---
+++ scripts/ansible_inventory_to_json.py
@@ -0,0 +1,41 @@
+#!/bin/env python
+
+import argparse
+import json
+import os
+import re
+import subprocess
+
+discard_host_strings = ['hosts']
+cwd = os.path.dirname(__file__)
+
+parser = argparse.ArgumentParser(description='Prints json mapping representing groups in Ansible inventory.')
+parser.add_argument('--inventory', dest='inventory_file', help='Ansible inventory',
+                    default=os.path.join(cwd, 'inventory'))
+_group_filter_help = "(optional) Comma-separated list of strings. If a group's name includes one of the" \
+                     "filter strings, it will be included in the results. If not specified, all " \
+                     "groups are returned."
+parser.add_argument('--group-filter', dest='group_filter', help=_group_filter_help, default='')
+args = parser.parse_args()
+
+group_map = dict()
+
+# Get group names
+file = open(args.inventory_file, 'r')
+for line in file:
+    match = re.search('\[(.*)\]', line)
+    if not match or 'vars' in match.group(1):
+        continue
+    if args.group_filter and not any(expr in match.group(1) for expr in args.group_filter.split(',')):
+        continue
+    section = match.group(1)
+    group_map[section] = []
+
+    # Get hosts
+    output = subprocess.check_output(['ansible', '-i', args.inventory_file, '--list-hosts', section])
+    for line in output.split('\n'):
+        line = line.strip()
+        if len(line) and not any(expr in line for expr in discard_host_strings):
+            group_map[section].append(line)
+
+print(json.dumps(group_map))
---
+++ tests/api/test_instance_groups.py
@@ -0,0 +1,33 @@
+import json
+import os
+import re
+
+import pytest
+
+from tests.api import Base_Api_Test
+
+
+@pytest.mark.api
+@pytest.mark.skip_selenium
+class Test_Instance_Groups(Base_Api_Test):
+
+    def test_instance_group_creation(self, authtoken, v2, ansible_runner):
+        inventory_path = os.environ.get('TQA_INVENTORY_FILE_PATH', '/tmp/setup/inventory')
+        cmd = 'scripts/ansible_inventory_to_json.py --inventory {0} --group-filter tower,instance_group_'.format(inventory_path)
+        contacted = ansible_runner.script(cmd)
+        assert len(contacted.values()) == 1, "Failed to run script against Tower instance"
+
+        group_mapping = json.loads(contacted.values().pop()['stdout'])
+        for group in group_mapping.keys():
+            match = re.search('instance_group_(.*)', group)
+            if match:
+                group_mapping[match.group(1)] = group_mapping.pop(group)
+
+        instance_groups = [group.name for group in v2.instance_groups.get().results]
+        assert len(instance_groups) == len(group_mapping.keys())
+        assert set(instance_groups) == set(group_mapping.keys())
+
+        for group in v2.instance_groups.get().results:
+            instances = [instance.hostname for instance in group.get_related('instances').results]
+            assert len(instances) == len(group_mapping[group.name])
+            assert set(instances) == set(group_mapping[group.name])
