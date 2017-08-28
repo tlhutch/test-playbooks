d06edc9730417de3e2c5085d1829158f7303de5b
Christopher Wang <cwang@ansible.com>
2017-08-28 11:30:13 -0400

    Add in coverage for launching jobs via /api/v1/jobs/.

--- tests/api/rbac/test_jobs_rbac.py
+++ tests/api/rbac/test_jobs_rbac.py
@@ -10,22 +10,22 @@ from tests.api import Base_Api_Test
 @pytest.mark.usefixtures('authtoken', 'install_enterprise_license_unlimited')
 class TestJobsRBAC(Base_Api_Test):
 
-    @pytest.mark.github('https://github.com/ansible/ansible-tower/issues/7527')
-    def test_v1_post_as_superuser(self, job_template, api_jobs_pg):
-        """Verify a superuser is able to create a job via POST to the /api/v1/jobs/ endpoint."""
+    def test_v1_launch_as_superuser(self, job_template, api_jobs_pg):
+        """Verify job creation via /api/v1/jobs/ and job start via /api/v2/jobs/N/start/."""
         job = api_jobs_pg.post(job_template.json)
         assert job.status == 'new'
+        job.related.start.post()
         assert job.wait_until_completed().is_successful
 
-    def test_v1_post_as_non_superuser(self, non_superusers, api_jobs_pg, job_template):
+    def test_v1_launch_as_non_superuser(self, job_template, non_superusers, api_jobs_pg):
         """Verify a non-superuser is unable to create a job via POST to the /api/v1/jobs/ endpoint."""
         for non_superuser in non_superusers:
             with self.current_user(non_superuser):
                 with pytest.raises(exc.Forbidden):
                     api_jobs_pg.post(job_template.json)
 
-    def test_v2_post_as_all_users(self, factories, v2, all_users):
-        """Creating jobs via post to /api/v2/jobs/ is not allowed."""
+    def test_v2_launch_as_all_users(self, factories, v2, all_users):
+        """Creating jobs via post to /api/v2/jobs/ should raise 405."""
         for user in all_users:
             with self.current_user(user):
                 with pytest.raises(exc.MethodNotAllowed):
