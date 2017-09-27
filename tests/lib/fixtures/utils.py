18f142f24396edad4f61ec5cc5f9f58fad1dea23
Ryan Fitzpatrick <rfitzpat@redhat.com>
2017-09-27 14:03:35 -0400

    update is_docker for 2.4

--- tests/lib/fixtures/utils.py
+++ tests/lib/fixtures/utils.py
@@ -19,7 +19,8 @@ def class_subrequest(request):
 def is_docker(ansible_module_cls):
     try:
         manager = ansible_module_cls.inventory_manager
-        tower_hosts = manager.get_group_dict().get('tower')
+        groups_dict = manager.get_group_dict() if hasattr(manager, 'get_group_dict') else manager.get_groups_dict()
+        tower_hosts = groups_dict.get('tower')
         return manager.get_host(tower_hosts[0]).get_vars().get('ansible_connection') == 'docker'
     except (TypeError, IndexError):
         return False
