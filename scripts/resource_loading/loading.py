9c288271ca6bcc62c411d25755a88d5098c528b4
Ryan Fitzpatrick <rfitzpat@redhat.com>
2017-10-18 10:23:01 -0400

    Tie missing --azure help

--- scripts/resource_loading/loading.py
+++ scripts/resource_loading/loading.py
@@ -51,7 +51,7 @@ _validate_help = 'Enable schema validation (default: False)'
 parser.add_argument('--validate', '-v', dest='validate', action='store_true', help=_validate_help)
 
 _azure_help = "Don't fail if azure-related inventory resources are missing."
-parser.add_argument('--no-azure', dest='no_azure', action='store_true')
+parser.add_argument('--no-azure', dest='no_azure', action='store_true', help=_azure_help)
 args = parser.parse_args()
 
 loader = DataLoader()
