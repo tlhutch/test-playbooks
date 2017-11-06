ef85cbe292bc8eb2cd5a9f87a6c4266432716fba
Ryan Fitzpatrick <rfitzpat@redhat.com>
2017-11-06 15:43:50 -0500

    adopt mp_trail for apply_license and crawler corrections

--- tests/lib/fixtures/api/license.py
+++ tests/lib/fixtures/api/license.py
@@ -1,6 +1,5 @@
 from contextlib import contextmanager
 import logging
-import os
 
 import pytest
 
@@ -9,7 +8,7 @@ log = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope='session')
-def apply_license(api, mp_message_board, mp_lock):
+def apply_license(api, mp_trail, mp_message_board):
     """Create a context manager for on-the-fly license switching. The initial
     license intallation state is retained after exiting.
     """
@@ -36,67 +35,43 @@ def apply_license(api, mp_message_board, mp_lock):
         """
         request = kwargs.pop('request', False)
 
-        config = api.current_version.get().config.get()
-        initial_info = config.license_info
+        license_hash = str(license_type) # TODO: make a license-specific hash
+        initial_info_key = license_hash + '_initial_info'
 
         def teardown_license():
-            log.info('Restoring initial license.')
-            if not initial_info:
-                config.delete()
-            else:
-                initial_info['eula_accepted'] = True
-                config.post(initial_info)
-
-        pid = os.getpid()
-        try:
-            log.info('{} Waiting for setup lock'.format(pid))
-            with mp_lock:
-                log.info('{} HAS LOCK: {} {}'.format(pid, id(mp_message_board), mp_message_board))
-                if 'apply_license_current_type' not in mp_message_board:
-                    log.info('{} ESTABLISHING APPLY_LICENSE: {}'.format(pid, mp_message_board))
-                    mp_message_board['apply_license_current_type'] = None
-                    mp_message_board['apply_license_previous_info'] = {}
-                    mp_message_board['apply_license_consumers'] = 0
-
-                if mp_message_board['apply_license_current_type'] != license_type \
-                   and mp_message_board['apply_license_consumers'] == 0:
-                    log.info('{} APPLYING LICENSE: {}'.format(pid, mp_message_board))
-                    initial_info = config.license_info
-                    mp_message_board['apply_license_previous_info'] = initial_info
-                    log.info('{} STORING PREVIOUS INFO: {}'.format(pid, mp_message_board))
-                    if license_type is None:
-                        log.info('{} Deleting current license...')
+            with mp_trail(license_hash, 'finish') as finish:
+                if finish:
+                    config = api.current_version.get().config.get()
+                    initial_info = mp_message_board[initial_info_key]
+                    log.info('Restoring initial license.')
+                    if not initial_info:
                         config.delete()
                     else:
-                        log.info('{} Applying {} license...'.format(pid, license_type))
+                        initial_info['eula_accepted'] = True
+                        config.post(initial_info)
+                    del mp_message_board[initial_info_key]
+
+        try:
+            with mp_trail(license_hash, 'start') as start:
+                if start:
+                    config = api.current_version.get().config.get()
+                    mp_message_board[initial_info_key] = config.license_info
+
+                    if license_type is None:
+                        log.info('Deleting current license...')
+                        config.delete()
+                    else:
+                        log.info('Applying {} license...'.format(license_type))
                         config.install_license(license_type=license_type, days=days, **kwargs)
-                    if request:
-                        # We need to explictly register teardowns instead of yield-driven
-                        # teardown for class_factory teardown ordering
-                        request.addfinalizer(teardown_license)
-                    mp_message_board['apply_license_current_type'] = license_type
-                    log.info('{} set current_type: {}'.format(pid, mp_message_board))
-                else:
-                    log.info('{} LICENSE ALREADY APPLIED: {}'.format(pid, mp_message_board))
+            if request:
+                # We need to explictly register teardowns instead of yield-driven
+                # teardown for class_factory teardown ordering
+                request.addfinalizer(teardown_license)
 
-                mp_message_board['apply_license_consumers'] += 1
-
-            log.info('{} Released setup lock'.format(pid))
-            yield config.get().license_info
+            yield
         finally:
             if not request:
-                log.info('{} Waiting for teardown lock'.format(pid))
-                with mp_lock:
-                    log.info('IN TEARDOWN: {}'.format(mp_message_board))
-                    current_type = mp_message_board['apply_license_current_type']
-                    previous_info = mp_message_board['apply_license_previous_info']
-
-                    mp_message_board['apply_license_consumers'] -= 1
-                    log.info('DECREMENTED CONSUMERS {}'.format(mp_message_board['apply_license_consumers']))
-                    if current_type != previous_info.get('license_type') and mp_message_board['apply_license_consumers'] == 0:
-                        teardown_license()
-                        mp_message_board['apply_license_current_type'] = previous_info.get('license_type')
-                log.info('{} Released teardown lock'.format(pid))
+                teardown_license()
 
     return _apply_license
 
