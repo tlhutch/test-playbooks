d27e4cec63b6c8b1dd28ed8cf739027f7a71761e
Ryan Fitzpatrick <rfitzpat@redhat.com>
2018-01-19 15:53:35 -0500

    Add eslint

--- tests/ui/nightwatch.conf.js
+++ tests/ui/nightwatch.conf.js
@@ -2,9 +2,9 @@ import path from 'path';
 
 const resolve = location => path.resolve(__dirname, location);
 
-const AWX_E2E_LAUNCH_URL = process.env.AWX_E2E_LAUNCH_URL
-const AWX_E2E_PASSWORD = process.env.AWX_E2E_PASSWORD 
-const AWX_E2E_USERNAME = process.env.AWX_E2E_USERNAME;
+const { AWX_E2E_LAUNCH_URL } = process.env;
+const { AWX_E2E_PASSWORD } = process.env;
+const { AWX_E2E_USERNAME } = process.env;
 const AWX_E2E_TIMEOUT_ASYNC = 30000;
 const AWX_E2E_TIMEOUT_LONG = 10000;
 const AWX_E2E_TIMEOUT_MEDIUM = 5000;
@@ -13,36 +13,36 @@ const AWX_E2E_TIMEOUT_SHORT = 1000;
 module.exports = {
     custom_commands_path: resolve('./awx/awx/ui/test/e2e/commands'),
     page_objects_path: resolve('./awx/awx/ui/test/e2e/objects'),
-    src_folders: ["tests"],
-    output_folder: "reports",
-    globals_path: "",
+    src_folders: ['tests'],
+    output_folder: 'reports',
+    globals_path: '',
     selenium: {
         start_process: true,
-        server_path: "./bin/selenium-server-standalone.jar",
-        log_path: "",
+        server_path: './bin/selenium-server-standalone.jar',
+        log_path: '',
         port: 4444,
         cli_args: {
-            "webdriver.chrome.driver": "./awx/awx/ui/node_modules/chromedriver/bin/chromedriver"
+            'webdriver.chrome.driver': './awx/awx/ui/node_modules/chromedriver/bin/chromedriver'
         }
     },
     test_settings: {
         default: {
             globals: {
+                waitForConditionTimeout: AWX_E2E_TIMEOUT_MEDIUM,
                 launch_url: AWX_E2E_LAUNCH_URL,
-                AWX_E2E_TIMEOUT_LONG: AWX_E2E_TIMEOUT_LONG,
-                AWX_E2E_TIMEOUT_MEDIUM: AWX_E2E_TIMEOUT_MEDIUM,
-                AWX_E2E_TIMEOUT_SHORT: AWX_E2E_TIMEOUT_SHORT,
-                waitForConditionTimeout: AWX_E2E_TIMEOUT_MEDIUM
+                AWX_E2E_TIMEOUT_LONG,
+                AWX_E2E_TIMEOUT_MEDIUM,
+                AWX_E2E_TIMEOUT_SHORT
             },
             selenium_port: 4444,
-            selenium_host: "localhost",
+            selenium_host: 'localhost',
             silent: true,
             screenshots: {
                 enabled: false,
-                path: ""
+                path: ''
             },
             desiredCapabilities: {
-                browserName: "chrome"
+                browserName: 'chrome'
             }
         }
     }
