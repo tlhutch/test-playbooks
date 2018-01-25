6658b0b34dcf9050f90423bc2fd9945fa480ceef
Ryan Fitzpatrick <rfitzpat@redhat.com>
2018-01-25 16:22:16 -0500

    Add SAML login test and update configuration

--- tests/ui/tests/saml_login.js
+++ tests/ui/tests/saml_login.js
@@ -1,42 +1,72 @@
 const { exec } = require('child_process');
 
+const { env } = process;
+
 module.exports = {
     before: (client, done) => {
-        process.stdout.write('IN BEGIN!');
-        let env = process.env;
         env.TOWERKIT_USER = client.globals.username;
         env.TOWERKIT_USER_PASSWORD = client.globals.password;
         exec(
-            `tkit -l -t ${client.globals.launch_url} -f ../scripts/tkit/configure_saml.py --saml-user saml_user --saml-password fo0m4nchU -x `,
+            `tkit -xl -t ${client.globals.launch_url} -f scripts/tkit/configure_saml.py`,
             { env },
             (err, stdout, stderr) => {
                 process.stdout.write(JSON.stringify(err));
                 process.stdout.write(stdout);
                 process.stdout.write(stderr);
-                done(30000);
-            });
+                done();
+            }
+        );
+        client.waitForAngular();
     },
-    'open ipsilon SSO': client => {
-        const loginPage = client.page.login().navigate();
-        loginPage.waitForAngular();
-        client
+    after: (client, done) => {
+        exec(
+            'echo "v2.settings.get().get_setting(\'saml\').delete(); ' +
+            'v2.users.get(username=\'saml_user\').results.pop().delete()" ' +
+            `| tkit -t ${client.globals.launch_url}`,
+            { env },
+            (err, stdout, stderr) => {
+                process.stdout.write(JSON.stringify(err));
+                process.stdout.write(stdout);
+                process.stdout.write(stderr);
+                done();
+            }
+        );
+    },
+    'log into Tower via ipsilon SSO': client => {
+        const login = client.page.login().navigate();
+        login
             .waitForElementVisible('.icon-saml-02', 5000)
             .click('.icon-saml-02')
-            .setValue('input[type=text][name=login_name]', 'test')
+            .waitForElementVisible('input[type=text][name=login_name]')
+            .setValue('input[type=text][name=login_name]', 'saml_user')
             .setValue('input[type=password][name=login_password]', 'fo0m4nchU')
             .click('button[type=submit][value=login]')
-            .waitForAngular()
-            .pause()
+            .waitForAngular();
     },
-    'open inventories': client => {
-        const inventories = client.page.inventories();
-        inventories.section.navigation.waitForElementVisible('@inventories');
-        inventories.section.navigation.expect.element('@inventories').enabled;
-        inventories.section.navigation.click('@inventories');
-        inventories.waitForElementVisible('div.spinny');
-        inventories.waitForElementNotVisible('div.spinny');
+    'open saml_user info': client => {
+        const users = client.page.users();
+        const row = '#users_table tbody tr';
+        users
+            .navigate()
+            .waitForElementVisible('div.spinny')
+            .waitForElementNotVisible('div.spinny')
+            .waitForElementVisible('smart-search input')
+            .sendKeys('smart-search input', `saml_user${client.Keys.ENTER}`)
+            .waitForElementVisible('div.spinny')
+            .waitForElementNotVisible('div.spinny')
+            .waitForElementNotPresent(`${row}:nth-of-type(2)`)
+            .expect.element(row).text.contain('saml_user');
+        users
+            .waitForElementVisible(`${row}:nth-of-type(1) a`)
+            .click(`${row}:nth-of-type(1) a`);
     },
-    'finish test': client => {
+    'confirm desired saml_user info': client => {
+        client.waitForElementVisible('input[name=first_name]');
+        client.expect.element('input[name=first_name]').value.contain('SAML');
+        client.expect.element('input[name=last_name]').value.contain('User');
+        client.expect.element('input[name=email]').value.contain('saml_user@testing.ansible.com');
+        client.expect.element('span[ng-show=external_account]').to.be.visible;
+        client.expect.element('span[ng-show=external_account]').text.contain('SOCIAL');
         client.end();
     }
 };
