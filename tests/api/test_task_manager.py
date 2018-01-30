888490bd56cf7b756aad22ef0dcc6b7abd6404fb
Christopher Wang <cwang@ansible.com>
2018-01-30 14:02:45 -0500

    Add marker for AT-7875.

--- tests/api/test_task_manager.py
+++ tests/api/test_task_manager.py
@@ -826,6 +826,7 @@ class Test_Cascade_Fail_Dependent_Jobs(Base_Api_Test):
         assert inv_source.get().status == 'failed'
         assert inv_source.last_job_failed
 
+    @pytest.mark.github('https://github.com/ansible/ansible-tower/issues/7875')
     def test_failed_project_update_should_cascade_fail_dependent_job(self, factories):
         jt = factories.v2_job_template()
         project = jt.ds.project
