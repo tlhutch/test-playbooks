621df62cb206de940ea31470736daf3078314bab
Christopher Wang <cwang@ansible.com>
2018-02-28 17:23:34 -0500

    Add read-only fields tests.

--- tests/api/test_openshift.py
+++ tests/api/test_openshift.py
@@ -428,3 +428,41 @@ class TestOpenShift(Base_Api_Test):
         assert ret == 0
 
         assert job.wait_until_completed().status == 'failed'
+
+    def test_verify_instance_group_read_only_fields(self, factories):
+        ig = factories.instance_group()
+        original_json = ig.json
+
+        ig.capacity = 777
+        ig.committed_capacity = 777
+        ig.consumed_capacity = 9000
+        ig.percent_capacity_remaining = 77.7
+        ig.jobs_running = 777
+        ig.instances = 777
+        ig.controller = 'ec2-fake-instance.com'  # FIXME: do we want this?
+
+        ig.get()
+        for field in ('capacity', 'committed_capacity', 'consumed_capacity', 'percent_capacity_remaining',
+                      'jobs_running', 'instances', 'controller'):
+            assert getattr(ig, field) == original_json[field]
+
+    def test_verify_instance_read_only_fields(self, v2):
+        instance = v2.instances.get().results.pop()
+        original_json = instance.json
+
+        instance.uuid = '77777777-7777-7777-7777-777777777777'
+        instance.hostname = 'ec2-fake-instance.com'
+        instance.version = '7.7.7.777'
+        instance.capacity = 777
+        instance.consumed_capacity = 777
+        instance.percent_capacity_remaining = 77.7
+        instance.jobs_running = 777
+        instance.cpu = 777
+        instance.memory = 777
+        instance.cpu_capacity = 777
+        instance.mem_capacity = 777
+
+        instance.get()
+        for field in ('uuid', 'hostname', 'version', 'capacity', 'consumed_capacity', 'percent_capacity_remaining',
+                      'jobs_running', 'cpu', 'memory', 'cpu_capacity', 'mem_capacity'):
+            assert getattr(instance, field) == original_json[field]
