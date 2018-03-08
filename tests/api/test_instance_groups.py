ad9a7a7dbf87bbbde279e909a67448b49ebb397d
Christopher Wang <cwang@ansible.com>
2018-03-08 12:08:36 -0500

    Add IG deletion protection test.

--- tests/api/test_instance_groups.py
+++ tests/api/test_instance_groups.py
@@ -1,4 +1,6 @@
+import towerkit.exceptions as exc
 import pytest
+
 
 from tests.api import Base_Api_Test
 
@@ -7,6 +9,18 @@ from tests.api import Base_Api_Test
 @pytest.mark.nondestructive
 @pytest.mark.usefixtures('authtoken', 'install_enterprise_license_unlimited')
 class TestInstanceGroups(Base_Api_Test):
+
+    @pytest.raises.github('https://github.com/ansible/ansible-tower/issues/7936')
+    def test_conflict_exception_when_attempting_to_delete_ig_with_running_job(self, v2, factories):
+        instance = v2.instances.get().results.pop()
+        ig = factories.instance_group()
+        ig.add_instance(instance)
+
+        jt = factories.v2_job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 30}')
+
+        with pytest.raises(exc.Conflict):
+            ig.delete()
+
     def test_verify_instance_group_read_only_fields(self, factories):
         ig = factories.instance_group()
         original_json = ig.json
