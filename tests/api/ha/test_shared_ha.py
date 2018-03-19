8950895a0bcf5cd7c2d6eb037b4d1a150be7995e
Christopher Wang <cwang@ansible.com>
2018-03-19 14:01:21 -0400

    Verify that other unified jobs distribute among IG instances.

--- tests/api/ha/test_shared_ha.py
+++ tests/api/ha/test_shared_ha.py
@@ -72,24 +72,6 @@ class TestSharedHA(Base_Api_Test):
         job_execution_nodes = set([job.execution_node for job in jobs.results])
         assert set([instance.hostname for instance in instances]) == set(job_execution_nodes)
 
-    def test_ahcs_should_distribute_among_new_instance_group_members(self, factories, v2):
-        import pdb; pdb.set_trace()
-        ig = factories.instance_group()
-        instances = random.sample(v2.instances.get().results, 3)
-        for instance in instances:
-            ig.add_instance(instance)
-
-        host = factories.v2_host()
-        host.ds.inventory.add_instance_group(ig)
-
-        for _ in range(9):
-            factories.v2_ad_hoc_command(inventory=host.ds.inventory, module_name='ping')
-        utils.poll_until(lambda: jobs.get(status='successful').count == 9, interval=5, timeout=300)
-        import pdb; pdb.set_trace()
-
-        job_execution_nodes = set([job.execution_node for job in jobs.results])
-        assert set([instance.hostname for instance in instances]) == set(job_execution_nodes)
-
     # VERIFY
     def test_jobs_should_distribute_among_mutually_exclusive_instance_groups(self, factories, v2):
         ig1, ig2 = [factories.instance_group() for _ in range(2)]
@@ -163,6 +145,137 @@ class TestSharedHA(Base_Api_Test):
         pod2_jobs = [job for job in jobs.results if job.execution_node == tower_pods[1]]
         assert len(pod1_jobs) > 0
         assert len(pod2_jobs) > 0
+
+    @pytest.mark.parametrize('resource', ['inventory', 'organization', 'both'])
+    def test_ahcs_run_on_target_instance_with_resource_ig_assignment(self, factories, v2, resource):
+        ig = factories.instance_group()
+        instance = v2.instances.get().results.pop()
+        ig.add_instance(instance)
+
+        host = factories.v2_host()
+        inv = host.ds.inventory
+
+        if resource == 'inventory':
+            inv.add_instance_group(ig)
+        elif resource == 'organization':
+            inv.ds.organization.add_instance_group(ig)
+        else:
+            inv.add_instance_group(ig)
+            inv.ds.organization.add_instance_group(ig)
+
+        ahc = factories.v2_ad_hoc_command(inventory=inv, module_name='ping')
+        assert ahc.wait_until_completed().is_successful
+        assert ahc.execution_node == instance.hostname
+
+    def test_ahcs_should_distribute_among_new_instance_group_members(self, factories, v2):
+        ig = factories.instance_group()
+        instances = random.sample(v2.instances.get().results, 3)
+        for instance in instances:
+            ig.add_instance(instance)
+
+        host = factories.v2_host()
+        inv = host.ds.inventory
+        inv.add_instance_group(ig)
+
+        for _ in range(9):
+            factories.v2_ad_hoc_command(inventory=inv, module_name='ping')
+        ahcs = inv.related.ad_hoc_commands.get()
+        utils.poll_until(lambda: ahcs.get(status='successful').count == 9, interval=5, timeout=300)
+
+        ahc_execution_nodes = [ahc.execution_node for ahc in ahcs.results]
+        assert set(ahc_execution_nodes) == set([instance.hostname for instance in instances])
+
+    def test_project_updates_run_on_target_instance_via_organization_ig_assignment(self, factories, v2):
+        ig = factories.instance_group()
+        instance = v2.instances.get().results.pop()
+        ig.add_instance(instance)
+
+        project = factories.v2_project()
+        project.ds.organization.add_instance_group(ig)
+
+        project_update = project.update()
+        assert project_update.wait_until_completed().is_successful
+        assert project_update.execution_node == instance.hostname
+
+    def test_project_updates_should_distribute_among_new_instance_group_members(self, factories, v2):
+        ig = factories.instance_group()
+        instances = random.sample(v2.instances.get().results, 3)
+        for instance in instances:
+            ig.add_instance(instance)
+
+        project = factories.v2_project()
+        project.ds.organization.add_instance_group(ig)
+        initial_update = project.get().related.last_update.get()
+
+        for _ in range(9):
+            project.update()
+        project_updates = project.related.project_updates.get()
+        utils.poll_until(lambda: project_updates.get(status='successful', not__id=initial_update.id).count == 9,
+                                 interval=5, timeout=300)
+
+        update_execution_nodes = [update.execution_node for update in project_updates.results]
+        assert set(update_execution_nodes) == set([instance.hostname for instance in instances])
+
+    @pytest.mark.parametrize('resource', ['inventory', 'organization', 'both'])
+    def test_inventory_updates_run_on_target_instance_via_resource_ig_assignment(self, factories, v2, resource):
+        ig = factories.instance_group()
+        instance = v2.instances.get().results.pop()
+        ig.add_instance(instance)
+
+        inv_source = factories.v2_inventory_source()
+        inv = inv_source.ds.inventory
+
+        if resource == 'inventory':
+            inv.add_instance_group(ig)
+        elif resource == 'organization':
+            inv.ds.organization.add_instance_group(ig)
+        else:
+            inv.add_instance_group(ig)
+            inv.ds.organization.add_instance_group(ig)
+
+        inv_update = inv_source.update()
+        assert inv_update.wait_until_completed().is_successful
+        assert inv_update.execution_node == instance.hostname
+
+    def test_inventory_updates_should_distribute_among_new_instance_group_members(self, factories, v2):
+        ig = factories.instance_group()
+        instances = random.sample(v2.instances.get().results, 3)
+        for instance in instances:
+            ig.add_instance(instance)
+
+        inv_source = factories.v2_inventory_source()
+        inv_source.ds.inventory.add_instance_group(ig)
+
+        for _ in range(9):
+            inv_source.update()
+        inv_updates = inv_source.related.inventory_updates.get()
+        utils.poll_until(lambda: inv_updates.get(status='successful').count == 9, interval=5, timeout=300)
+
+        update_execution_nodes = [update.execution_node for update in inv_updates.results]
+        assert set(update_execution_nodes) == set([instance.hostname for instance in instances])
+
+    def test_wfjt_node_jobs_run_on_target_instance_via_unified_jt_ig_assignment(self, factories, v2):
+        ig = factories.instance_group()
+        instance = v2.instances.get().results.pop()
+        ig.add_instance(instance)
+
+        wfjt = factories.v2_workflow_job_template()
+        jt = factories.v2_job_template()
+        inv_source = factories.v2_inventory_source()
+        project = jt.ds.project
+
+        node1 = factories.v2_workflow_job_template_node(workflow_job_template=wfjt, unified_job_template=jt)
+        node2 = node1.add_always_node(unified_job_template=inv_source)
+        node2.add_always_node(unified_job_template=project)
+
+        for resource in (jt, inv_source.ds.inventory, project.ds.organization):
+            resource.add_instance_group(ig)
+
+        wfj = wfjt.launch()
+        assert wfj.wait_until_completed().is_successful
+        assert jt.get().related.last_job.get().execution_node == instance.hostname
+        assert inv_source.get().related.last_update.get().execution_node == instance.hostname
+        assert project.get().related.last_update.get().execution_node == instance.hostname
 
     def test_jobs_should_not_run_on_disabled_instances(self, request, v2, factories):
         ig = factories.instance_group()
