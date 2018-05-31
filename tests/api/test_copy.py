b79192061e12ba435d895865af0b7dc19a091c1e
Ryan Petrello <rpetrell@redhat.com>
2018-05-31 10:59:27 -0400

    verify that copying is not allowed in /api/v1/
    see: https://github.com/ansible/tower/issues/1978

---
+++ tests/api/test_copy.py
@@ -0,0 +1,27 @@
+import pytest
+import towerkit.exceptions as exc
+
+from tests.api import Base_Api_Test
+
+
+@pytest.mark.api
+@pytest.mark.destructive
+@pytest.mark.usefixtures('authtoken', 'install_enterprise_license_unlimited')
+class TestCopying(Base_Api_Test):
+
+    @pytest.mark.parametrize('obj', [
+        'job_template',
+        'project',
+        'inventory',
+        'inventory_script',
+        'credential',
+        'workflow_job_template',
+        'notification_template',
+    ])
+    @pytest.mark.github('https://github.com/ansible/tower/issues/1978')
+    def test_v1_copy_is_not_allowed(self, factories, v1, obj):
+        assert 'copy' in getattr(factories, 'v2_{}'.format(obj))().related
+        x = getattr(factories, obj)()
+        assert 'copy' not in x.related
+        with pytest.raises(exc.NotFound, message='Action only possible starting with v2 API.'):
+            v1.walk(x.url + 'copy/').post({'name': 'Some Copy'})
