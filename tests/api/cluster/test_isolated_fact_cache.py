8614d198226f5afbef59dfd2eca003f6e55e7e53
Christopher Wang <cwang@ansible.com>
2018-06-28 13:12:17 -0400

    Verify that jobs ran on an isolated node.

--- tests/api/cluster/test_isolated_fact_cache.py
+++ tests/api/cluster/test_isolated_fact_cache.py
@@ -18,6 +18,11 @@ class TestIsolatedFactCache(Base_Api_Test):
         assert ansible_facts.ansible_machine == 'x86_64'
         assert ansible_facts.ansible_system == 'Linux'
 
+    def assert_job_ran_on_isolated_node(self, ansible_facts):
+        assert ansible_facts.ansible_env.ANSIBLE_CALLBACK_PLUGINS == \
+            '/var/lib/awx/venv/awx/lib/python2.7/site-packages/awx/lib/isolated_callbacks'
+        assert ansible_facts.ansible_env.AWX_ISOLATED_DATA_DIR == ansible_facts.ansible_env.AWX_PRIVATE_DATA_DIR
+
     def test_ingest_facts_with_gather_facts_on_isolated_node(self, factories, isolated_instance_group):
         host = factories.v2_host()
         ansible_facts = host.related.ansible_facts.get()
@@ -29,20 +34,23 @@ class TestIsolatedFactCache(Base_Api_Test):
 
         ansible_facts = host.related.ansible_facts.get()
         self.assert_updated_facts(ansible_facts)
+        self.assert_job_ran_on_isolated_node(ansible_facts)
 
     def test_consume_facts_with_multiple_hosts_on_isolated_node(self, factories, isolated_instance_group):
         inventory = factories.v2_inventory()
         hosts = [factories.v2_host(inventory=inventory) for _ in range(3)]
+        ansible_facts = hosts.pop().related.ansible_facts.get()  # facts should be the same between hosts
 
         jt = factories.v2_job_template(inventory=hosts[0].ds.inventory, playbook='gather_facts.yml', use_fact_cache=True)
         jt.add_instance_group(isolated_instance_group)
         assert jt.launch().wait_until_completed().is_successful
+        self.assert_updated_facts(ansible_facts.get())
+        self.assert_job_ran_on_isolated_node(ansible_facts)
 
         jt.patch(playbook='use_facts.yml', job_tags='ansible_facts')
         job = jt.launch().wait_until_completed()
         assert job.is_successful
 
-        ansible_facts = hosts.pop().related.ansible_facts.get()  # facts should be the same between hosts
         assert job.result_stdout.count(ansible_facts.ansible_distribution) == 3
         assert job.result_stdout.count(ansible_facts.ansible_machine) == 3
         assert job.result_stdout.count(ansible_facts.ansible_system) == 3
@@ -55,6 +63,7 @@ class TestIsolatedFactCache(Base_Api_Test):
         assert jt.launch().wait_until_completed().is_successful
         ansible_facts = host.related.ansible_facts.get()
         self.assert_updated_facts(ansible_facts)
+        self.assert_job_ran_on_isolated_node(ansible_facts)
 
         jt.playbook = 'clear_facts.yml'
         assert jt.launch().wait_until_completed().is_successful
