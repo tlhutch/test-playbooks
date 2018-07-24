4f76630829a40841a225079fcc6bc51ed7d5073c
chris meyers <chris.meyers.fsu@gmail.com>
2018-07-24 15:09:07 -0400

    more complete jt callback host list
    * Add every interface IP and name to the list of hosts in the inventory
    and include the "internet" ip. One of these will match the remote host
    when a callback is issued.

--- tests/api/job_templates/test_job_template_callbacks.py
+++ tests/api/job_templates/test_job_template_callbacks.py
@@ -4,6 +4,9 @@ import socket
 import json
 import requests
 import os
+import fcntl
+import struct
+import array
 
 from towerkit.config import config
 from towerkit import utils, exceptions as exc
@@ -18,9 +21,36 @@ from tests.api import Base_Api_Test
 class TestJobTemplateCallbacks(Base_Api_Test):
     @pytest.fixture(scope='class')
     def remote_hosts(self):
-        hosts = ['gateway'] # Docker dev env remote
-        hosts.append(socket.gethostbyname(socket.gethostname())) # Network Interface
-        hosts.append(requests.get('https://api.ipify.org').text) # External IP
+        def all_interfaces():
+            max_possible = 128  # arbitrary. raise if needed.
+            bytes = max_possible * 32
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            names = array.array('B', '\0' * bytes)
+            outbytes = struct.unpack('iL', fcntl.ioctl(
+                s.fileno(),
+                0x8912,  # SIOCGIFCONF
+                struct.pack('iL', bytes, names.buffer_info()[0])
+            ))[0]
+            namestr = names.tostring()
+            lst = []
+            for i in range(0, outbytes, 40):
+                name = namestr[i:i + 16].split('\0', 1)[0]
+                ip = namestr[i + 20:i + 24]
+                lst.append((name, ip))
+            return lst
+
+        def format_ip(addr):
+            return str(ord(addr[0])) + '.' + \
+               str(ord(addr[1])) + '.' + \
+               str(ord(addr[2])) + '.' + \
+               str(ord(addr[3]))
+
+        ifs = all_interfaces()
+
+        hosts = [requests.get('https://api.ipify.org').text] # External IP
+        for i in ifs:
+            hosts.append(i[0])
+            hosts.append(format_ip(i[1]))
         return hosts
 
     def get_job_id_from_location_header(self, resp):
@@ -572,7 +602,7 @@ class TestJobTemplateCallbacks(Base_Api_Test):
         res = requests.post("{}{}".format(config.base_url, jt.related.callback),
                             data={'host_config_key': host_config_key},
                             verify=False)
-        assert res.status_code == 201, \
+        assert 201 == res.status_code, \
                 "Launching Job Template via provision callback failed. Remote host list {}".format(remote_hosts)
 
         job_id = self.get_job_id_from_location_header(res)
