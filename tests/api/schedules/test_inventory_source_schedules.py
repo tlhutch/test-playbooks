2fd2663473f35aed9903cccf49dab40d7131e227
AlanCoding <arominge@redhat.com>
2018-07-26 11:30:59 -0400

    add tests for inventory and project schedules

---
+++ tests/api/schedules/test_inventory_source_schedules.py
@@ -0,0 +1,20 @@
+import pytest
+
+from towerkit.utils import poll_until
+
+from tests.api.schedules import SchedulesTest
+
+
+@pytest.mark.api
+@pytest.mark.destructive
+@pytest.mark.usefixtures('authtoken', 'install_enterprise_license_unlimited')
+class TestInventorySourceSchedules(SchedulesTest):
+
+    def test_inventory_source_schedules_are_functional(self, factories):
+        schedule = factories.v2_inventory_source().add_schedule(rrule=self.minutely_rrule())
+
+        schedule_jobs = schedule.related.unified_jobs.get()
+        poll_until(lambda: schedule_jobs.get().count == 1, interval=15, timeout=5 * 60)
+
+        inventory_update = schedule_jobs.get().results.pop().wait_until_completed()
+        assert inventory_update.is_successful
