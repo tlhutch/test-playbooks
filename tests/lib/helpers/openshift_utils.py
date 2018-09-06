1f5d05b9653c517a0bd77518719b2cf890360a88
chris meyers <chris.meyers.fsu@gmail.com>
2018-09-06 16:54:24 -0400

    use stateful sets instead of deployments

--- tests/lib/helpers/openshift_utils.py
+++ tests/lib/helpers/openshift_utils.py
@@ -27,7 +27,7 @@ def get_tower_pods():
 
 
 def scale_dc(dc, replicas):
-    cmd = 'oc scale dc {0} --replicas={1}'.format(dc, str(replicas))
+    cmd = 'oc scale sts {0} --replicas={1}'.format(dc, str(replicas))
     ret = subprocess.call(cmd, shell=True)
     assert ret == 0
 
