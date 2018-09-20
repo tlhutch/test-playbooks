c5511652f41ed9ede4f9604f19a2fc92a405e576
Ryan Petrello <rpetrell@redhat.com>
2018-09-19 21:52:20 -0400

    update supervisor fixture for celery -> dispatcher

--- tests/lib/fixtures/supervisorctl.py
+++ tests/lib/fixtures/supervisorctl.py
@@ -13,19 +13,19 @@ def pause_awx_task_system(request, skip_docker, ansible_runner):
     Note: this is about as noisy of a neighbor a test can be.  Its worth should be re-evaluated:
     1. Stopping celery will halt task processing tower activity (the point of the fixture) but this will
        break other tests if multiprocessing is running.
-    2. Failing to stop the awx-celeryd service will exit pytest, killing the test run.
+    2. Failing to stop the awx-dispatcher service will exit pytest, killing the test run.
     3. Having to halt a critical part of tower to confirm pending jobs can be canceled seems like a bad approach.
     """
     def teardown():
         log.debug("calling supervisorctl teardown pause_awx_task_system")
-        contacted = ansible_runner.supervisorctl(name='tower-processes:awx-celeryd', state='started')
+        contacted = ansible_runner.supervisorctl(name='tower-processes:awx-dispatcher', state='started')
         result = contacted.values()[0]
         if result.get('failed'):
-            pytest.exit("tower-processes:awx-celeryd failed to restart - {0}.".format(json.dumps(result, indent=2)))
+            pytest.exit("tower-processes:awx-dispatcher failed to restart - {0}.".format(json.dumps(result, indent=2)))
     request.addfinalizer(teardown)
 
     log.debug("calling supervisorctl fixture pause_awx_task_system")
-    contacted = ansible_runner.supervisorctl(name='tower-processes:awx-celeryd', state='stopped')
+    contacted = ansible_runner.supervisorctl(name='tower-processes:awx-dispatcher', state='stopped')
     result = contacted.values()[0]
     assert(not result.get('failed')
-           ), "Stopping tower-processes:awx-celeryd failed - {0}.".format(json.dumps(result, indent=2))
+           ), "Stopping tower-processes:awx-dispatcher failed - {0}.".format(json.dumps(result, indent=2))
