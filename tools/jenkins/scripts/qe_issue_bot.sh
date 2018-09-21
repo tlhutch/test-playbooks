a6c79371ad4be42fb1a5ed3858730fd67b64bfa5
Jim Ladd <jladd@redhat.com>
2018-09-21 15:26:01 -0700

    Add jenkins shell scripts for slack bots

---
+++ tools/jenkins/scripts/qe_issue_bot.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+pip install -U slacker
+pip install -U pygithub
+
+
+if [[ $TEST_NOTIFICATION == true ]]; then
+  export SLACK_CHANNEL="#test-slack"
+else
+  export SLACK_CHANNEL="#ship_it"
+fi
+
+python issue_bot.py
