a992b61f3c02a72102ff3f62dce0da2bd29f29a0
Yanis Guenane <yguenane@redhat.com>
2018-09-24 11:40:48 +0200

    Jenkins: Test_TowerQA_Integration migration

---
+++ tools/jenkins/scripts/Test_TowerQA_Integration.sh
@@ -0,0 +1,107 @@
+#!/bin/bash -xe
+
+rm -f ./run_integration_tests
+
+if [[ ${sha1} =~ /pr/([0-9]+) ]]
+then
+  PULL_ID_FROM_BRANCH=${BASH_REMATCH[1]}
+fi
+
+ghprbPullId=${ghprbPullId:-${PULL_ID_FROM_BRANCH}}
+
+# Build & run if not testing against a pr
+if [ -z "$ghprbPullId" ]
+then
+  touch ./run_integration_tests
+  exit
+fi
+
+GH_API_PR_LABEL_URL=https://api.github.com/repos/ansible/tower-qa/issues/${ghprbPullId}/labels
+INTEGRATION_LABEL=`curl -u :${GITHUB_ACCESS_TOKEN} ${GH_API_PR_LABEL_URL} 2>/dev/null | grep 'ci:integration' | wc -l`
+if [ ${INTEGRATION_LABEL} -gt 0 ]
+then
+  touch ./run_integration_tests
+fi
+
+
+if [[ -e "./run_integration_tests" ]]; then
+
+    cd $WORKSPACE
+    mkdir -p ~/.ssh/
+    cp $PUBLIC_KEY ~/.ssh/id_rsa.pub
+
+    pip install -U pip setuptools
+    pip install -U setuptools
+    pip install sphinx==1.5.6
+    pip install -U pyrax boto boto3 botocore azure apache-libcloud
+    pip install -U argparse # required by pyrax -> novaclient, but not explicitly listed
+    pip install -U junit-xml
+
+    pip uninstall -y ansible
+
+    if [[ "${ANSIBLE_INSTALL_METHOD}" == nightly ]]; then
+        pip install -U -I "${ANSIBLE_NIGHTLY_REPO}/${ANSIBLE_NIGHTLY_BRANCH}/tar/ansible-latest.tar.gz"
+    else
+        pip install -U -I ansible
+    fi
+
+    # Increase ssh timeout
+    export ANSIBLE_TIMEOUT=30
+
+    # return to kansas
+    export ANSIBLE_NOCOLOR=True
+
+    #
+    # Install Tower
+    #
+
+    export INSTANCE_NAME_PREFIX=${INSTANCE_NAME_PREFIX}-`echo ${sha1} | sed -e 's,/,_,g'`
+    MINIMUM_VAR_SPACE=0 \
+    ANSIBLE_NIGHTLY_REPO="${ANSIBLE_NIGHTLY_REPO}/${ANSIBLE_NIGHTLY_BRANCH}" \
+    python scripts/cloud_vars_from_env.py --cloud-provider ${CLOUD_PROVIDER} --platform ${PLATFORM} > vars.yml
+    ansible-playbook -i playbooks/inventory -e @vars.yml playbooks/deploy-tower.yml
+    
+    TOWER_URL=`python scripts/ansible_inventory_to_json.py --inventory playbooks/inventory.log | jq -r .tower\[0\]`
+    TOWER_VERSION=`curl -ks https://${TOWER_URL}/api/v1/ping/ | jq -r .version | cut -d . -f 1-3`
+    echo ${TOWER_VERSION}
+    
+    #
+    # Run Tests
+    #
+    
+    pip install -r requirements.txt
+    pip install -U git+https://github.com/rmfitzpatrick/pytest-ansible.git@handle_ansible_vars_restructuring
+    
+    # extract tower hostname
+    ANSIBLE_INVENTORY=playbooks/inventory.log
+    INVENTORY_GROUP="${PLATFORM}:&${CLOUD_PROVIDER}"
+    INVENTORY_HOST=$(ansible -i ${ANSIBLE_INVENTORY} --list-hosts ${INVENTORY_GROUP} | tail -n 1 | awk 'NR==1{print $1}')
+    
+    # decrypt credentials
+    ansible-vault decrypt --vault-password-file="${VAULT_FILE}" config/credentials.vault --output=config/credentials.yml
+    
+    run_tests_and_generate_html(){
+        set +e
+    	py.test ${PYTEST_ARGS} \
+        -c config/api.cfg \
+        --ansible-host-pattern="${INVENTORY_HOST}" \
+        --ansible-inventory="${ANSIBLE_INVENTORY}" \
+        --base-url="https://${INVENTORY_HOST}" \
+        --mp --np 4 \
+        tests/api
+        
+        TEST_STATUS=$?
+    
+        mkdir -p reports/html
+        junit2html reports/junit/results.xml reports/html/index.html
+        ansible-playbook -i playbooks/inventory.log -e @vars.yml playbooks/reap-tower-ec2.yml
+    
+    	set -e
+        return $TEST_STATUS
+    }
+    run_tests_and_generate_html
+    
+    echo "### DESTROY CLOUD ###"
+    bash -e scripts/jenkins_runplay.sh playbooks/destroy.yml | tee destroy.log
+
+fi
