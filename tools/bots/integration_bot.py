388660d1ad65c3a20d123f9aa6b3cc43dac052c1
Ryan Petrello <rpetrell@redhat.com>
2018-10-05 13:41:22 -0400

    improve the integration bot Slack message a bit

--- tools/bots/integration_bot.py
+++ tools/bots/integration_bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from collections import namedtuple
 from datetime import datetime, timedelta
 import os
 import pytz
@@ -28,10 +29,12 @@ show_button_owner = os.getenv('SHOW_BUTTON_OWNER')
 def get_test_results():
     """
     Returns either:
-    - (<number of currently failing tests>, <change in test failures>)
-    - (-1, 0) if last test run did not occur in past 24 hours, or
-    - (-2, 0) if test results were not found
+    - (<number of currently failing tests>, <url of most recent nightly test run>, <change in test failures>, <url of prior nightly test run>)
+    - (-1, None, 0, None) if last test run did not occur in past 24 hours, or
+    - (-2, None, 0, None) if test results were not found
     """
+    buildresult = namedtuple('buildresult', 'failures, url')
+
     def result_from_past_day(build):
         timestamp = build.get_timestamp()
         today = datetime.now(pytz.UTC)
@@ -78,23 +81,33 @@ def get_test_results():
             if results['passed']:
                 if not found_first_result:
                     if result_from_past_day(run):
-                        return (0, None, 0)
-                    return (-1, None, 0)
+                        return (0, None, 0, None)
+                    return (-1, None, 0, None)
                 else:
                     # Test was passing, now failing
-                    return (failure_history[0][0], failure_history[0][1], failure_history[1][0])
+                    return (
+                        failure_history[0].failures,
+                        failure_history[0].url,
+                        failure_history[1].failures,
+                        failure_history[1].url
+                    )
 
             total_failures = results['failed'] + results['error']
-            failure_history.append([total_failures, url])
+            failure_history.append(buildresult(total_failures, url))
             if not found_first_result and len(failure_history) == 1:
                 found_first_result = True
                 if not result_from_past_day(build):
-                    return (-1, None, None)
+                    return (-1, None, None, None)
 
             if len(failure_history) == 2:
-                return failure_history[0][0], failure_history[0][1], (failure_history[0][0] - failure_history[1][0])
+                return (
+                    failure_history[0].failures,
+                    failure_history[0].url,
+                    failure_history[0].failures - failure_history[1].failures,
+                    failure_history[1].url
+                )
 
-    return (-2, None)
+    return (-2, None, 0, None)
 
 
 def post_slack_msg(text):
@@ -102,7 +115,7 @@ def post_slack_msg(text):
 
 
 def create_test_update():
-    total_failures, url, change = get_test_results()
+    total_failures, url, change, old_url = get_test_results()
 
     if total_failures == 0:
         post_slack_msg(':green_ball: *{0}* ({1}) is green! :green_ball:'.format(job_name, url))
@@ -130,12 +143,12 @@ def create_test_update():
             break
 
     if change > 0:
-        description = '*{0}* ({1}) has {2} more failures'.format(job_name, url, change)
+        description = '*{0}* ({1}) has {2} more failures than {3}'.format(job_name, url, change, old_url)
     elif change == 0:
-        description = '*{0}* ({1}) has the same number of failures'.format(job_name, url)
+        description = '*{0}* ({1}) has the same number of failures as {3}'.format(job_name, url, old_url)
     else:
         change = -change
-        description = '*{0}* ({1}) has {2} fewer failures'.format(job_name, url, change)
+        description = '*{0}* ({1}) has {2} fewer failures than {3}'.format(job_name, url, change, old_url)
 
     day_of_the_week = datetime.today().weekday()
     button_owner_map = {0: 'spredzy',
