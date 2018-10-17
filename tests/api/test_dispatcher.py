29ee63b46cf035f104474da2c946407d301265b5
Jim Ladd <jladd@redhat.com>
2018-10-17 08:54:36 -0700

    Add dispatcher tests

---
+++ tests/api/test_dispatcher.py
@@ -0,0 +1,212 @@
+import logging
+import re
+
+import pytest
+from towerkit.utils import poll_until
+
+from tests.api import APITest
+
+
+log = logging.getLogger(__name__)
+
+
+@pytest.mark.api
+@pytest.mark.destructive
+@pytest.mark.skip_openshift
+@pytest.mark.usefixtures('authtoken', 'install_enterprise_license_unlimited')
+@pytest.mark.mp_group('TestDispatcher', 'isolated_serial')
+class TestDispatcher(APITest):
+
+    def runner_output(self, result):
+        return "stdout:\n\n{0[stdout]}\n\nstderr:\n{0[stderr]}".format(result)
+
+    @pytest.fixture(scope="function")
+    def run_remote_command(self, ansible_runner):
+        def _run_remote_command(cmd):
+            contacted = ansible_runner.command(cmd)
+            result = contacted.values()[0]
+            assert result['rc'] == 0, "{0} failed. {1}".format(cmd, self.runner_output(result))
+            return result
+        return _run_remote_command
+
+    @pytest.fixture(scope="function")
+    def kill_process(self, run_remote_command, process_present):
+        def _kill_process(pid):
+            if type(pid) != list:
+                pid = [pid]
+            run_remote_command('kill -9 {}'.format(' '.join([str(p) for p in pid])))
+            poll_until(lambda: not process_present(pid), timeout=30)
+        return _kill_process
+
+    @pytest.fixture(scope="function")
+    def process_present(self, run_remote_command):
+        """
+        Given a process (or list of processes), returns list of any processes that were found.
+        """
+        def _process_present(pid):
+            if type(pid) != list:
+                pid = [pid]
+            result = run_remote_command('ps -A -o pid')
+            present = []
+            for p in pid:
+                if str(p) in result['stdout_lines']:
+                    present.append(p)
+            return present
+        return _process_present
+
+    @pytest.fixture(scope="function")
+    def get_dispatcher_pids(self, run_remote_command):
+        def _get_dispatcher_pids():
+            """
+            Returns tuple containing:
+              * PID of parent process
+              * list of PIDs of dispatcher's child processes
+            """
+            result = run_remote_command('awx-manage run_dispatcher --status')
+            for line in result['stdout_lines']:
+                matches = re.search('\[pid:(\d+)\] workers', line) # noqa: W605
+                if matches:
+                    parent_pid = matches.group(1)
+                    break
+            else:
+                raise Exception('Could not locate parent PID in dispatcher output.\n\n{}'
+                                .format(self.runner_output(result)))
+            child_pids = []
+            for line in result['stdout_lines']:
+                matches = re.search('worker\[pid:(\d+)\]', line) # noqa: W605
+                if matches:
+                    child_pids.append(matches.group(1))
+            return (parent_pid, child_pids)
+        return _get_dispatcher_pids
+
+    def ensure_jt_runs_on_primary_instance(self, jt, v):
+        igs = v.instance_groups.get()
+        if igs.count != 1:
+            ig = [ig for ig in igs.results if ig.name == '1'].pop()
+            jt.add_instance_group(ig)
+
+    def test_dispatcher_graceful_restart(self, factories, v2, run_remote_command):
+        jt = factories.v2_job_template(playbook='sleep.yml',
+                                       extra_vars=dict(sleep_interval=30))
+        jt.ds.inventory.add_host()
+        job = jt.launch().wait_until_status('running')
+
+        try:
+            run_remote_command('supervisorctl stop tower-processes:awx-dispatcher')
+            job.wait_until_status('failed', since_job_created=False)
+            assert job.job_explanation == 'Task was marked as running in Tower but was not present in the job queue, so it has been marked as failed.'
+        finally:
+            run_remote_command('supervisorctl start tower-processes:awx-dispatcher')
+
+        jt.extra_vars = '{"sleep_interval": 1}'
+        job = jt.launch().wait_until_completed()
+        assert job.is_successful
+
+    def test_dispatcher_hard_restart(self, factories, v2, run_remote_command):
+        jt = factories.v2_job_template(playbook='sleep.yml',
+                                       extra_vars=dict(sleep_interval=30))
+        jt.ds.inventory.add_host()
+        job = jt.launch().wait_until_status('running')
+
+        # kill all dispatcher processes, supervisorctl immediately restarts dispatcher
+        run_remote_command("pkill -f --signal 9 run_dispatcher")
+
+        # when dispatcher restarts, it reaps the running job
+        job.wait_until_status('failed', since_job_created=False)
+        assert job.job_explanation == 'Task was marked as running in Tower but was not present in the job queue, so it has been marked as failed.'
+
+        try:
+            result = run_remote_command('supervisorctl status tower-processes:awx-dispatcher')
+            assert 'running' in result['stdout'].lower(), "supervisorctl failed to restart dispatcher. {0}".format(self.runner_output(result))
+        except:
+            # restart dispatcher if necessary
+            # do not expect this to be needed; supervisor should automatically restart dispatcher
+            run_remote_command('supervisorctl start tower-processes:awx-dispatcher')
+            raise
+
+        jt.extra_vars = '{"sleep_interval": 1}'
+        job = jt.launch().wait_until_completed()
+        assert job.is_successful
+
+    @pytest.mark.github('https://github.com/ansible/tower/issues/3138')
+    def test_kill_dispatcher_child_process_executing_job(self, factories, v2, run_remote_command, kill_process,
+                                                         get_dispatcher_pids):
+
+        def get_worker_running_job(job_id):
+            """
+            awx-manage run_dispatcher --status returns status of dispatcher child processes. For example:
+
+            .  worker[pid:8688] sent=688 finished=687 qsize=1 rss=109.848MB
+                 - running 36ff5e4a-1946-40a5-8770-3b379addb54e RunJob(*[43])
+
+            Find pid of worker running given job.
+            """
+            result = run_remote_command('awx-manage run_dispatcher --status')
+
+            worker_description = ''
+            for i, line in enumerate(result['stdout_lines']):
+                if re.search("RunJob\([^\)]*{}[^\)]*\)".format(job.id), line): # noqa: W605
+                    worker_description = result['stdout_lines'][i - 1]
+                    break
+            else:
+                raise Exception('Unable to find worker running job {0}\n\nawx_manage run_dispatcher --status shows:\n{1}'
+                                .format(job.id, result['stdout_lines']))
+
+            matches = re.search('\[pid:(\d+)]', worker_description) # noqa: W605
+            return matches.group(1)
+
+        jt = factories.job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 120}')
+        jt.ds.inventory.add_host()
+
+        self.ensure_jt_runs_on_primary_instance(jt, v2)
+        job = jt.launch()
+
+        _, worker_pids_before = get_dispatcher_pids()
+        pid = get_worker_running_job(job.id)
+        kill_process(pid)
+        _, worker_pids_after = get_dispatcher_pids()
+        # Expect to find one worker in place of removed worker
+        assert len(set(worker_pids_after) - (set(worker_pids_before) - set(pid))) == 1
+
+        assert job.wait_until_completed(since_job_created=False).status == 'failed'
+
+        jt.extra_vars = '{"sleep_interval": 1}'
+        job = jt.launch().wait_until_completed()
+        assert job.is_successful
+
+    def test_kill_all_dispatcher_child_processes(self, v2, factories, run_remote_command, get_dispatcher_pids, kill_process):
+        _, pids_before = get_dispatcher_pids()
+        kill_process(pids_before)
+
+        def processes_respawned():
+            _, pids = get_dispatcher_pids()
+            if len(pids) != len(pids_before):
+                assert len(pids) < len(pids_before)
+                return False
+            return True
+        poll_until(processes_respawned, timeout=90)
+
+        jt = factories.job_template()
+        jt.ds.inventory.add_host()
+        self.ensure_jt_runs_on_primary_instance(jt, v2)
+        job = jt.launch().wait_until_completed()
+        assert job.is_successful
+
+    def test_kill_dispatcher_parent_process(self, factories, v2, get_dispatcher_pids, kill_process, process_present):
+        parent_pid, child_pids = get_dispatcher_pids()
+        kill_process(parent_pid)
+        assert not process_present([parent_pid] + child_pids)
+
+        def processes_respawned():
+            _, pids = get_dispatcher_pids()
+            if len(pids) != len(child_pids):
+                assert len(pids) < len(child_pids)
+                return False
+            return True
+        poll_until(processes_respawned, timeout=60)
+
+        jt = factories.job_template()
+        jt.ds.inventory.add_host()
+        self.ensure_jt_runs_on_primary_instance(jt, v2)
+        job = jt.launch().wait_until_completed()
+        assert job.is_successful
--- tests/api/test_tower_services.py
+++ tests/api/test_tower_services.py
@@ -1,6 +1,10 @@
-import pytest
-import re
 import logging
+import re
+import time
+
+import pytest
+from towerkit import utils
+
 from tests.api import APITest
 
 
@@ -9,19 +13,9 @@ log = logging.getLogger(__name__)
 
 @pytest.mark.api
 @pytest.mark.destructive
-@pytest.mark.second_to_last
 @pytest.mark.skip_openshift
 @pytest.mark.mp_group('AnsibleTowerService', 'isolated_serial')
-class Test_Ansible_Tower_Service(APITest):
-    """Executes ansible-tower-service commands and checks process statuses.
-    Note: we check process output with systemctl on EL7 systems and with
-    service on EL6 systems. Did not implement with Ubuntu because of the
-    following:
-    * On Ubuntu systems, ansible-tower-service status sometimes incorrectly
-    reports the status of redis-server.
-    * On Ubuntu systems, rapidly restarting supervisor can leave your system
-    in an indeterminate state.
-    """
+class TestTowerServices(APITest):
     pytestmark = pytest.mark.usefixtures('authtoken')
 
     @pytest.mark.parametrize("command, active", [
@@ -30,12 +24,19 @@ class Test_Ansible_Tower_Service(APITest):
         ("restart", True),
         ("status", True)
     ], ids=['ansible-tower-service start', 'ansible-tower-service stop', 'ansible-tower-service restart', 'ansible-tower-service status'])
-    def test_tower_status_on_el7(self, ansible_runner, ansible_os_family, ansible_distribution_major_version,
-                                 restart_tower_on_teardown, command, active):
-        """Execute ansible-tower-service commands and check process statuses."""
-        # check that Tower is running on an EL7 system
-        if not (ansible_os_family == 'RedHat' and ansible_distribution_major_version == '7'):
-            pytest.skip("Only supported on EL7 distributions")
+    def test_tower_status_on_el7(self, ansible_runner, ansible_os_family, restart_tower_on_teardown, command, active):
+        """Executes ansible-tower-service commands and checks process statuses.
+        Note: we check process output with systemctl on EL7 systems and with
+        service on EL6 systems. Did not implement with Ubuntu because of the
+        following:
+        * On Ubuntu systems, ansible-tower-service status sometimes incorrectly
+        reports the status of redis-server.
+        * On Ubuntu systems, rapidly restarting supervisor can leave your system
+        in an indeterminate state.
+        """
+        # check that Tower is running on an EL system
+        if not (ansible_os_family == 'RedHat'):
+            pytest.skip("Only supported on EL distributions")
 
         # issue ansible-tower-service command
         contacted = ansible_runner.command('ansible-tower-service ' + command)
@@ -54,4 +55,108 @@ class Test_Ansible_Tower_Service(APITest):
             result = contacted.values()[0]
             assert ("Active: active (running)" in result['stdout']) is active, \
                 u"Unexpected process status for process {0} after executing ansible-tower-service {1}\n\nstdout:\n{2}"\
-                    .format(process, command, result['stdout'])
+                .format(process, command, result['stdout'])
+
+    def test_database_unavailable(self, install_enterprise_license_unlimited,
+                                  factories, v2, ansible_runner, ansible_os_family):
+        if ansible_os_family == 'Debian':
+            pg_service = 'postgresql'
+        else:
+            pg_service = 'postgresql-9.6'
+
+        jt = factories.v2_job_template(playbook='sleep.yml',
+                                       extra_vars=dict(sleep_interval=120))
+        jt.ds.inventory.add_host()
+        job = jt.launch().wait_until_status('running')
+
+        try:
+            contacted = ansible_runner.service(name=pg_service, state='stopped')
+            result = contacted.values()[0]
+            assert not result.get('failed', False), \
+                "Stopping postgres failed. Command stderr: \n{0}\n\nCommand stdout: \n{1}"\
+                .format(result['stderr'], result['stdout'])
+            time.sleep(60)
+        finally:
+            contacted = ansible_runner.service(name=pg_service, state='started')
+            result = contacted.values()[0]
+            assert not result.get('failed', False), \
+                "Starting postgres failed. Command stderr: \n{0}\n\nCommand stdout: \n{1}"\
+                .format(result['stderr'], result['stdout'])
+
+        def online():
+            try:
+                v2.get()
+            except:
+                return False
+            return True
+        utils.poll_until(online, interval=5, timeout=120)
+        job.wait_until_status(['error', 'failed'], since_job_created=False)
+        assert job.job_explanation == 'Task was marked as running in Tower but was not present in the job queue, so it has been marked as failed.'
+
+        jt.extra_vars = '{"sleep_interval": 1}'
+        job = jt.launch().wait_until_completed()
+        assert job.is_successful
+
+    def test_rabbitmq_unavailable(self, install_enterprise_license_unlimited,
+                                  factories, v2, ansible_runner, ansible_os_family):
+        jt = factories.v2_job_template(playbook='sleep.yml',
+                                       extra_vars=dict(sleep_interval=120))
+        jt.ds.inventory.add_host()
+        job = jt.launch().wait_until_status('running')
+
+        try:
+            contacted = ansible_runner.service(name='rabbitmq-server', state='stopped')
+            result = contacted.values()[0]
+            assert not result.get('failed', False), \
+                "Stopping postgres failed. Command stderr: \n{0}\n\nCommand stdout: \n{1}"\
+                .format(result['stderr'], result['stdout'])
+            time.sleep(60)
+        finally:
+            contacted = ansible_runner.service(name='rabbitmq-server', state='started')
+            result = contacted.values()[0]
+            assert not result.get('failed', False), \
+                "Starting postgres failed. Command stderr: \n{0}\n\nCommand stdout: \n{1}"\
+                .format(result['stderr'], result['stdout'])
+
+        def online():
+            try:
+                v2.get()
+            except:
+                return False
+            return True
+        utils.poll_until(online, interval=5, timeout=120)
+        job.wait_until_status('successful', since_job_created=False)
+
+        jt.extra_vars = '{"sleep_interval": 1}'
+        job = jt.launch().wait_until_completed()
+        assert job.is_successful
+
+    def test_tower_restart(self, install_enterprise_license_unlimited, factories, v2, ansible_runner):
+        jt = factories.v2_job_template(playbook='sleep.yml',
+                                       extra_vars=dict(sleep_interval=120))
+        jt.ds.inventory.add_host()
+        job = jt.launch().wait_until_status('running')
+
+        try:
+            contacted = ansible_runner.command('ansible-tower-service stop')
+            result = contacted.values()[0]
+            assert result['rc'] == 0, "ansible-tower-service {0} failed. Command stderr: \n{1}".format(result['stderr'])
+            time.sleep(60)
+        finally:
+            contacted = ansible_runner.command('ansible-tower-service start')
+            result = contacted.values()[0]
+            assert result['rc'] == 0, "ansible-tower-service {0} failed. Command stderr: \n{1}".format(result['stderr'])
+
+        def online():
+            try:
+                v2.get()
+            except:
+                return False
+            return True
+        utils.poll_until(online, interval=5, timeout=120)
+        job.wait_until_status('failed', since_job_created=False)
+        assert job.job_explanation == 'Task was marked as running in Tower but was not present in the job queue, so it has been marked as failed.'
+
+        jt.extra_vars = '{"sleep_interval": 1}'
+        job = jt.launch().wait_until_completed()
+        assert job.is_successful
