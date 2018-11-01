40174ecec41a4dc2bab1fcdb6d7609028f235857
Mat Wilson <mawilson@redhat.com>
2018-11-01 10:55:17 -0700

    add resource creation tests, rename to benchmark

--- tests/api/test_api_benchmark.py
+++ tests/api/test_api_benchmark.py
@@ -51,7 +51,41 @@ class TestApiPerformance(APITest):
     def get_endpoint(self, endpoint):
         endpoint.get()
 
+    def resource_creation_function(self, v2, resource_type):
+        return getattr(v2, resource_type).create
+
     @pytest.mark.parametrize('endpoint', endpoints)
-    def test_endpoint_response_time(self, v2, endpoint, benchmark):
+    def test_benchmark_endpoint_response(self, v2, endpoint, benchmark):
         e = getattr(v2, endpoint)
         benchmark(self.get_endpoint, e)
+
+    def test_benchmark_org_creation(self, v2, benchmark):
+        f = self.resource_creation_function(v2, 'organizations')
+        benchmark(f)
+
+    def test_benchmark_user_creation(self, v2, factories, benchmark):
+        f = self.resource_creation_function(v2, 'users')
+        org = factories.v2_organization()
+        benchmark(f, organization=org)
+
+    def test_benchmark_cloud_credential_creation(self, v2, factories, benchmark):
+        f = self.resource_creation_function(v2, 'credentials')
+        org = factories.v2_organization()
+        benchmark(f, organization=org, kind='aws', username='foo', password='bar')
+
+    def test_benchmark_project_creation(self, v2, factories, benchmark):
+        f = self.resource_creation_function(v2, 'projects')
+        org = factories.v2_organization()
+        benchmark(f, organization=org, wait=False)
+
+    def test_benchmark_inventory_creation(self, v2, factories, benchmark):
+        f = self.resource_creation_function(v2, 'inventory')
+        org = factories.v2_organization()
+        benchmark(f, organization=org)
+
+    def test_job_template_creation(self, v2, factories, benchmark):
+        org = factories.v2_organization()
+        inventory = factories.v2_inventory(organization=org)
+        project = factories.v2_project(organization=org)
+        f = self.resource_creation_function(v2, 'job_templates')
+        benchmark(f, organization=org, inventory=inventory, project=project)
