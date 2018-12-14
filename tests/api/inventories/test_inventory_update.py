c29545951a3df7fbead00f84876c5fc117aa7287
Yanis Guenane <yanis@guenane.org>
2018-12-14 11:38:33 +0100

    Skip test test_inventory_events_are_inserted_in_the_background in OpenShift
    Also victim of out of sync clocks on openshift

--- tests/api/inventories/test_inventory_update.py
+++ tests/api/inventories/test_inventory_update.py
@@ -618,6 +618,7 @@ class TestInventoryUpdate(APITest):
         assert 'ERROR! No inventory was parsed, please check your configuration and options' in inv_update.result_stdout
         assert 'SyntaxError' not in inv_update.result_stdout
 
+    @pytest.mark.skip_openshift  # Github Issue: https://github.com/ansible/tower-qa/issues/2591
     def test_inventory_events_are_inserted_in_the_background(self, factories):
         aws_cred = factories.v2_credential(kind='aws')
         ec2_source = factories.v2_inventory_source(source='ec2', credential=aws_cred)
