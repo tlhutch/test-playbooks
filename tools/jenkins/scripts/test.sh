cdee98976b20e622a37c6068839efef72a644682
Yanis Guenane <yanis@guenane.org>
2019-01-29 13:10:18 +0100

    test.sh: FIPS retrieve proper user based on inventory

--- tools/jenkins/scripts/test.sh
+++ tools/jenkins/scripts/test.sh
@@ -12,7 +12,7 @@ source "$(dirname "${0}")"/lib/common
 
 INVENTORY=$(retrieve_inventory_file)
 TOWER_HOST=$(retrieve_tower_server_from_inventory "${INVENTORY}")
-CREDS=$(retrieve_credential_file "${TOWER_HOST}")
+CREDS=$(retrieve_credential_file "${INVENTORY}")
 
 set +e
 
@@ -20,6 +20,7 @@ TOWERKIT_SCHEMA_VALIDATION=off pytest -c config/api.cfg \
     --ansible-host-pattern="${TOWER_HOST}" \
     --ansible-inventory="${INVENTORY}" \
     --api-credentials="${CREDS}" \
+    --github-cfg="${CREDS}" \
     -k "${TESTEXPR}" --base-url="https://${TOWER_HOST}"
 
 set -e
--- tools/jenkins/scripts/verify.sh
+++ tools/jenkins/scripts/verify.sh
@@ -12,7 +12,7 @@ source "$(dirname "${0}")"/lib/common
 
 INVENTORY=$(retrieve_inventory_file)
 TOWER_HOST="$(retrieve_tower_server_from_inventory "${INVENTORY}")"
-CREDS=$(retrieve_credential_file "${TOWER_HOST}")
+CREDS=$(retrieve_credential_file "${INVENTORY}")
 TOWER_URL="https://${TOWER_HOST}"
 until is_tower_ready "${TOWER_URL}"; do :; done
 
