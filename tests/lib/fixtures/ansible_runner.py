b89a8ca68e3862eb414d913191e630e69ab165f1
Mat Wilson <mawilson@redhat.com>
2019-02-19 17:09:26 -0800

    flake8

--- tests/lib/fixtures/ansible_runner.py
+++ tests/lib/fixtures/ansible_runner.py
@@ -5,6 +5,7 @@ import pytest
 def ansible_runner(ansible_module):
     return ansible_module
 
+
 @pytest.fixture(scope='class')
 def ansible_runner_class(ansible_module):
     return ansible_module
