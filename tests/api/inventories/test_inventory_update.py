ad7ee64c6a8cf609a7411266d086970f8daf5135
Elijah DeLee <kdelee@gmail.com>
2019-02-22 17:08:54 -0500

    Add test to recreate db lock that used to happen

--- tests/api/inventories/test_inventory_update.py
+++ tests/api/inventories/test_inventory_update.py
@@ -151,6 +151,81 @@ class TestInventoryUpdate(APITest):
         for item in compare_list[1:]:
             assert item == compare_list[0]
 
+    def test_update_with_overwrite_attempt_deadlock(self, v2, factories):
+        """Update an inventory with multiple sources that update same groups and hosts.'
+
+        In past, this could result in bad clobbering behavior or a database lock.
+        See https://github.com/ansible/tower/issues/3212.
+        """
+        source_scripts = []
+        inv_sources = []
+        shared_org = factories.organization()
+        shared_parent_inv = factories.inventory(organization=shared_org)
+        custom_script = """#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import uuid
+unique_name = str(uuid.uuid4())
+host_1 = 'host_{}'.format(unique_name)
+host_2 = 'host_2_{}'.format(unique_name)
+print json.dumps({
+'_meta': {'hostvars': {
+    '{}'.format(host_1): {'name':'{}'.format(host_1)},
+    '{}'.format(host_2): {'name':'{}'.format(host_2)},
+    'all_have': {'name':'all_have'},
+    'all_have2': {'name':'all_have2'},
+    'all_have3': {'name':'all_have3'},
+    }},
+'ungrouped': {'hosts': ['groupless']},
+'child_group': {'hosts': ['{}'.format(host_1), 'all_have']},
+'child_group2': {'hosts': ['{}'.format(host_1), 'all_have2']},
+'parent_group': {'hosts': ['{}'.format(host_2), 'all_have3'], 'children': ['child_group', 'child_group2']}
+})"""
+        for i in range(3):
+            inv_script = factories.v2_inventory_script(
+                script=custom_script,
+                organization=shared_org,
+            )
+            inv_source = factories.v2_inventory_source(
+                overwrite=True,
+                overwrite_vars=True,
+                inventory_script=inv_script,
+                organization=shared_org,
+                inventory=shared_parent_inv
+            )
+            source_scripts.append(inv_script)
+            inv_sources.append(inv_source)
+            inv_source.update().wait_until_completed().assert_successful()
+        shared_parent_inv = shared_parent_inv.get()
+        assert shared_parent_inv.total_inventory_sources == 3, "Not all sources were added correctly from independently run updates"
+        # Now try and update all inventories simultaneously
+        # in the past would have run into database lock from race on shared groups and hosts
+        updates = shared_parent_inv.related.update_inventory_sources.post()
+        for update in updates:
+            update_page = v2.inventory_updates.get(id=update['id']).results.pop()
+            update_page.wait_until_completed().assert_successful()
+
+        shared_parent_inv = shared_parent_inv.get()
+        # Each has 2 unique hosts, which makes 6 hosts
+        # Then the three invs all share 4 other hosts, all_have, all_have2, all_have3, and groupless
+        assert shared_parent_inv.total_hosts == 10, 'Should have had 6 unique hosts and 4 shared hosts'
+        assert shared_parent_inv.total_groups == 3, 'Should have had parent group, child_group, and child_group2'
+        # make assertions about group membership
+        groups = shared_parent_inv.related.groups.get().results
+        for group in groups:
+            assert group.name in ['child_group', 'child_group2', 'parent_group']
+            hosts = group.related.hosts.get().results
+            host_names = [host.name for host in hosts]
+            assert len(hosts) == 4, 'Should have 3 unique hosts, one from each inventory, and one shared host'
+            if group.name == 'child_group':
+                assert 'all_have' in host_names
+            if group.name == 'child_group2':
+                assert 'all_have2' in host_names
+            if group.name == 'parent_group':
+                assert 'all_have3' in host_names
+            for host in hosts:
+                assert host.name == host.related.variable_data.get()['name']
+
     def test_update_with_overwrite_deletion(self, factories):
         """Verify inventory update with overwrite will not persist old stuff that it imported.
         * Memberships created within our script-spawned group should removed by a 2nd import.
