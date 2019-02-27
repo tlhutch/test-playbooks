c8eaaf56e76f7e7929de666588d645a97fd684d6
Yanis Guenane <yanis@guenane.org>
2019-02-27 11:59:31 +0100

    Pipeline: integration ensuire junit results are displayed in Jenkins as junit

--- tools/jenkins/pipelines/integration.groovy
+++ tools/jenkins/pipelines/integration.groovy
@@ -172,6 +172,7 @@ Bundle?: ${params.BUNDLE}"""
             steps {
                sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
                    sh 'ansible-playbook -v -i playbooks/inventory.test_runner playbooks/test_runner/run_integration_test.yml'
+                   junit 'artifacts/results.xml'
                 }
             }
         }
