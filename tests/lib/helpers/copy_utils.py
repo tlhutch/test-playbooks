fb5d06686433e88158a7235c34526bd3d52eb198
AlanCoding <arominge@redhat.com>
2019-03-05 08:19:38 -0500

    add verbosity to field differ assertion fixture to clarify flake

--- tests/lib/helpers/copy_utils.py
+++ tests/lib/helpers/copy_utils.py
@@ -1,10 +1,21 @@
 def check_fields(original, replica, identical_fields, unequal_fields, no_assert=False):
     try:
-        for field in identical_fields:
-            assert original.json[field] == replica.json[field]
+        errors = ''
+        try:
+            for field in identical_fields:
+                assert original.json[field] == replica.json[field], field
+        except AssertionError as e:
+            errors += str(e)
 
-        for field in unequal_fields:
-            assert original.json[field] != replica.json[field]
+        try:
+            for field in unequal_fields:
+                assert original.json[field] != replica.json[field], field
+        except AssertionError as e:
+            errors += str(e)
+
+        if errors:
+            raise AssertionError(errors)
+
     except AssertionError:
         if no_assert:
             return False
