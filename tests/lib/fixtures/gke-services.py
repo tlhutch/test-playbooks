9cd682ade93d4c2ee4b76e7374d0c445e9d62f9a
Mat Wilson <mawilson@redhat.com>
2019-03-15 00:49:59 -0700

    add gke_client fixture and update creds

---
+++ tests/lib/fixtures/gke-services.py
@@ -0,0 +1,26 @@
+import pytest
+import googleapiclient.discovery
+import kubernetes.client
+import base64
+from google.oauth2 import service_account
+import yaml
+
+
+@pytest.fixture
+def gke_client():
+    credentials = yaml.load(open("config/credentials.yml", 'r'))
+    sa_creds = service_account.Credentials.from_service_account_info(credentials['cloud']['gke']['serviceaccount']['key'])
+    gke = googleapiclient.discovery.build('container', 'v1', credentials=sa_creds)
+    name = credentials['cloud']['gke']['cluster']
+    gke_clusters = gke.projects().locations().clusters()
+    gke_cluster = gke_clusters.get(name=name).execute()
+    kube_config = kubernetes.client.Configuration()
+    kube_config.host = 'https://{}'.format(gke_cluster['endpoint'])
+    kube_config.verify_ssl = True
+    kube_config.api_key['authorization'] = credentials['cloud']['gke']['api_key']
+    kube_config.api_key_prefix['authorization'] = 'Bearer'
+    kube_config.ssl_ca_cert = '/tmp/ssl_ca_cert'
+    with open(kube_config.ssl_ca_cert, 'wb') as f:
+        f.write(base64.decodestring(gke_cluster['masterAuth']['clusterCaCertificate'].encode()))
+
+    return kubernetes.client.ApiClient(configuration=kube_config)
