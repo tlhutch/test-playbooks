d8f0761c924e5e3c05584664f0a6adf60fc304a5
Yanis Guenane <yanis@guenane.org>
2019-03-19 10:29:25 +0100

    Pipeline: Add brew pipeline

---
+++ tools/jenkins/pipelines/brew.groovy
@@ -0,0 +1,55 @@
+pipeline {
+
+    agent none
+
+    parameters {
+        string(
+            name: 'TOWER_RELEASE',
+            defaultValue: 'devel',
+            description: 'Tower release to build'
+        )
+    }
+
+    options {
+        timestamps()
+    }
+
+    stages {
+
+        stage('Build Tower Brew RPM') {
+            steps {
+                build(
+                    job: 'Brew/Build_Tower_Brew_RPM',
+                    parameters: [
+                        string(name: 'TOWER_PACKAGING_BRANCH', value: "origin/${params.TOWER_RELEASE}"),
+                        booleanParam(name: 'TRIGGER', value: false)
+                    ]
+                )
+            }
+        }
+
+        stage('Build Tower Brew Container Images') {
+            steps {
+                build(
+                    job: 'Brew/Build_Tower_Brew_Container_Image',
+                    parameters: [
+                        string(name: 'TOWER_PACKAGING_BRANCH', value: "origin/${params.TOWER_RELEASE}"),
+                        booleanParam(name: 'TRIGGER', value: false)
+                    ]
+                )
+            }
+        }
+
+        stage('Push Brew Container Images') {
+            steps {
+                build(
+                    job: 'Brew/Push_Brew_Container_Image',
+                    parameters: [
+                        string(name: 'TOWER_PACKAGING_BRANCH', value: "origin/${params.TOWER_RELEASE}"),
+                        booleanParam(name: 'TRIGGER', value: false)
+                    ]
+                )
+            }
+        }
+    }
+}
