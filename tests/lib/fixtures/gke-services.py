23cfd0f050f76e009c516731adcf90533ec5690c
Mat Wilson <mawilson@redhat.com>
2019-03-18 17:21:02 -0700

    create client object, add deployment generator and teardown method

--- tests/lib/fixtures/gke-services.py
+++ tests/lib/fixtures/gke-services.py
@@ -3,6 +3,37 @@ import googleapiclient.discovery
 import kubernetes.client
 import base64
 from google.oauth2 import service_account
+import fauxfactory
+
+
+class K8sClient(object):
+    def __init__(self, client):
+        self.client = client
+        self.apps = kubernetes.client.AppsV1Api(self.client)
+        self.core = kubernetes.client.CoreV1Api(self.client)
+        self.storage = kubernetes.client.StorageV1Api(self.client)
+        self.deploymentobject = kubernetes.client.V1Deployment()
+        self.serviceobject = kubernetes.client.V1Service()
+        self.secretobject = kubernetes.client.V1Secret()
+        self.configmapobject = kubernetes.client.V1ConfigMap()
+
+    def generate_deployment(self, prefix, containerspec):
+        deployment = self.deploymentobject
+        deploymentname = '{}-{}'.format(prefix, fauxfactory.gen_string('alphanumeric', 5)).lower()
+        deployment.api_version = 'apps/v1'
+        deployment.metadata = {'name': deploymentname,
+                               'integration': 'true'}
+        deployment.spec = {}
+        deployment.spec['selector'] = {'matchLabels': {'run': deploymentname}}
+        deployment.spec['template'] = {}
+        deployment.spec['template']['metadata'] = {'labels': {'run': deploymentname}}
+        deployment.spec['template']['spec'] = {'containers': containerspec}
+
+        return deploymentname, deployment
+
+    def destroy(self, deploymentname):
+        self.core.delete_namespaced_service(deploymentname, 'default', body=kubernetes.client.V1DeleteOptions())
+        self.apps.delete_namespaced_deployment(deploymentname, 'default', body=kubernetes.client.V1DeleteOptions())
 
 
 def create_gke_clients():
@@ -21,7 +52,7 @@ def create_gke_clients():
         with open(kube_config.ssl_ca_cert, 'wb') as f:
             f.write(base64.decodestring(gke_cluster['masterAuth']['clusterCaCertificate'].encode()))
         client = kubernetes.client.ApiClient(configuration=kube_config)
-        return kubernetes.client.AppsV1Api(client), kubernetes.client.CoreV1Api(client)
+        return K8sClient(client)
     return create_clients
 
 
