92d5dbfc2070d36eaa4297af0fa70f6dd29f8ad5
Mat Wilson <mawilson@redhat.com>
2019-03-26 17:10:45 -0700

    add rbac use test for azure kv

--- tests/api/credentials/test_credential_plugins.py
+++ tests/api/credentials/test_credential_plugins.py
@@ -858,3 +858,42 @@ class TestAzureKVCredentials(APITest):
 
         hostvars = job.related.job_events.get(host=host.id, task='debug', event__startswith='runner_on_ok').results.pop().event_data.res.hostvars
         assert hostvars[host.name]['extra_var_from_field_one'] == 'latest-username'
+
+    def test_azure_key_vault_RBAC_users_can_be_assigned_use_on_credentials(self, factories, v2):
+        azure_credential = self.create_azurekv_credential(factories, v2, 'https://qecredplugin.vault.azure.net/')
+        org = factories.v2_organization()
+        user = factories.user(organization=org)
+
+        # create an SSH credential
+        cred_type = v2.credential_types.get(managed_by_tower=True, kind='ssh').results.pop()
+        payload = factories.v2_credential.payload(
+            name=fauxfactory.gen_utf8(),
+            description=fauxfactory.gen_utf8(),
+            credential_type=cred_type
+        )
+        credential = v2.credentials.post(payload)
+
+        metadata = {
+            'secret_field': 'example-user'
+        }
+        credential.related.input_sources.post(dict(
+            input_field_name='username',
+            source_credential=azure_credential.id,
+            metadata=metadata,
+        ))
+
+        credential.patch(organization=org.id)
+        credential.set_object_roles(user, 'use')
+
+        jt = factories.v2_job_template()
+        resources = ['inventory', 'credential', 'project']
+        for r in resources:
+            jt.ds[r].patch(organization=org.id)
+            jt.ds[r].set_object_roles(user, 'use')
+        jt.set_object_roles(user, 'admin')
+
+        with self.current_user(username=user.username, password=user.password):
+            jt.patch(credential=credential.id)
+            job = jt.launch().wait_until_completed()
+
+        assert job.is_successful
