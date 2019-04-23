bd9feec6119a032fa8d0548a0a06acfab4248138
Yanis Guenane <yanis@guenane.org>
2019-04-23 09:49:10 +0200

    Pipeline: Integration comment in E2E testing

--- tools/jenkins/pipelines/integration.groovy
+++ tools/jenkins/pipelines/integration.groovy
@@ -163,13 +163,14 @@ Bundle?: ${params.BUNDLE}"""
             }
         }
 
-        stage ('E2E Tests') {
-            steps {
-               sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
-                   sh 'ansible-playbook -v -i playbooks/inventory.test_runner playbooks/test_runner/run_e2e_test.yml'
-                }
-            }
-        }
+        // NOTE: https://github.com/ansible/tower-qa/issues/3216:w
+        // stage ('E2E Tests') {
+        //     steps {
+        //        sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
+        //            sh 'ansible-playbook -v -i playbooks/inventory.test_runner playbooks/test_runner/run_e2e_test.yml'
+        //         }
+        //     }
+        // }
 
         stage ('Integration Tests') {
             steps {
