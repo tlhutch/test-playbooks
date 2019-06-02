fa0768873127f186e0274f6fd113d17c9849bfe4
Yanis Guenane <yanis@guenane.org>
2019-06-02 18:48:04 +0200

    issue_bot.py: Update to reflect process change

--- tools/bots/issue_bot.py
+++ tools/bots/issue_bot.py
@@ -1,18 +1,67 @@
 #!/usr/bin/env python
 
 import os
+import requests
 
-from github import Github
 from slacker import Slacker
 
-github_username = os.getenv('GITHUB_USERNAME')
-github_password = os.getenv('GITHUB_PASSWORD')
-github_repo = os.getenv('GITHUB_REPO')
-github_milestone = os.getenv('GITHUB_MILESTONE')
+
+github_token = os.getenv('GITHUB_TOKEN')
+tower_version = os.getenv('TOWER_VERSION')
 
 slack_token = os.getenv('SLACK_TOKEN')
 slack_channel = os.getenv('SLACK_CHANNEL')
 slack = Slacker(slack_token)
+
+API_GITHUB = 'https://api.github.com'
+
+
+def github_request(url):
+    """Generic Github request handler."""
+
+    res = requests.get(
+        url,
+        headers={
+            'Authorization': 'token %s' % github_token,
+            'Accept': 'application/vnd.github.inertia-preview+json'
+        }
+    )
+
+    return res
+
+
+def get_project_id_and_number(version):
+    """Return the project id associated with a Tower version."""
+
+    url = '{}/orgs/ansible/projects'.format(API_GITHUB)
+    projects = github_request(url).json()
+
+    return [(project['id'], project['number']) for project in projects if project['name'] == 'Ansible Tower {}'.format(version)][0]
+
+
+def get_issues_from_project(project_id):
+    """Return all the issues in a project."""
+
+    _issues = []
+    # Retrieve list of cards_url
+    url = '{}/projects/{}/columns'.format(API_GITHUB, project_id)
+
+    _columns = github_request(url).json()
+    cards_url = [column['cards_url'] for column in _columns if 'QE' not in column['name']]
+
+    for url in cards_url:
+        _issues += [github_request(_issue['content_url']).json() for _issue in github_request(url).json()]
+
+    return [issue for issue in _issues if issue['state'] == 'open']
+
+
+def is_issue_in_need_test(issue):
+
+    for label in issue['labels']:
+        if label['name'] == 'state:needs_test':
+            return True
+
+    return False
 
 
 def post_slack_msg(text):
@@ -20,27 +69,21 @@ def post_slack_msg(text):
 
 
 def create_issue_update():
-    git = Github(github_username, github_password)
-    repo = git.get_repo(github_repo)
-    milestones = repo.get_milestones(state="open")
-    for milestone in milestones:
-        if milestone.title.lower() == github_milestone.lower():
-            current_milestone = milestone
-            break
-    else:
-        raise Exception("Failed to find milestone '{}'".format(github_milestone))
 
-    issues = repo.get_issues(milestone=current_milestone)
-    num_issues = len([i for i in issues if not i.pull_request])
+    project_id, project_number = get_project_id_and_number(tower_version)
+    issues = get_issues_from_project(project_id)
 
     needs_test = 0
-    for i in issues:
-        for label in i._labels.value:
-            if label._name.value == 'state:needs_test':
-                needs_test += 1
+    for issue in issues:
+        if is_issue_in_need_test(issue):
+            needs_test += 1
 
-    url = 'https://github.com/{}/issues?q=is:open+is:issue+milestone:{}'.format(github_repo, github_milestone)
-    return "{} open issues in milestone `{}` ({} in `state:needs_test`) - Link: {}".format(num_issues, github_milestone, needs_test, url)
+    url = 'https://github.com/issues?q=is:open+is:issue+project:ansible/{}'.format(
+        project_number
+    )
+    return "{} open issues in project `{}` ({} in `state:needs_test`) - Link: {}".format(
+        len(issues), tower_version, needs_test, url
+    )
 
 
 post_slack_msg(create_issue_update())
