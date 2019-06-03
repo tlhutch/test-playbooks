19dfc5131bba27d0bf23d14ba1e0810f6e25247f
Mat Wilson <mawilson@redhat.com>
2019-06-03 16:25:17 -0700

    vcenter spawn minimum viable

--- tests/api/inventories/test_vmware_inventory.py
+++ tests/api/inventories/test_vmware_inventory.py
@@ -15,3 +15,35 @@ def k8s_govcsim(gke_client_cscope, request):
     prefix = 'govcsim'
     cluster_domain = 'services.k8s.tower-qe.testing.ansible.com'
     deployment_name = K8sClient.random_deployment_name(prefix)
+    containerspec = [{'name': '{}-{}'.format(deployment_name, 'app'),
+                        'image': 'quay.io/ansible/vcenter-test-container',
+                        'ports': [{'containerPort': 5000, 'protocol': 'TCP'},
+                                  {'containerPort': 443, 'protocol': 'TCP'}],
+                        'env': []}]
+    portspec = [{'name': 'govcsimcontrol',
+                 'port': 5000,
+                 'protocol': 'TCP',
+                 'targetPort': 5000},
+                {'name': 'govcsimserver',
+                 'port': 443,
+                 'protocol': 'TCP',
+                 'targetPort': 443}]
+    govcsim_deployment = K8sClient.generate_deployment(deployment_name, containerspec)
+    govcsim_service = K8sClient.generate_service(deployment_name, portspec)
+    K8sClient.apps.create_namespaced_deployment(body=govcsim_deployment, namespace='default')
+    K8sClient.core.create_namespaced_service(body=govcsim_service, namespace='default')
+    request.addfinalizer(lambda: K8sClient.destroy(deployment_name))
+    controller_url = "https://http-{}-port-5000.{}".format(deployment_name, cluster_domain)
+    sim_url = "https://https-{}-port-443.{}".format(deployment_name, cluster_domain)
+
+    sess = requests.Session()
+    sim = sess.get('{}/spawn?username=user&password=pass&cluster=1&port=443&vm=5'.format(controller_url))
+    return sim_url
+
+
+@pytest.mark.api
+@pytest.mark.destructive
+@pytest.mark.usefixtures('authtoken', 'install_enterprise_license_unlimited')
+class TestGovcsim(APITest):
+    def test_govcsim_spawn(self, k8s_govcsim):
+    
