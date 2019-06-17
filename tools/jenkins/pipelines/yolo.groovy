8d2061b58a0d2370197dd82cc0d47da452a23adb
Elyézer Rezende <elyezermr@gmail.com>
2019-06-17 17:57:09 -0400

    Run some stages in parallel in yolo

--- tools/jenkins/pipelines/yolo.groovy
+++ tools/jenkins/pipelines/yolo.groovy
@@ -129,155 +129,160 @@ pipeline {
             }
         }
 
-        stage('Build Installer') {
-            when {
-                expression {
-                    return params.BUILD_INSTALLER_AND_PACKAGE
-                }
-            }
+        stage('Build Tower and deploy test-runner') {
+            failFast true
+            parallel {
+                stage('Build Installer') {
+                    when {
+                        expression {
+                            return params.BUILD_INSTALLER_AND_PACKAGE
+                        }
+                    }
 
-            steps {
-                build(
-                    job: 'Build_Tower_TAR',
-                    parameters: [
-                        string(
-                            name: 'TOWER_PACKAGING_REPO',
-                            value: "git@github.com:${params.TOWER_PACKAGING_FORK}/tower-packaging.git"
-                        ),
-                        string(
-                            name: 'TOWER_PACKAGING_BRANCH',
-                            value: "origin/${params.TOWER_PACKAGING_BRANCH}"
-                        ),
-                        string(
-                            name: 'TOWER_REPO',
-                            value: "git@github.com:${params.TOWER_FORK}/${params.PRODUCT}.git"
-                        ),
-                        string(
-                            name: 'TOWER_BRANCH',
-                            value: "origin/${params.TOWER_BRANCH}"
-                        ),
-                        string(
-                            name: 'NIGHTLY_REPO_DIR',
-                            value: NIGHTLY_REPO_DIR
+                    steps {
+                        build(
+                            job: 'Build_Tower_TAR',
+                            parameters: [
+                                string(
+                                    name: 'TOWER_PACKAGING_REPO',
+                                    value: "git@github.com:${params.TOWER_PACKAGING_FORK}/tower-packaging.git"
+                                ),
+                                string(
+                                    name: 'TOWER_PACKAGING_BRANCH',
+                                    value: "origin/${params.TOWER_PACKAGING_BRANCH}"
+                                ),
+                                string(
+                                    name: 'TOWER_REPO',
+                                    value: "git@github.com:${params.TOWER_FORK}/${params.PRODUCT}.git"
+                                ),
+                                string(
+                                    name: 'TOWER_BRANCH',
+                                    value: "origin/${params.TOWER_BRANCH}"
+                                ),
+                                string(
+                                    name: 'NIGHTLY_REPO_DIR',
+                                    value: NIGHTLY_REPO_DIR
+                                )
+                            ]
                         )
-                    ]
-                )
-            }
-        }
-
-        stage('Build Package') {
-            when {
-                expression {
-                    return params.BUILD_INSTALLER_AND_PACKAGE
+                    }
                 }
-            }
 
-            steps {
-                script {
-                    if ( params.PLATFORM.contains('ubuntu') ) {
-                        PACKAGE_JOB_NAME = 'Build_Tower_DEB'
-                    } else {
-                        PACKAGE_JOB_NAME = 'Build_Tower_RPM'
+                stage('Build Package') {
+                    when {
+                        expression {
+                            return params.BUILD_INSTALLER_AND_PACKAGE
+                        }
                     }
 
-                    build(
-                        job: PACKAGE_JOB_NAME,
-                        parameters: [
-                            string(
-                                name: 'TOWER_PACKAGING_REPO',
-                                value: "git@github.com:${params.TOWER_PACKAGING_FORK}/tower-packaging.git"
-                            ),
-                            string(
-                                name: 'TOWER_PACKAGING_BRANCH',
-                                value: "origin/${params.TOWER_PACKAGING_BRANCH}"
-                            ),
-                            string(
-                                name: 'TOWER_REPO',
-                                value: "git@github.com:${params.TOWER_FORK}/${params.PRODUCT}.git"
-                            ),
-                            string(
-                                name: 'TOWER_BRANCH',
-                                value: "origin/${params.TOWER_BRANCH}"
-                            ),
-                            string(
-                                name: 'NIGHTLY_REPO_DIR',
-                                value: NIGHTLY_REPO_DIR
-                            ),
-                            string(
-                                name: 'TARGET_DIST',
-                                value: target_dist
-                            ),
-                            string(
-                                name: 'MOCK_CFG',
-                                value: mock_cfg
-                            ),
-                            booleanParam(
-                                name: 'TRIGGER',
-                                value: false
+                    steps {
+                        script {
+                            if ( params.PLATFORM.contains('ubuntu') ) {
+                                PACKAGE_JOB_NAME = 'Build_Tower_DEB'
+                            } else {
+                                PACKAGE_JOB_NAME = 'Build_Tower_RPM'
+                            }
+
+                            build(
+                                job: PACKAGE_JOB_NAME,
+                                parameters: [
+                                    string(
+                                        name: 'TOWER_PACKAGING_REPO',
+                                        value: "git@github.com:${params.TOWER_PACKAGING_FORK}/tower-packaging.git"
+                                    ),
+                                    string(
+                                        name: 'TOWER_PACKAGING_BRANCH',
+                                        value: "origin/${params.TOWER_PACKAGING_BRANCH}"
+                                    ),
+                                    string(
+                                        name: 'TOWER_REPO',
+                                        value: "git@github.com:${params.TOWER_FORK}/${params.PRODUCT}.git"
+                                    ),
+                                    string(
+                                        name: 'TOWER_BRANCH',
+                                        value: "origin/${params.TOWER_BRANCH}"
+                                    ),
+                                    string(
+                                        name: 'NIGHTLY_REPO_DIR',
+                                        value: NIGHTLY_REPO_DIR
+                                    ),
+                                    string(
+                                        name: 'TARGET_DIST',
+                                        value: target_dist
+                                    ),
+                                    string(
+                                        name: 'MOCK_CFG',
+                                        value: mock_cfg
+                                    ),
+                                    booleanParam(
+                                        name: 'TRIGGER',
+                                        value: false
+                                    )
+                                ]
                             )
-                        ]
-                    )
-                }
-            }
-        }
-
-        stage('Deploy test-runner node') {
-            when {
-                expression {
-                    return params.RUN_INSTALLER
-                }
-            }
-
-            steps {
-                checkout([
-                    $class: 'GitSCM',
-                    branches: [[name: "*/${params.TOWER_QA_BRANCH}" ]],
-                    userRemoteConfigs: [
-                        [
-                            credentialsId: 'd2d4d16b-dc9a-461b-bceb-601f9515c98a',
-                            url: "git@github.com:${params.TOWER_QA_FORK}/tower-qa.git"
-                        ]
-                    ]
-                ])
-
-                script {
-                    if (params.RUNNER_FORK != '' && params.RUNNER_BRANCH != '') {
-                        AWX_ANSIBLE_RUNNER_URL = "https://github.com/${params.RUNNER_FORK}/ansible-runner.git@${params.RUNNER_BRANCH}"
-                    } else {
-                        AWX_ANSIBLE_RUNNER_URL = ''
+                        }
                     }
                 }
 
-                withCredentials([file(credentialsId: '171764d8-e57c-4332-bff8-453670d0d99f', variable: 'PUBLIC_KEY'),
-                                 file(credentialsId: 'abcd0260-fb83-404e-860f-f9697911a0bc', variable: 'VAULT_FILE'),
-                                 file(credentialsId: '86ed99e9-dad9-49e9-b0db-9257fb563bad', variable: 'JSON_KEY_FILE'),
-                                 string(credentialsId: 'aws_access_key', variable: 'AWS_ACCESS_KEY'),
-                                 string(credentialsId: 'aws_secret_key', variable: 'AWS_SECRET_KEY'),
-                                 string(credentialsId: 'awx_admin_password', variable: 'AWX_ADMIN_PASSWORD')]) {
-                    withEnv(["AWS_SECRET_KEY=${AWS_SECRET_KEY}",
-                             "AWS_ACCESS_KEY=${AWS_ACCESS_KEY}",
-                             "AWX_ADMIN_PASSWORD=${AWX_ADMIN_PASSWORD}",
-                             "AWX_ANSIBLE_RUNNER_URL=${AWX_ANSIBLE_RUNNER_URL}",
-                             "SCENARIO=${SCENARIO}",
-                             "PLATFORM=${PLATFORM}",
-                             "ANSIBLE_VERSION=${ANSIBLE_NIGHTLY_BRANCH}",
-                             "DEPLOYMENT_NAME=yolo-build-${env.BUILD_ID}",
-                             "AW_REPO_URL=http://nightlies.testing.ansible.com/ansible-tower_nightlies_m8u16fz56qr6q7/${NIGHTLY_REPO_DIR}"]) {
-                        sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
-                            sh 'mkdir -p ~/.ssh && cp ${PUBLIC_KEY} ~/.ssh/id_rsa.pub'
-                            sh 'cp ${JSON_KEY_FILE} json_key_file'
-                            sh 'ansible-vault decrypt --vault-password-file="${VAULT_FILE}" config/credentials.vault --output=config/credentials.yml'
-                            sh 'ansible-vault decrypt --vault-password-file="${VAULT_FILE}" config/credentials-pkcs8.vault --output=config/credentials-pkcs8.yml || true'
+                stage('Deploy test-runner node') {
+                    when {
+                        expression {
+                            return params.RUN_INSTALLER
+                        }
+                    }
 
-                            // Generate variable file for test runner
-                            sh 'SCENARIO=test_runner ./tools/jenkins/scripts/generate_vars.sh'
+                    steps {
+                        checkout([
+                            $class: 'GitSCM',
+                            branches: [[name: "*/${params.TOWER_QA_BRANCH}" ]],
+                            userRemoteConfigs: [
+                                [
+                                    credentialsId: 'd2d4d16b-dc9a-461b-bceb-601f9515c98a',
+                                    url: "git@github.com:${params.TOWER_QA_FORK}/tower-qa.git"
+                                ]
+                            ]
+                        ])
 
-                            // Generate variable file for tower deployment
-                            sh './tools/jenkins/scripts/generate_vars.sh'
+                        script {
+                            if (params.RUNNER_FORK != '' && params.RUNNER_BRANCH != '') {
+                                AWX_ANSIBLE_RUNNER_URL = "https://github.com/${params.RUNNER_FORK}/ansible-runner.git@${params.RUNNER_BRANCH}"
+                            } else {
+                                AWX_ANSIBLE_RUNNER_URL = ''
+                            }
+                        }
 
-                            sh 'ansible-playbook -v -i playbooks/inventory -e @playbooks/test_runner_vars.yml playbooks/deploy-test-runner.yml'
+                        withCredentials([file(credentialsId: '171764d8-e57c-4332-bff8-453670d0d99f', variable: 'PUBLIC_KEY'),
+                                        file(credentialsId: 'abcd0260-fb83-404e-860f-f9697911a0bc', variable: 'VAULT_FILE'),
+                                        file(credentialsId: '86ed99e9-dad9-49e9-b0db-9257fb563bad', variable: 'JSON_KEY_FILE'),
+                                        string(credentialsId: 'aws_access_key', variable: 'AWS_ACCESS_KEY'),
+                                        string(credentialsId: 'aws_secret_key', variable: 'AWS_SECRET_KEY'),
+                                        string(credentialsId: 'awx_admin_password', variable: 'AWX_ADMIN_PASSWORD')]) {
+                            withEnv(["AWS_SECRET_KEY=${AWS_SECRET_KEY}",
+                                     "AWS_ACCESS_KEY=${AWS_ACCESS_KEY}",
+                                     "AWX_ADMIN_PASSWORD=${AWX_ADMIN_PASSWORD}",
+                                     "AWX_ANSIBLE_RUNNER_URL=${AWX_ANSIBLE_RUNNER_URL}",
+                                     "SCENARIO=${SCENARIO}",
+                                     "PLATFORM=${PLATFORM}",
+                                     "ANSIBLE_VERSION=${ANSIBLE_NIGHTLY_BRANCH}",
+                                     "DEPLOYMENT_NAME=yolo-build-${env.BUILD_ID}",
+                                     "AW_REPO_URL=http://nightlies.testing.ansible.com/ansible-tower_nightlies_m8u16fz56qr6q7/${NIGHTLY_REPO_DIR}"]) {
+                                sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
+                                    sh 'mkdir -p ~/.ssh && cp ${PUBLIC_KEY} ~/.ssh/id_rsa.pub'
+                                    sh 'cp ${JSON_KEY_FILE} json_key_file'
+                                    sh 'ansible-vault decrypt --vault-password-file="${VAULT_FILE}" config/credentials.vault --output=config/credentials.yml'
+                                    sh 'ansible-vault decrypt --vault-password-file="${VAULT_FILE}" config/credentials-pkcs8.vault --output=config/credentials-pkcs8.yml || true'
 
-                            sh "ansible test-runner -i playbooks/inventory.test_runner -m git -a 'repo=git@github.com:${params.TOWER_QA_FORK}/tower-qa version=${params.TOWER_QA_BRANCH} dest=tower-qa ssh_opts=\"-o StrictHostKeyChecking=no\" force=yes'"
+                                    // Generate variable file for test runner
+                                    sh 'SCENARIO=test_runner ./tools/jenkins/scripts/generate_vars.sh'
+
+                                    // Generate variable file for tower deployment
+                                    sh './tools/jenkins/scripts/generate_vars.sh'
+
+                                    sh 'ansible-playbook -v -i playbooks/inventory -e @playbooks/test_runner_vars.yml playbooks/deploy-test-runner.yml'
+
+                                    sh "ansible test-runner -i playbooks/inventory.test_runner -m git -a 'repo=git@github.com:${params.TOWER_QA_FORK}/tower-qa version=${params.TOWER_QA_BRANCH} dest=tower-qa ssh_opts=\"-o StrictHostKeyChecking=no\" force=yes'"
+                                }
+                            }
                         }
                     }
                 }
@@ -292,9 +297,9 @@ pipeline {
             }
 
             steps {
-               sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
-                   sh 'ansible-playbook -v -i playbooks/inventory.test_runner playbooks/test_runner/run_install.yml'
-                   sh 'ansible-playbook -v -i playbooks/inventory.test_runner playbooks/test_runner/run_fetch_artifacts.yml'
+                sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
+                    sh 'ansible-playbook -v -i playbooks/inventory.test_runner playbooks/test_runner/run_install.yml'
+                    sh 'ansible-playbook -v -i playbooks/inventory.test_runner playbooks/test_runner/run_fetch_artifacts.yml'
                 }
             }
         }
