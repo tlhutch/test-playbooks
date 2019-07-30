290e9b85086776406d11b25f09a06ec2ae4f2206
John Hill <johill@redhat.com>
2019-07-30 11:23:39 -0400

    Re-enable e2e stage (#3853)
    * Re-enable e2e stage and convert to shell statement for live output

--- tools/jenkins/pipelines/integration.groovy
+++ tools/jenkins/pipelines/integration.groovy
@@ -163,14 +163,13 @@ Bundle?: ${params.BUNDLE}"""
             }
         }
 
-        // NOTE: https://github.com/ansible/tower-qa/issues/3216:w
-        // stage ('E2E Tests') {
-        //     steps {
-        //        sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
-        //            sh 'ansible-playbook -v -i playbooks/inventory.test_runner playbooks/test_runner/run_e2e_test.yml'
-        //         }
-        //     }
-        // }
+        stage ('E2E Tests') {
+            steps {
+                sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
+                    sh "ssh ${SSH_OPTS} ec2-user@${TEST_RUNNER_HOST} 'cd tower-qa && ./tools/jenkins/scripts/e2e.sh'"
+                }
+            }
+        }
 
         stage ('Integration Tests') {
             steps {
