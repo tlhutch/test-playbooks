fc768cb426e44ee3151f5001efaf82035382a768
Elyézer Rezende <elyezermr@gmail.com>
2019-08-05 14:23:21 -0400

    Copy artifacts from the right job on LP pipelines
    Copy the jUnit XML from the right job

--- tools/jenkins/pipelines/lp-validator.groovy
+++ tools/jenkins/pipelines/lp-validator.groovy
@@ -76,7 +76,7 @@ The <http://jenkins.ansible.eng.rdu2.redhat.com/view/Tower/job/Pipelines/job/lpt
                         ]
                     )
 
-                    copyArtifacts filter: 'reports/junit/results-final.xml', fingerprintArtifacts: true, flatten: true, projectName: 'qe-sandbox/elyezer/elyezer-lptesting-pipeline', selector: specific("${finalStatus.number}")
+                    copyArtifacts filter: 'reports/junit/results-final.xml', fingerprintArtifacts: true, flatten: true, projectName: 'Pipelines/lptesting-pipeline', selector: specific("${finalStatus.number}")
                     sh 'yum install -y fpaste'
                     fpaste_url = sh(script: 'fpaste results-final.xml 2>/dev/null', returnStdout: true)
                     fpaste_url = fpaste_url.split(' ')[-1].trim()
