458c656187b5e13a99112f2e44e387793777c081
Yanis Guenane <yanis@guenane.org>
2019-08-06 15:54:51 +0200

    Pipeline: New brew pipeline script based only

--- tools/jenkins/pipelines/brew.groovy
+++ tools/jenkins/pipelines/brew.groovy
@@ -1,12 +1,47 @@
 pipeline {
 
-    agent none
+    agent { label 'buildvm' }
 
     parameters {
         string(
-            name: 'TOWER_VERSION',
-            defaultValue: 'devel',
-            description: 'Tower version to build'
+            name: 'TOWER_FORK',
+            description: 'Fork of tower to deploy',
+            defaultValue: 'ansible'
+        )
+        string(
+            name: 'TOWER_BRANCH',
+            description: 'Branch to use for Tower',
+            defaultValue: 'devel'
+        )
+        string(
+            name: 'TOWER_PACKAGING_FORK',
+            description: 'Fork of tower-packaging to deploy',
+            defaultValue: 'ansible'
+        )
+        string(
+            name: 'TOWER_PACKAGING_BRANCH',
+            description: 'Branch to use for tower-packaging',
+            defaultValue: 'devel'
+        )
+        choice(
+            name: 'TARGET_DIST',
+            description: 'mock environment to use',
+            choices: ['epel-7-x86_64', 'epel-8-x86_64']
+        )
+        choice(
+            name: 'OFFICIAL',
+            description: 'Is this part of an official release ?',
+            choices: ['no', 'yes']
+        )
+        choice(
+            name: 'BUILD_CONTAINER',
+            description: 'Should the container image be built ?',
+            choices: ['yes', 'no']
+        )
+        choice(
+            name: 'PUSH_CONTAINER',
+            description: 'Should the container image be pushed ?',
+            choices: ['yes', 'no']
         )
     }
 
@@ -17,40 +52,85 @@ pipeline {
 
     stages {
 
+        stage('Checkout tower-packaging') {
+            steps {
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: "*/${params.TOWER_PACKAGING_BRANCH}" ]],
+                    userRemoteConfigs: [
+                        [
+                            credentialsId: 'd2d4d16b-dc9a-461b-bceb-601f9515c98a',
+                            url: "git@github.com:${params.TOWER_PACKAGING_FORK}/tower-packaging.git"
+                        ]
+                    ]
+                ])
+            }
+        }
+
         stage('Build Tower Brew RPM') {
             steps {
-                build(
-                    job: '../Brew/Build_Tower_Brew_RPM',
-                    parameters: [
-                        string(name: 'TOWER_PACKAGING_BRANCH', value: "origin/${params.TOWER_VERSION}"),
-                        booleanParam(name: 'TRIGGER', value: false)
-                    ]
-                )
+                withCredentials([string(credentialsId: 'red_hat_password', variable: 'RED_HAT_PASSWORD')]) {
+                    withEnv(["OFFICIAL=${params.OFFICIAL}",
+                             "TOWER_REPO=git@github.com:${params.TOWER_FORK}/tower.git",
+                             "TOWER_BRANCH=${params.TOWER_BRANCH}",
+                             "TARGET_DIST=${params.TARGET_DIST}",
+                             "KEYTAB_FILE=/home/jenkins/ansible-tower-build.keytab",
+                             "RED_HAT_USER=shanemcd@redhat.com",
+                             "RED_HAT_PASSWORD=${RED_HAT_PASSWORD}"]) {
+                        sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
+                            sh './tools/jenkins/scripts/build_brew_rpm.sh'
+                        }
+                    }
+                }
             }
         }
 
         stage('Build Tower Brew Container Images') {
+            when {
+                expression {
+                    return params.BUILD_CONTAINER == 'yes'
+                }
+            }
+
             steps {
-                build(
-                    job: '../Brew/Build_Tower_Brew_Container_Image',
-                    parameters: [
-                        string(name: 'TOWER_PACKAGING_BRANCH', value: "origin/${params.TOWER_VERSION}"),
-                        booleanParam(name: 'TRIGGER', value: false)
-                    ]
-                )
+                withEnv(["KEYTAB_FILE=/home/jenkins/ansible-tower-build.keytab",
+                         "TOWER_BRANCH=${params.TOWER_BRANCH}"]) {
+                    sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
+                        sh './tools/jenkins/scripts/build_brew_container.sh'
+                    }
+                }
+                script {
+                    BREW_CONTAINER_IMAGE = readFile('ansible-tower/BREW_CONTAINER_IMAGE').trim()
+                }
+                archiveArtifacts artifacts: 'ansible-tower/BREW_CONTAINER_IMAGE'
             }
         }
 
         stage('Push Brew Container Images') {
-            steps {
-                build(
-                    job: '../Brew/Push_Brew_Container_Image',
-                    parameters: [
-                        string(name: 'TOWER_PACKAGING_BRANCH', value: "origin/${params.TOWER_VERSION}"),
-                        booleanParam(name: 'TRIGGER', value: false)
-                    ]
-                )
+            when {
+                expression {
+                    return params.PUSH_CONTAINER == 'yes'
+                }
             }
+
+            agent { label 'jenkins-jnlp-agent' }
+
+            steps {
+                withCredentials([string(credentialsId: 'awx_admin_password', variable: 'OPENSHIFT_PASSWORD')]) {
+                    withEnv(["OPENSHIFT_PASSWORD=${OPENSHIFT_PASSWORD}",
+                             "BREW_CONTAINER_IMAGE=${BREW_CONTAINER_IMAGE}"]) {
+                        sshagent(credentials : ['d2d4d16b-dc9a-461b-bceb-601f9515c98a']) {
+                            sh './tools/jenkins/scripts/push_brew_container.sh'
+                        }
+                    }
+                }
+                archiveArtifacts artifacts: 'VERSION'
+            }
+        }
+    }
+    post {
+        always {
+            cleanWs()
         }
     }
 }
