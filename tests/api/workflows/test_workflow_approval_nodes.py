25fbf23ade2ae94d8cbb0b8c1a099d49267ebafd
Apurva Bakshi <bakshiapurva93@gmail.com>
2019-08-08 15:03:48 -0400

    rbac tests

--- tests/api/workflows/test_workflow_approval_nodes.py
+++ tests/api/workflows/test_workflow_approval_nodes.py
@@ -1,5 +1,6 @@
 import pytest
 from awxkit.utils import poll_until
+import awxkit.exceptions
 
 from tests.api import APITest
 
@@ -26,6 +27,7 @@ class TestWorkflowApprovalNodes(APITest):
             user = factories.user(organization=org)
             with self.current_user(org_admin):
                 wfjt.set_object_roles(user, 'approve')
+
         timeout = 100
         description = 'Mark my words'
         name = 'hellow world'
@@ -55,7 +57,6 @@ class TestWorkflowApprovalNodes(APITest):
         poll_until(lambda: wf_approval.get().status == 'pending', interval=1, timeout=60)
         all_pending_approvals = v2.workflow_approvals.get(status='pending').results
         assert wf_approval.id in [approval.id for approval in all_pending_approvals]
-
         if approve:
             if user != 'sysadmin':
                 with self.current_user(user.username, user.password):
@@ -77,10 +78,50 @@ class TestWorkflowApprovalNodes(APITest):
 
         wf_job.wait_until_completed().assert_successful()
 
-    @pytest.mark.parametrize('approve', [True, False], ids=['approve', 'deny'])
-    @pytest.mark.parametrize('user', ['system_auditor', 'random_user', 'user_in_org'])
-    def test_users_without_role_cannot_approve(self, v2, factories, user, approve):
-        pytest.skip('Not Implemented')
+    @pytest.mark.parametrize('role', ['wf_executor', 'wf_read', 'org_executor', 'org_read', 'system_auditor', 'user_in_org', 'random_user'])
+    def test_users_without_role_cannot_approve(self, v2, factories, org_admin, role):
+        """Verify that Users with no permission cannot approve a node"""
+        org = org_admin.related.organizations.get().results.pop()
+        wfjt = factories.workflow_job_template(organization=org)
+
+        if role == 'wf_executor':
+            user = factories.user()
+            wfjt.set_object_roles(user, 'execute')
+        if role == 'wf_read':
+            user = factories.user()
+            wfjt.set_object_roles(user, 'read')
+        if role == 'org_executor':
+            user = factories.user()
+            org.set_object_roles(user, 'execute')
+        if role == 'org_read':
+            user = factories.user()
+            org.set_object_roles(user, 'read')
+        if role == 'system_auditor':
+            user = factories.user(is_system_auditor=True)
+        elif role == 'user_in_org':
+            user = factories.user(organization=org)
+        else:
+            random_org = factories.organization()
+            user = factories.user(organization=random_org)
+        timeout = 100
+        description = 'Mark my words'
+        name = 'hellow world'
+        approval_node = factories.workflow_job_template_node(
+            workflow_job_template=wfjt,
+            unified_job_template=None
+        ).make_approval_node(timeout=timeout, description=description, name=name)
+
+        wf_job = wfjt.launch()
+        wf_job.wait_until_status('running')
+        approval_jt = approval_node.related.unified_job_template.get()
+        approval_job_node = wf_job.related.workflow_nodes.get(unified_job_template=approval_jt.id).results.pop()
+        poll_until(lambda: hasattr(approval_job_node.get().related, 'job'), interval=1, timeout=60)
+        wf_approval = approval_job_node.related.job.get()
+        poll_until(lambda: wf_approval.get().status == 'pending', interval=1, timeout=60)
+
+        with self.current_user(user.username, user.password):
+            with pytest.raises(awxkit.exceptions.Forbidden):
+                wf_approval.approve()
 
     def test_update_existing_node_to_approval_node(self, v2, factories, org_admin):
         """Create a workflow with an approval node and approve it."""
@@ -129,15 +170,19 @@ class TestWorkflowApprovalNodes(APITest):
     def test_approval_node_timeout(self, v2, factories):
         """Create a workflow with an approval node and approve it."""
         wfjt = factories.workflow_job_template()
-        timeout = 1
         approval_node = factories.workflow_job_template_node(
             workflow_job_template=wfjt,
             unified_job_template=None
-            ).make_approval_node(timeout=timeout)
+            ).make_approval_node()
 
         # Confirm that approval JT created for us is what we expect
         approval_jt = approval_node.related.unified_job_template.get()
-        assert approval_jt.timeout == timeout
+        assert approval_jt.timeout == 0
+
+        approval_jt.timeout = 1
+        approval_jt = approval_node.related.unified_job_template.get()
+        assert approval_jt.timeout == 1
+
 
         wf_job = wfjt.launch()
         wf_job.wait_until_status('running')
@@ -149,56 +194,81 @@ class TestWorkflowApprovalNodes(APITest):
                                      f'Workflow Approval: {wf_approval}\n' \
                                      f'Approval JT: {approval_jt}\n' \
                                      f'Workflow Job: {wf_job.get()}\n'
+        with pytest.raises(awxkit.exceptions.BadRequest):
+            wf_approval.approve()
         all_failed_approvals = v2.workflow_approvals.get(status='failed').results
         assert wf_approval.id in [approval.id for approval in all_failed_approvals]
-
+        # also assert reason of failure and timeout variables in workflow approvals
         wf_job.wait_until_completed().assert_status('failed')
+        import pdb
+        pdb.set_trace()
 
-    def test_workflow_approval_visibility(self, v2, factories, org_admin):
-        """Create a workflow with an approval node and approve it."""
+
+    @pytest.mark.parametrize('role', ['sysadmin', 'org_admin', 'wf_admin', 'org_wf_admin', 'org_approve', 'wf_approve', 'org_executor', 'wf_executor', 'org_read', 'wf_read','system_auditor', 'user_in_org', 'random_user'])
+    def test_workflow_approval_visibility(self, v2, factories, org_admin, role):
+        """Verify that Users with no permission cannot view an approval"""
         org = org_admin.related.organizations.get().results.pop()
-        wf_approve_user = factories.user(organization=org)
-        org_approve_user = factories.user(organization=org)
-        org.set_object_roles(org_approve_user, 'approve')
-        underpriv_user = factories.user(organization=org)
         wfjt = factories.workflow_job_template(organization=org)
-        wfjt.set_object_roles(wf_approve_user, 'approve')
-        wfjt = factories.workflow_job_template()
+
+        if role == 'wf_executor':
+            user = factories.user()
+            wfjt.set_object_roles(user, 'execute')
+        if role == 'wf_read':
+            user = factories.user()
+            wfjt.set_object_roles(user, 'read')
+        if role == 'wf_approve':
+            user = factories.user()
+            wfjt.set_object_roles(user, 'approve')
+        if role == 'wf_admin':
+            user = factories.user()
+            wfjt.set_object_roles(user, 'admin')
+        if role == 'org_executor':
+            user = factories.user()
+            org.set_object_roles(user, 'execute')
+        if role == 'org_read':
+            user = factories.user()
+            org.set_object_roles(user, 'read')
+        if role == 'org_approve':
+            user = factories.user()
+            org.set_object_roles(user, 'approve')
+        if role == 'org_wf_admin':
+            user = factories.user()
+            org.set_object_roles(user, 'workflow admin')
+        if role == 'system_auditor':
+            user = factories.user(is_system_auditor=True)
+        if role == 'org_admin':
+            user = org_admin
+        elif role == 'user_in_org':
+            user = factories.user(organization=org)
+        elif role == 'random_user':
+            random_org = factories.organization()
+            user = factories.user(organization=random_org)
+        timeout = 100
+        description = 'Mark my words'
+        name = 'hellow world'
         approval_node = factories.workflow_job_template_node(
             workflow_job_template=wfjt,
             unified_job_template=None
-            ).make_approval_node()
-
-        # Confirm that approval JT created for us is what we expect
-        approval_jt = approval_node.related.unified_job_template.get()
+        ).make_approval_node(timeout=timeout, description=description, name=name)
 
         wf_job = wfjt.launch()
         wf_job.wait_until_status('running')
+        approval_jt = approval_node.related.unified_job_template.get()
         approval_job_node = wf_job.related.workflow_nodes.get(unified_job_template=approval_jt.id).results.pop()
         poll_until(lambda: hasattr(approval_job_node.get().related, 'job'), interval=1, timeout=60)
         wf_approval = approval_job_node.related.job.get()
-
-        # Can't hope for users to see it until the admin can, takes time to change state
         poll_until(lambda: wf_approval.get().status == 'pending', interval=1, timeout=60)
-        poll_until(lambda: v2.workflow_approvals.get(status='pending').count == 1, interval=1, timeout=60)
-
-        with self.current_user(wf_approve_user):
-            assert v2.workflow_approvals.get(status='pending').count == 1, 'Not seeing proper number of approvals'
-            approvals = v2.workflow_approvals.get(status='pending').results
-            assert wf_approval.id in [approval.id for approval in approvals]
-
-        with self.current_user(org_approve_user):
-            approvals = v2.workflow_approvals.get(status='pending').results
-            assert wf_approval.id in [approval.id for approval in approvals]
-
-        with self.current_user(org_approve_user):
-            approvals = v2.workflow_approvals.get(status='pending').results
-            assert wf_approval.id in [approval.id for approval in approvals]
-
-        with self.current_user(underpriv_user):
-            assert v2.workflow_approvals.get(status='pending').count == 0, 'Not seeing proper number of approvals'
-            approvals = v2.workflow_approvals.get(status='pending').results
-            assert wf_approval.id not in [approval.id for approval in approvals]
+        if role == 'user_in_org' or role == 'random_user':
+            with self.current_user(user.username, user.password):
+                all_pending_approvals = v2.workflow_approvals.get(status='pending').results
+                assert wf_approval.id not in [approval.id for approval in all_pending_approvals]
+        elif role == 'sysadmin':
+            all_pending_approvals = v2.workflow_approvals.get(status='pending').results
+            assert wf_approval.id in [approval.id for approval in all_pending_approvals]
+        else:
+            with self.current_user(user.username, user.password):
+                all_pending_approvals = v2.workflow_approvals.get(status='pending').results
+                assert wf_approval.id in [approval.id for approval in all_pending_approvals]
 
         wf_approval.approve()
-        wf_job.wait_until_completed().assert_status('success')
+        wf_job.wait_until_completed().assert_status('successful')
