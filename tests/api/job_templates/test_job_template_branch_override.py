6ee1bb240acd2e0d42db0a6b8f04623c70ff6a10
AlanCoding <arominge@redhat.com>
2019-08-15 12:54:05 -0400

    add test for scm_branch

--- tests/api/job_templates/test_job_template_branch_override.py
+++ tests/api/job_templates/test_job_template_branch_override.py
@@ -424,6 +424,43 @@ class TestJobTemplateBranchOverride(APITest):
         job = wf_job_node.related.job.get()
         assert job.scm_branch == prompt_branch
 
+    def test_workflow_job_template_promptable(self, factories):
+        project = factories.project(allow_override=True)
+        host = factories.host()
+        # run the JT so we make the project folder dirty with old data
+        jt_prompt = factories.job_template(
+            project=project,
+            playbook='debug.yml',
+            inventory=host.related.inventory.get(),
+            ask_scm_branch_on_launch=True
+            )
+
+        wfjt = factories.workflow_job_template(scm_branch='wfjt_branch', ask_scm_branch_on_launch=True)
+        assert wfjt.scm_branch == 'wfjt_branch'  # sanity
+        assert wfjt.ask_scm_branch_on_launch is True  # sanity
+
+        node = factories.workflow_job_template_node(
+            workflow_job_template=wfjt,
+            unified_job_template=jt_prompt,
+            scm_branch='wfjt_node_branch'
+        )
+        assert node.scm_branch == 'wfjt_node_branch'  # sanity
+
+        # launch and verify with just the WFJT value
+        wf_job = wfjt.launch().wait_until_completed()
+        wf_job.scm_branch == 'wfjt_branch'  # sanity
+        wf_job.assert_status('failed')  # Should fail because branch does not exist
+        wf_job_node = wf_job.related.workflow_nodes.get().results.pop()
+        job = wf_job_node.related.job.get()
+        assert job.scm_branch == 'wfjt_branch'
+
+        wf_job = wfjt.launch(payload=dict(scm_branch='wfjt_prompt_branch')).wait_until_completed()
+        wf_job.scm_branch == 'wfjt_prompt_branch'  # sanity
+        wf_job.assert_status('failed')  # Should fail because branch does not exist
+        wf_job_node = wf_job.related.workflow_nodes.get().results.pop()
+        job = wf_job_node.related.job.get()
+        assert job.scm_branch == 'wfjt_prompt_branch'
+
     @pytest.mark.parametrize('refspec, good_ref, bad_ref', [
         ('+refs/pull/62/head:refs/remotes/origin/pull/62/head', 'pull/62/head', 'pull/hotdog/catsup'),
         ('+refs/pull/62/head:pull/hotdog/catsup', 'pull/hotdog/catsup', 'pull/62/head'),
