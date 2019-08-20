810a574bcf28053451cca2f05c80b380ca3bc1fd
Elijah DeLee <kdelee@redhat.com>
2019-08-20 14:06:42 -0400

    Safer/lazier license application, whoever gets there first wins

--- tests/lib/license.py
+++ tests/lib/license.py
@@ -11,6 +11,13 @@ log = logging.getLogger(__name__)
 # The following two helpers poll until the license is effective.
 # Refer to https://github.com/ansible/tower/issues/2375
 def apply_license_until_effective(config, license_info):
+    # If we have an equivalent license, go ahead and return
+    config = config.get()
+    if config.license_info:
+        if 'license_type' in config.license_info and 'instance_count' in config.license_info:
+            if config.license_info.license_type == license_info['license_type'] and config.license_info.instance_count == license_info['instance_count']:
+                if not config.license_info.date_expired:
+                    return
     log.info('Applying {} license...'.format(license_info['license_type']))
     try:
         config.post(license_info)
@@ -20,7 +27,7 @@ def apply_license_until_effective(config, license_info):
             return
         else:
             raise
-    poll_until(lambda: config.get().license_info and config.license_info.license_key == license_info['license_key'],
+    poll_until(lambda: config.get().license_info and config.license_info.instance_count == license_info['instance_count'],
                interval=1, timeout=90)
 
 
