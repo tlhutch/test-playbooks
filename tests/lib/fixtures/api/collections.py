e4ac272afd4bfe1a85b75257f499510f880e1be1
Elyézer Rezende <elyezermr@gmail.com>
2019-08-26 23:09:11 -0400

    Update the collections fixture
    Proper handle cluster vs standalone deployments when setting up the
    collections pre-requisites. This will make more predictable where the
    things are run and therefore avoid tests being flake because the first
    host on the tower group is not the same as the one passed as the
    base-url.

--- tests/lib/fixtures/api/collections.py
+++ tests/lib/fixtures/api/collections.py
@@ -5,20 +5,20 @@ from awxkit import config
 import pytest
 
 
-def _run_install_tower_modules_collection(ansible_adhoc, docker=False):
-    result = ansible_adhoc()['tower'].shell('echo $HOME')
+def _run_install_tower_modules_collection(ansible_adhoc):
+    result = ansible_adhoc.shell('echo $HOME')
     user_home_dir = result.values()[0]['stdout_lines'][0]
     collection_install_path = os.path.join(user_home_dir, 'collections', 'ansible_collections', 'chrismeyersfsu', 'tower_modules')
-    ansible_adhoc()['tower'].shell('curl -L -o /tmp/collection.tar.gz https://galaxy.ansible.com/download/chrismeyersfsu-tower_modules-0.0.1.tar.gz')
-    ansible_adhoc()['tower'].shell('mkdir -p {}'.format(collection_install_path))
-    ansible_adhoc()['tower'].shell('tar -xf /tmp/collection.tar.gz -C {}'.format(collection_install_path))
+    ansible_adhoc.shell('curl -L -o /tmp/collection.tar.gz https://galaxy.ansible.com/download/chrismeyersfsu-tower_modules-0.0.1.tar.gz')
+    ansible_adhoc.shell('mkdir -p {}'.format(collection_install_path))
+    ansible_adhoc.shell('tar -xf /tmp/collection.tar.gz -C {}'.format(collection_install_path))
 
 
-def _run_uninstall_tower_modules_collection(ansible_adhoc, docker=False):
-    result = ansible_adhoc()['tower'].shell('echo $HOME')
+def _run_uninstall_tower_modules_collection(ansible_adhoc):
+    result = ansible_adhoc.shell('echo $HOME')
     user_home_dir = result.values()[0]['stdout_lines'][0]
     collection_install_path = os.path.join(user_home_dir, 'collections', 'ansible_collections', 'chrismeyersfsu', 'ansible_content_example')
-    ansible_adhoc()['tower'].shell('rm -rf {}'.format(collection_install_path))
+    ansible_adhoc.shell('rm -rf {}'.format(collection_install_path))
 
 
 @pytest.fixture(scope='class')
@@ -27,16 +27,25 @@ def tower_modules_collection(request, is_docker, session_ansible_adhoc):
     Emulate ansible 2.9 ansible-galaxy collection install. This fixture exists
     because the aforementioned command doesn't yet exist. Thus, our emulation.
     '''
-    if not config.prevent_teardown:
-        uninstall_tower_modules_collection_args = [
-            session_ansible_adhoc,
-            is_docker,
-        ]
-        uninstall_tower_modules_collection = functools.partial(_run_uninstall_tower_modules_collection, *uninstall_tower_modules_collection_args)
-        request.addfinalizer(uninstall_tower_modules_collection)
+    # On cluster deployments target the `cluster_installer` group which is the
+    # group that contains the host that the API calls will be made during
+    # testing. This is important, specially for the manual project test,
+    # because it needs that a local directory is present on the host's
+    # filesystem that is receiving the API call. On the other hand, for
+    # standalone deployments the target group should be `tower`
     try:
-        _run_install_tower_modules_collection(session_ansible_adhoc, is_docker)
+        ansible_adhoc = session_ansible_adhoc()['cluster_installer']
+    except KeyError:
+        ansible_adhoc = session_ansible_adhoc()['tower']
+
+    if not config.prevent_teardown:
+        uninstall_tower_modules_collection = functools.partial(
+            _run_uninstall_tower_modules_collection, ansible_adhoc)
+        request.addfinalizer(uninstall_tower_modules_collection)
+
+    try:
+        _run_install_tower_modules_collection(ansible_adhoc)
     except AssertionError as e:
         pytest.fail(
             'Failed to install chrismeyersfsu.ansible_content_example\n{}'.format(e)
-            )
+        )
