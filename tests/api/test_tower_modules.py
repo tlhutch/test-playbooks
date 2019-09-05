f73bd9e116fdfe65cc61756e1b22b11f6c850ba1
Alan Rominger <arominge@redhat.com>
2019-09-05 10:15:07 -0400

    Use Ansible default usr share directory for collection install (#4044)
    * Use Ansible default usr share directory for collection install
    
    * Clarify skip reason
    
    * fix permissions of chrismeyers collection after installing
    
    * Add test for fully qualified collection name
    
    * Add note about Ansible version

--- tests/api/test_tower_modules.py
+++ tests/api/test_tower_modules.py
@@ -41,16 +41,18 @@ The streams get crossed when the ansible virtualenv contains a different version
 than the version in the custom virtualenv. When a package is not found in the custom
 virtualenv, the ansible virtualenv is the fallback virtualenv. This is problematic when
 the python lib found doesn't support both versions of python.
+Second Note: Collections were only added in Ansible 2.9, and are rapidly changing
+with relevant fixes as of the time this was written, so the current version is used.
 '''
 CUSTOM_VENVS = [
     {
         'name': 'python2_tower_modules',
-        'packages': 'ansible-tower-cli psutil ansible',
+        'packages': 'ansible-tower-cli psutil git+https://github.com/ansible/ansible.git',
         'python_interpreter': 'python2'
     },
     {
         'name': 'python3_tower_modules',
-        'packages': 'ansible-tower-cli psutil ansible',
+        'packages': 'ansible-tower-cli psutil git+https://github.com/ansible/ansible.git',
         'python_interpreter': 'python36'
     },
 ]
@@ -75,13 +77,13 @@ def skip_if_wrong_python(request, os_python_version):
     """
     python_venv_name = request.getfixturevalue('python_venv_name')
     if not python_venv_name.startswith(f'python{os_python_version}'):
-        pytest.skip(f'OS Python version is {os_python_version} but')
+        pytest.skip(f'OS Python version is {os_python_version} which does not match venv')
 
 
 @pytest.fixture
-def tower_credential(factories, admin_user):
-    return factories.credential(kind='tower', username=admin_user.username,
-                                password=admin_user.password, host=config.base_url)
+def tower_credential(factories):
+    return factories.credential(kind='tower', username=config.credentials.default.username,
+                                password=config.credentials.default.password, host=config.base_url)
 
 
 @pytest.mark.fixture_args(venvs=CUSTOM_VENVS, cluster=True)
@@ -89,12 +91,11 @@ def tower_credential(factories, admin_user):
 @pytest.mark.parametrize('python_venv_name', CUSTOM_VENVS_NAMES)
 class Test_Ansible_Tower_Modules_via_Playbooks(APITest):
     @pytest.mark.parametrize('tower_module', TOWER_MODULES_PARAMS)
-    def test_ansible_tower_module(self, v2, factories, tower_module, project, tower_credential, venv_path, python_venv_name, is_cluster):
+    def test_ansible_tower_module(self, factories, tower_module, project, tower_credential, venv_path, python_venv_name, is_cluster):
         """
         Ansible modules that interact with Tower live in an Ansible Collection.
-        Along side those modules live playbooks that test the modules in the
-        collection.
-        This test invokes those test that live along side the collection.
+        This test invokes the integration tests that ran in Ansible core CI
+        before it was split out into a standalone collection.
         """
         if is_cluster and tower_module == 'project_manual':
             pytest.skip(
@@ -120,75 +121,72 @@ class Test_Ansible_Tower_Modules_via_Playbooks(APITest):
 @pytest.mark.usefixtures('skip_if_openshift', 'authtoken', 'tower_modules_collection', 'shared_custom_venvs')
 @pytest.mark.parametrize('python_venv_name', CUSTOM_VENVS_NAMES)
 class Test_Ansible_Tower_Modules(APITest):
-    def test_ansible_tower_module_organization_create(self, request, v2, factories, project, tower_credential, venv_path, python_venv_name):
-        org_name = utils.random_title()
-        virtual_env_path = venv_path(python_venv_name)
-
+    def run_tower_module(self, module_name, module_args, factories, virtual_env_path=None, more_vars=None):
         extra_vars = {
-            'module_name': 'tower_organization',
-            'module_args': {
-                'name': org_name,
-                'description': 'hello world',
-            },
-            'ansible_python_interpreter': os.path.join(virtual_env_path, 'bin/python'),
+            'module_name': module_name,
+            'module_args': module_args,
         }
-        request.addfinalizer(lambda *args: v2.organizations.get(name=org_name).results[0].delete())
+        if virtual_env_path:
+            extra_vars['ansible_python_interpreter'] = os.path.join(virtual_env_path, 'bin/python')
+        if more_vars:
+            extra_vars.update(more_vars)
 
-        jt = factories.job_template(playbook='invoke_x.yml', extra_vars=json.dumps(extra_vars),
-                                    credential=tower_credential, project=project, verbosity=5)
-        jt.custom_virtualenv = virtual_env_path
+        jt = factories.job_template(
+            playbook='invoke_x.yml', extra_vars=json.dumps(extra_vars), verbosity=5
+        )
+        jt.add_credential(factories.credential(
+            kind='tower',
+            username=config.credentials.default.username,
+            password=config.credentials.default.password,
+            host=config.base_url
+        ))
+        if virtual_env_path:
+            jt.custom_virtualenv = virtual_env_path
         job = jt.launch()
         job.wait_until_completed()
         job.assert_successful()
+        return job
+
+    def test_ansible_tower_fully_qualified_collection_name(self, factories, venv_path, python_venv_name):
+        org = factories.organization()
+        # TODO: update module name to "awx.awx.tower_organization" when modules are fully migrated
+        # or maybe "awx.awx.organization", still not fully decided
+        # no assertion here because only testing that job was successful
+        self.run_tower_module(
+            'chrismeyersfsu.tower_modules.tower_organization', {'name': org.name},
+            factories, venv_path(python_venv_name)
+        )
+
+    def test_ansible_tower_module_organization_create(self, request, v2, factories, venv_path, python_venv_name):
+        org_name = utils.random_title()
+        request.addfinalizer(lambda *args: v2.organizations.get(name=org_name).results[0].delete())
+        self.run_tower_module('tower_organization', {
+            'name': org_name,
+            'description': 'hello world',
+        }, factories, venv_path(python_venv_name))
 
         org = v2.organizations.get(name=org_name).results[0]
         assert org_name == org['name']
-        assert extra_vars['module_args']['description'] == org['description']
+        assert org['description'] == 'hello world'
 
-    def test_ansible_tower_module_organization_delete(self, request, v2, factories, project, tower_credential, venv_path, python_venv_name):
+    def test_ansible_tower_module_organization_delete(self, factories, v2, venv_path, python_venv_name):
         org = factories.organization()
-        virtual_env_path = venv_path(python_venv_name)
-
-        extra_vars = {
-            'module_name': 'tower_organization',
-            'module_args': {
-                'name': org.name,
-                'state': 'absent',
-            },
-            'ansible_python_interpreter': os.path.join(virtual_env_path, 'bin/python'),
-        }
-
-        jt = factories.job_template(playbook='invoke_x.yml', extra_vars=json.dumps(extra_vars),
-                                    credential=tower_credential, project=project, verbosity=5)
-        jt.custom_virtualenv = virtual_env_path
-        job = jt.launch()
-        job.wait_until_completed()
-        job.assert_successful()
+        self.run_tower_module('tower_organization', {
+            'name': org.name,
+            'state': 'absent',
+        }, factories, venv_path(python_venv_name))
 
         assert 0 == len(v2.organizations.get(name=org.name).results)
 
-    def test_ansible_tower_module_organization_check_mode(self, request, v2, factories, project, tower_credential, tower_version, venv_path, python_venv_name):
+    def test_ansible_tower_module_organization_check_mode(self, factories, tower_version, venv_path, python_venv_name):
         '''
         Ensure that orgnization check_mode: True returns the tower_version
         '''
         org = factories.organization()
-        virtual_env_path = venv_path(python_venv_name)
-
-        extra_vars = {
-            'module_name': 'tower_organization',
-            'module_args': {
-                'name': org.name,
-            },
-            'check_mode': True,
-            'ansible_python_interpreter': os.path.join(virtual_env_path, 'bin/python'),
-        }
-
-        jt = factories.job_template(playbook='invoke_x.yml', extra_vars=json.dumps(extra_vars),
-                                    credential=tower_credential, project=project, verbosity=5)
-        jt.custom_virtualenv = virtual_env_path
-        job = jt.launch()
-        job.wait_until_completed()
-        job.assert_successful()
+        job = self.run_tower_module(
+            'tower_organization', {'name': org.name}, factories, venv_path(python_venv_name),
+            more_vars={'check_mode': True}
+        )
 
         job_event = job.get_related('job_events').get(event='runner_on_ok', task='invoke_arbitrary_module').results[0]
         module_tower_version = job_event['event_data']['res']['tower_version']
--- tests/lib/fixtures/api/collections.py
+++ tests/lib/fixtures/api/collections.py
@@ -1,23 +1,17 @@
 import functools
 import os
+import stat
 
 from awxkit import config
 import pytest
 
 
-def _run_install_tower_modules_collection(ansible_adhoc):
-    result = ansible_adhoc.shell('echo $HOME')
-    user_home_dir = result.values()[0]['stdout_lines'][0]
-    collection_install_path = os.path.join(user_home_dir, 'collections', 'ansible_collections', 'chrismeyersfsu', 'tower_modules')
-    ansible_adhoc.shell('curl -L -o /tmp/collection.tar.gz https://galaxy.ansible.com/download/chrismeyersfsu-tower_modules-0.0.1.tar.gz')
-    ansible_adhoc.shell('mkdir -p {}'.format(collection_install_path))
-    ansible_adhoc.shell('tar -xf /tmp/collection.tar.gz -C {}'.format(collection_install_path))
+# https://github.com/ansible/ansible/blob/faaa669764faba8f2a1b4292afc3bc494e4a1932/lib/ansible/config/base.yml#L221
+COLLECTIONS_SHARE_FOLDER = '/usr/share/ansible/collections'
 
 
 def _run_uninstall_tower_modules_collection(ansible_adhoc):
-    result = ansible_adhoc.shell('echo $HOME')
-    user_home_dir = result.values()[0]['stdout_lines'][0]
-    collection_install_path = os.path.join(user_home_dir, 'collections', 'ansible_collections', 'chrismeyersfsu', 'ansible_content_example')
+    collection_install_path = os.path.join(COLLECTIONS_SHARE_FOLDER, 'ansible_collections', 'chrismeyersfsu')
     ansible_adhoc.shell('rm -rf {}'.format(collection_install_path))
 
 
@@ -44,7 +38,20 @@ def tower_modules_collection(request, is_docker, session_ansible_adhoc):
         request.addfinalizer(uninstall_tower_modules_collection)
 
     try:
-        _run_install_tower_modules_collection(ansible_adhoc)
+        contacted = ansible_adhoc.shell(
+            'ansible-galaxy collection install chrismeyersfsu.tower_modules -p {}'.format(
+                COLLECTIONS_SHARE_FOLDER
+            )
+        )
+        for result in contacted.values():
+            assert result.get('rc', None) == 0, result
+        contacted = ansible_adhoc.file(
+            dest=COLLECTIONS_SHARE_FOLDER,
+            mode=stat.S_IRUSR | stat.S_IXUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH,
+            recurse=True
+        )
+        for result in contacted.values():
+            assert result.get('mode', None) == '0755', result
     except AssertionError as e:
         pytest.fail(
             'Failed to install chrismeyersfsu.ansible_content_example\n{}'.format(e)
