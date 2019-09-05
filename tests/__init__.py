a1006fac7ea2a7c574e3478207e75ad70fe7bf1e
Caleb Boylan <calebboylan@gmail.com>
2019-09-05 10:39:26 -0700

    Remove urllib3 InsecureRequestWarning warnings

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,7 @@
 import logging
+import urllib3
 
 logging.basicConfig(format="%(asctime)s [%(process)d][%(name)-4s:%(lineno)-2d][%(levelname)-4s] %(message)s")
 
 logging.getLogger('factory').setLevel('WARN')
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
