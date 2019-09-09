06a49eaaadf3119f828021802a71638530d70108
Ryan Petrello <rpetrell@redhat.com>
2019-09-09 12:33:23 -0400

    update the association tests to use run using primary keys *and* names
    see: https://github.com/ansible/awx/pull/4653

--- tests/cli/test_custom_commands/test_association.py
+++ tests/cli/test_custom_commands/test_association.py
@@ -22,19 +22,20 @@ class TestCredentialAssociation(object):
         assert result.returncode == 1
         assert result.json['error'] == 'Cannot assign multiple Machine credentials.'
 
-    def test_jt_credential(self, cli, job_template_ping, network_credential_with_ssh_key_data):
+    @pytest.mark.parametrize('by', ('id', 'name'))
+    def test_jt_credential(self, cli, job_template_ping, network_credential_with_ssh_key_data, by):
         assert job_template_ping.related.credentials.get().count == 1
 
         result = cli([
-            'awx', 'job_templates', 'associate', str(job_template_ping.id),
-            '--credential', str(network_credential_with_ssh_key_data.id)
+            'awx', 'job_templates', 'associate', str(getattr(job_template_ping, by)),
+            '--credential', str(getattr(network_credential_with_ssh_key_data, by)),
         ], auth=True)
         assert result.json['count'] == 2
         assert job_template_ping.related.credentials.get().count == 2
 
         result = cli([
-            'awx', 'job_templates', 'disassociate', str(job_template_ping.id),
-            '--credential', str(network_credential_with_ssh_key_data.id)
+            'awx', 'job_templates', 'disassociate', str(getattr(job_template_ping, by)),
+            '--credential', str(getattr(network_credential_with_ssh_key_data, by)),
         ], auth=True)
         assert result.json['count'] == 1
         assert job_template_ping.related.credentials.get().count == 1
@@ -48,14 +49,15 @@ class TestNotificationAssociation(object):
         ['success_notification', 'notification_templates_success'],
         ['failure_notification', 'notification_templates_error'],
     ])
+    @pytest.mark.parametrize('by', ('id', 'name'))
     def test_jt_notification(self, cli, job_template_ping, webhook_notification_template,
-                             param, related_endpoint):
+                             param, related_endpoint, by):
         endpoint = getattr(job_template_ping.related, related_endpoint)
         assert endpoint.get().count == 0
         for action, count in [['associate', 1], ['disassociate', 0]]:
             cli([
-                'awx', 'job_templates', action, str(job_template_ping.id),
-                '--{}'.format(param), str(webhook_notification_template.id)
+                'awx', 'job_templates', action, str(getattr(job_template_ping, by)),
+                '--{}'.format(param), str(getattr(webhook_notification_template, by))
             ], auth=True)
             assert endpoint.get().count == count
 
@@ -64,15 +66,16 @@ class TestNotificationAssociation(object):
         ['success_notification', 'notification_templates_success'],
         ['failure_notification', 'notification_templates_error'],
     ])
+    @pytest.mark.parametrize('by', ('id', 'name'))
     def test_project_notification(self, cli, webhook_notification_template,
                              project_ansible_playbooks_git_nowait,
-                             param, related_endpoint):
+                             param, related_endpoint, by):
         endpoint = getattr(project_ansible_playbooks_git_nowait.related, related_endpoint)
         assert endpoint.get().count == 0
         for action, count in [['associate', 1], ['disassociate', 0]]:
             cli([
-                'awx', 'projects', action, str(project_ansible_playbooks_git_nowait.id),
-                '--{}'.format(param), str(webhook_notification_template.id)
+                'awx', 'projects', action, str(getattr(project_ansible_playbooks_git_nowait, by)),
+                '--{}'.format(param), str(getattr(webhook_notification_template, by)),
             ], auth=True)
             assert endpoint.get().count == count
 
@@ -81,14 +84,16 @@ class TestNotificationAssociation(object):
         ['success_notification', 'notification_templates_success'],
         ['failure_notification', 'notification_templates_error'],
     ])
+    @pytest.mark.parametrize('by', ('id', 'name'))
     def test_inventory_notification(self, cli, webhook_notification_template,
-                                    inventory_source, param, related_endpoint):
+                                    inventory_source, param, related_endpoint,
+                                    by):
         endpoint = getattr(inventory_source.related, related_endpoint)
         assert endpoint.get().count == 0
         for action, count in [['associate', 1], ['disassociate', 0]]:
             cli([
-                'awx', 'inventory_sources', action, str(inventory_source.id),
-                '--{}'.format(param), str(webhook_notification_template.id)
+                'awx', 'inventory_sources', action, str(getattr(inventory_source, by)),
+                '--{}'.format(param), str(getattr(webhook_notification_template, by))
             ], auth=True)
             assert endpoint.get().count == count
 
@@ -97,14 +102,15 @@ class TestNotificationAssociation(object):
         ['success_notification', 'notification_templates_success'],
         ['failure_notification', 'notification_templates_error'],
     ])
+    @pytest.mark.parametrize('by', ('id', 'name'))
     def test_org_notification(self, cli, webhook_notification_template,
-                              organization, param, related_endpoint):
+                              organization, param, related_endpoint, by):
         endpoint = getattr(organization.related, related_endpoint)
         assert endpoint.get().count == 0
         for action, count in [['associate', 1], ['disassociate', 0]]:
             cli([
-                'awx', 'organizations', action, str(organization.id),
-                '--{}'.format(param), str(webhook_notification_template.id)
+                'awx', 'organizations', action, str(getattr(organization, by)),
+                '--{}'.format(param), str(getattr(webhook_notification_template, by)),
             ], auth=True)
             assert endpoint.get().count == count
 
@@ -113,13 +119,15 @@ class TestNotificationAssociation(object):
         ['success_notification', 'notification_templates_success'],
         ['failure_notification', 'notification_templates_error'],
     ])
+    @pytest.mark.parametrize('by', ('id', 'name'))
     def test_wfjt_notification(self, cli, webhook_notification_template,
-                               workflow_job_template, param, related_endpoint):
+                               workflow_job_template, param, related_endpoint,
+                               by):
         endpoint = getattr(workflow_job_template.related, related_endpoint)
         assert endpoint.get().count == 0
         for action, count in [['associate', 1], ['disassociate', 0]]:
             cli([
-                'awx', 'workflow_job_templates', action, str(workflow_job_template.id),
-                '--{}'.format(param), str(webhook_notification_template.id)
+                'awx', 'workflow_job_templates', action, str(getattr(workflow_job_template, by)),
+                '--{}'.format(param), str(getattr(webhook_notification_template, by)),
             ], auth=True)
             assert endpoint.get().count == count
