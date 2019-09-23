8c91349549ef6c57e46487b530d7ae12a5913341
Mat Wilson <mawilson@redhat.com>
2019-09-23 13:59:57 -0700

    update gathering function to return true/false status, and add test for scenario in which the perf stats feature is not enabled

--- tests/api/external_data/test_resource_profiling.py
+++ tests/api/external_data/test_resource_profiling.py
@@ -18,10 +18,17 @@ class TestResourceProfiling(APITest):
     # Attempt to simplify locating files in a cluster
     def find_resource_profile(self, ansible_runner, job_id):
         result = ansible_runner.find(paths='/var/log/tower/playbook_profiling/{}'.format(job_id), recurse=True)
+        import pdb; pdb.set_trace()
         execution_node_result = [r for r in result.values() if r['matched'] > 0]
-        if len(execution_node_result) != 1:
-            raise Exception("execution node could not be determined")
-        return execution_node_result[0]
+        if len(execution_node_result) > 1:
+            raise Exception("Multiple nodes contain profiles for this job")
+        elif len(execution_node_result) == 1:
+            profile_found = True
+            profile_result = execution_node_result[0]
+        elif len(execution_node_result) == 0:
+            profile_found = False
+            profile_result = None
+        return profile_found, profile_result
 
     def test_performance_stats_files_created(self, ansible_runner, skip_if_openshift, resource_profiling_enabled, factories):
         expected_files = ['memory',
@@ -30,8 +37,16 @@ class TestResourceProfiling(APITest):
         jt = factories.job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 2}')
         factories.host(inventory=jt.ds.inventory)
         job = jt.launch().wait_until_completed()
-        result = self.find_resource_profile(ansible_runner, job.id)
-        stat_files = result['files']
+        profile_found, profile_result = self.find_resource_profile(ansible_runner, job.id)
+        assert profile_found == True, profile_result
+        stat_files = profile_result['files']
         filenames = [f['path'] for f in stat_files]
         for e in expected_files:
             assert any(e.lower() in f.lower() for f in filenames), filenames
+
+    def test_performance_stats_not_created_when_disabled(self, ansible_runner, skip_if_openshift, factories):
+        jt = factories.job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 2}')
+        factories.host(inventory=jt.ds.inventory)
+        job = jt.launch().wait_until_completed()
+        profile_found, profile_result = self.find_resource_profile(ansible_runner, job.id)
+        assert not profile_found, profile_result
