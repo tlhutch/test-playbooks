3b38ba147853661c1a39ccb91d7038b24014c89d
Mat Wilson <mawilson@redhat.com>
2019-09-24 11:53:38 -0700

    add test for intervals

--- tests/api/external_data/test_resource_profiling.py
+++ tests/api/external_data/test_resource_profiling.py
@@ -18,7 +18,11 @@ class TestResourceProfiling(APITest):
     # Attempt to simplify locating files in a cluster
     def find_resource_profile(self, ansible_runner, job_id):
         result = ansible_runner.find(paths='/var/log/tower/playbook_profiling/{}'.format(job_id), recurse=True)
-        execution_node_result = [r for r in result.values() if r['matched'] > 0]
+        execution_node_result = []
+        # Using this because we need to grab the hostname from the keys based on index
+        for h in range(len(result.values())):
+            if result.values()[h]['matched'] > 0:
+                execution_node_result.append((result.values()[h], list(result.keys())[h]))
         if len(execution_node_result) > 1:
             raise Exception("Multiple nodes contain profiles for this job")
         elif len(execution_node_result) == 1:
@@ -38,7 +42,7 @@ class TestResourceProfiling(APITest):
         job = jt.launch().wait_until_completed()
         profile_found, profile_result = self.find_resource_profile(ansible_runner, job.id)
         assert profile_found, profile_result
-        stat_files = profile_result['files']
+        stat_files = profile_result[0]['files']
         filenames = [f['path'] for f in stat_files]
         for e in expected_files:
             assert any(e.lower() in f.lower() for f in filenames), filenames
@@ -49,3 +53,20 @@ class TestResourceProfiling(APITest):
         job = jt.launch().wait_until_completed()
         profile_found, profile_result = self.find_resource_profile(ansible_runner, job.id)
         assert not profile_found, profile_result
+
+    def test_performance_stats_intervals_are_applied(self, ansible_runner, ansible_adhoc, update_setting_pg, v2, factories, skip_if_openshift, resource_profiling_enabled):
+        system_settings = v2.settings.get().get_endpoint('jobs')
+        payload = {'AWX_RESOURCE_PROFILING_CPU_POLL_INTERVAL': '0.5',
+                   'AWX_RESOURCE_PROFILING_MEMORY_POLL_INTERVAL': '0.5',
+                   'AWX_RESOURCE_PROFILING_PID_POLL_INTERVAL': '0.5'}
+        update_setting_pg(system_settings, payload)
+        jt = factories.job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 10}')
+        factories.host(inventory=jt.ds.inventory)
+        job = jt.launch().wait_until_completed()
+        profile_found, profile_result = self.find_resource_profile(ansible_runner, job.id)
+        assert profile_found
+        stat_files = profile_result[0]['files']
+        hosts = ansible_adhoc()
+        for f in stat_files:
+            file_wc = hosts[profile_result[1]].command('wc {}'.format(f['path'])).values()[0]['stdout'].split()[0]
+            assert 19 < int(file_wc) < 25, file_wc
