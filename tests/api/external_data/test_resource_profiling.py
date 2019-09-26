5b22b0540ffaaafd8f35b07fdec0e851f063e45c
Mat Wilson <mawilson@redhat.com>
2019-09-26 13:34:26 -0700

    perf stats- major refactor, add iso node test

--- tests/api/external_data/test_resource_profiling.py
+++ tests/api/external_data/test_resource_profiling.py
@@ -5,6 +5,10 @@ import pytest
 @pytest.mark.usefixtures('authtoken')
 class TestResourceProfiling(APITest):
 
+    expected_files = ['memory',
+                      'cpu',
+                      'pids']
+
     def toggle_resource_profiling(self, update_setting_pg, v2, state="false"):
         system_settings = v2.settings.get().get_endpoint('jobs')
         payload = {'AWX_RESOURCE_PROFILING_ENABLED': state}
@@ -15,46 +19,40 @@ class TestResourceProfiling(APITest):
         self.toggle_resource_profiling(update_setting_pg, v2, state="true")
         request.addfinalizer(lambda: self.toggle_resource_profiling(update_setting_pg, v2, state="false"))
 
-    # Attempt to simplify locating files in a cluster
-    def find_resource_profile(self, ansible_runner, job_id):
-        result = ansible_runner.find(paths='/var/log/tower/playbook_profiling/{}'.format(job_id), recurse=True)
-        execution_node_result = []
-        # Using this because we need to grab the hostname from the keys based on index
-        for h in range(len(result.values())):
-            if result.values()[h]['matched'] > 0:
-                execution_node_result.append((result.values()[h], list(result.keys())[h]))
-        if len(execution_node_result) > 1:
-            raise Exception("Multiple nodes contain profiles for this job")
-        elif len(execution_node_result) == 1:
-            profile_found = True
-            profile_result = execution_node_result[0]
-        elif len(execution_node_result) == 0:
-            profile_found = False
-            profile_result = None
-        return profile_found, profile_result
+    def get_resource_profiles(self, ansible_adhoc, job_id, execution_node):
+        hosts = ansible_adhoc()
+        profile_paths = hosts[execution_node].find(paths='/var/log/tower/playbook_profiling/{}'.format(job_id), recurse=True).values()[0]['files']
+        filenames = [f['path'] for f in profile_paths]
+        return filenames
 
-    def test_performance_stats_files_created(self, ansible_runner, skip_if_openshift, resource_profiling_enabled, factories):
-        expected_files = ['memory',
-                          'cpu',
-                          'pids']
+    def count_lines_in_files(self, ansible_adhoc, filenames, execution_node):
+        hosts = ansible_adhoc()
+        linecounts = {}
+        for f in filenames:
+            file_lc = hosts[execution_node].command('wc {}'.format(f)).values()[0]['stdout'].split()[0]
+            linecounts[f] = int(file_lc)
+        return linecounts
+
+    def check_filenames(self, filenames):
+        for e in self.expected_files:
+            assert any(e.lower() in f.lower() for f in filenames), '{} not found'.format(e)
+
+    def test_performance_stats_files_created(self, ansible_adhoc, skip_if_openshift, resource_profiling_enabled, factories):
         jt = factories.job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 2}')
         factories.host(inventory=jt.ds.inventory)
         job = jt.launch().wait_until_completed()
-        profile_found, profile_result = self.find_resource_profile(ansible_runner, job.id)
-        assert profile_found, profile_result
-        stat_files = profile_result[0]['files']
-        filenames = [f['path'] for f in stat_files]
-        for e in expected_files:
-            assert any(e.lower() in f.lower() for f in filenames), filenames
+        profiles = self.get_resource_profiles(ansible_adhoc, job.id, job.execution_node)
+        assert len(profiles) == len(self.expected_files)
+        self.check_filenames(profiles)
 
-    def test_performance_stats_not_created_when_disabled(self, ansible_runner, skip_if_openshift, factories):
+    def test_performance_stats_not_created_when_disabled(self, ansible_adhoc, skip_if_openshift, factories):
         jt = factories.job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 2}')
         factories.host(inventory=jt.ds.inventory)
         job = jt.launch().wait_until_completed()
-        profile_found, profile_result = self.find_resource_profile(ansible_runner, job.id)
-        assert not profile_found, profile_result
+        profiles = self.get_resource_profiles(ansible_adhoc, job.id, job.execution_node)
+        assert len(profiles) == 0
 
-    def test_performance_stats_intervals_are_applied(self, ansible_runner, ansible_adhoc, update_setting_pg, v2, factories, skip_if_openshift, resource_profiling_enabled):
+    def test_performance_stats_intervals_are_applied(self, ansible_adhoc, update_setting_pg, v2, factories, skip_if_openshift, resource_profiling_enabled):
         system_settings = v2.settings.get().get_endpoint('jobs')
         payload = {'AWX_RESOURCE_PROFILING_CPU_POLL_INTERVAL': '0.5',
                    'AWX_RESOURCE_PROFILING_MEMORY_POLL_INTERVAL': '0.5',
@@ -63,10 +61,18 @@ class TestResourceProfiling(APITest):
         jt = factories.job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 10}')
         factories.host(inventory=jt.ds.inventory)
         job = jt.launch().wait_until_completed()
-        profile_found, profile_result = self.find_resource_profile(ansible_runner, job.id)
-        assert profile_found
-        stat_files = profile_result[0]['files']
-        hosts = ansible_adhoc()
-        for f in stat_files:
-            file_wc = hosts[profile_result[1]].command('wc {}'.format(f['path'])).values()[0]['stdout'].split()[0]
-            assert 19 < int(file_wc) < 25, file_wc
+        profiles = self.get_resource_profiles(ansible_adhoc, job.id, job.execution_node)
+        self.check_filenames(profiles)
+        linecounts = self.count_lines_in_files(ansible_adhoc, profiles, job.execution_node)
+        for f in profiles:
+            assert 19 < linecounts[f] < 25, linecounts[f]
+
+    def test_performance_stats_generated_on_isolated_nodes(self, ansible_adhoc, update_setting_pg, v2, factories, skip_if_openshift, skip_if_not_cluster, resource_profiling_enabled):
+        ig = v2.instance_groups.get(name='protected').results.pop()
+        jt = factories.job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 2}')
+        jt.add_instance_group(ig)
+        factories.host(inventory=jt.ds.inventory)
+        job = jt.launch().wait_until_completed()
+        profiles = self.get_resource_profiles(ansible_adhoc, job.id, job.execution_node)
+        assert len(profiles) == len(self.expected_files)
+        self.check_filenames(profiles)
