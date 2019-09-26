c27440d5dd54f11804c527996ca67e1879f12ff2
Mat Wilson <mawilson@redhat.com>
2019-09-26 16:24:10 -0700

    add verbosity test for resource profiling

--- tests/api/external_data/test_resource_profiling.py
+++ tests/api/external_data/test_resource_profiling.py
@@ -2,6 +2,7 @@ from tests.api import APITest
 import pytest
 
 
+@pytest.mark.serial
 @pytest.mark.usefixtures('authtoken')
 class TestResourceProfiling(APITest):
 
@@ -15,9 +16,8 @@ class TestResourceProfiling(APITest):
         update_setting_pg(system_settings, payload)
 
     @pytest.fixture
-    def resource_profiling_enabled(self, update_setting_pg, v2, request):
+    def global_resource_profiling_enabled(self, update_setting_pg, v2, request):
         self.toggle_resource_profiling(update_setting_pg, v2, state="true")
-        request.addfinalizer(lambda: self.toggle_resource_profiling(update_setting_pg, v2, state="false"))
 
     def get_resource_profiles(self, ansible_adhoc, job_id, execution_node):
         hosts = ansible_adhoc()
@@ -37,8 +37,16 @@ class TestResourceProfiling(APITest):
         for e in self.expected_files:
             assert any(e.lower() in f.lower() for f in filenames), '{} not found'.format(e)
 
-    def test_performance_stats_files_created(self, ansible_adhoc, skip_if_openshift, resource_profiling_enabled, factories):
+    def test_performance_stats_files_created(self, ansible_adhoc, skip_if_openshift, global_resource_profiling_enabled, factories):
         jt = factories.job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 2}')
+        factories.host(inventory=jt.ds.inventory)
+        job = jt.launch().wait_until_completed()
+        profiles = self.get_resource_profiles(ansible_adhoc, job.id, job.execution_node)
+        assert len(profiles) == len(self.expected_files)
+        self.check_filenames(profiles)
+
+    def test_performance_stats_enabled_by_verbosity_5(self, ansible_adhoc, skip_if_openshift, factories):
+        jt = factories.job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 2}', verbosity=5)
         factories.host(inventory=jt.ds.inventory)
         job = jt.launch().wait_until_completed()
         profiles = self.get_resource_profiles(ansible_adhoc, job.id, job.execution_node)
@@ -52,7 +60,7 @@ class TestResourceProfiling(APITest):
         profiles = self.get_resource_profiles(ansible_adhoc, job.id, job.execution_node)
         assert len(profiles) == 0
 
-    def test_performance_stats_intervals_are_applied(self, ansible_adhoc, update_setting_pg, v2, factories, skip_if_openshift, resource_profiling_enabled):
+    def test_performance_stats_intervals_are_applied(self, ansible_adhoc, update_setting_pg, v2, factories, skip_if_openshift, global_resource_profiling_enabled):
         system_settings = v2.settings.get().get_endpoint('jobs')
         payload = {'AWX_RESOURCE_PROFILING_CPU_POLL_INTERVAL': '0.5',
                    'AWX_RESOURCE_PROFILING_MEMORY_POLL_INTERVAL': '0.5',
@@ -67,7 +75,7 @@ class TestResourceProfiling(APITest):
         for f in profiles:
             assert 19 < linecounts[f] < 25, linecounts[f]
 
-    def test_performance_stats_generated_on_isolated_nodes(self, ansible_adhoc, update_setting_pg, v2, factories, skip_if_openshift, skip_if_not_cluster, resource_profiling_enabled):
+    def test_performance_stats_generated_on_isolated_nodes(self, ansible_adhoc, update_setting_pg, v2, factories, skip_if_openshift, skip_if_not_cluster, global_resource_profiling_enabled):
         ig = v2.instance_groups.get(name='protected').results.pop()
         jt = factories.job_template(playbook='sleep.yml', extra_vars='{"sleep_interval": 2}')
         jt.add_instance_group(ig)
