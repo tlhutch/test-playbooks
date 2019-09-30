2e07d735ae9d5add7c620bc3d79f6cba92fd1ff7
Elijah DeLee <kdelee@redhat.com>
2019-09-30 16:35:15 -0400

    adjust pod_spec changes now that default is json

--- tests/api/cluster/test_container_groups.py
+++ tests/api/cluster/test_container_groups.py
@@ -18,17 +18,25 @@ class TestContainerGroups(APITest):
         client.setup_container_group_namespace(namespace=namespace)
         request.addfinalizer(client.destroy_container_group_namespace)
         cred_type = v2_class.credential_types.get(namespace='kubernetes_bearer_token').results.pop()
-        cred = class_factories.credential(credential_type=cred_type, inputs={'host': client.client.configuration.host, 'verify_ssl': True, 'ssl_ca_cert': client.cacrt, 'bearer_token': client.serviceaccount_token})
+        cred = class_factories.credential(
+            credential_type=cred_type,
+            inputs={
+                'host': client.client.configuration.host,
+                'verify_ssl': True,
+                'ssl_ca_cert': client.cacrt,
+                'bearer_token': client.serviceaccount_token
+                }
+            )
         ig = class_factories.instance_group(name=f'ContainerGroup - {namespace}')
         ig.credential = cred.id
         ig_options = v2_class.instance_groups.options()
-        pod_spec = yaml.load(ig_options.actions['POST']['pod_spec_override']['default'], Loader=yaml.SafeLoader)
+        pod_spec = dict(ig_options.actions['POST']['pod_spec_override']['default'])
         pod_spec['metadata']['namespace'] = namespace
         ig.pod_spec_override = json.dumps(pod_spec)
 
         return ig, client
 
-    @pytest.fixture(scope='function', params=['cacrt', 'token', 'host', 'image', 'role'])
+    @pytest.fixture(scope='function', params=['cacrt', 'token', 'host', 'image', 'role', 'entry_point'])
     def bad_container_group_and_client(self, request, gke_client_fscope, factories, v2):
         problem = request.param
         if problem == 'role':
@@ -44,14 +52,18 @@ class TestContainerGroups(APITest):
         cacrt = 'blah' if problem == 'cacrt' else client.cacrt
         host = 'broken.example.com' if problem == 'host' else client.client.configuration.host
         token = '230420934823broken928340983240' if problem == 'token' else client.serviceaccount_token
-        cred = factories.credential(credential_type=cred_type, inputs={'host': host, 'verify_ssl': True, 'ssl_ca_cert': cacrt, 'bearer_token': token})
+        cred = factories.credential(credential_type=cred_type, inputs={
+            'host': host, 'verify_ssl': True, 'ssl_ca_cert': cacrt, 'bearer_token': token}
+            )
         ig = factories.instance_group(name=f'ContainerGroup - {namespace}')
         ig.credential = cred.id
         ig_options = v2.instance_groups.options()
-        pod_spec = yaml.load(ig_options.actions['POST']['pod_spec_override']['default'], Loader=yaml.SafeLoader)
+        pod_spec = dict(ig_options.actions['POST']['pod_spec_override']['default'])
         pod_spec['metadata']['namespace'] = namespace
         if problem == 'image':
             pod_spec['spec']['containers'][0]['image'] = 'nginx'
+        if problem == 'entry_point':
+            pod_spec['spec']['containers'][0]['args'] = ['sleep', '5']
         ig.pod_spec_override = json.dumps(pod_spec)
 
         return ig, client, problem
@@ -59,8 +71,7 @@ class TestContainerGroups(APITest):
     def test_failed_job(self, bad_container_group_and_client, factories):
         container_group, client, problem = bad_container_group_and_client
         jt = factories.job_template()
-        with pytest.raises(exc.NoContent):
-            jt.related.instance_groups.post(dict(id=container_group.id, associate=True))
+        jt.add_instance_group(container_group)
         job = jt.launch().wait_until_completed()
         if problem == 'cacrt':
             assert 'OpenSSL.SSL.Error' in job.result_traceback
@@ -69,19 +80,26 @@ class TestContainerGroups(APITest):
         elif problem == 'host':
             assert 'Name or service not known' in job.result_traceback
         elif problem == 'role':
-            pytest.skip("Unsure what is supposed to happen here")
+            pytest.skip("Need to fixup tower-qe cluster not to use legacy auth to test this.")
         elif problem == 'image':
-            pytest.skip("Waiting for planned changes to be pushed")
+            assert 'rsync error' in job.result_traceback
+        elif problem == 'entry_point':
+            # need to do more research to see what is supposed to happen
+            pass
         job.assert_status('error')
+        assert job.instance_group == container_group.id, "Container group is not indicated that the job tried to run on"
 
     def test_launch_job(self, container_group_and_client, factories):
         container_group, client = container_group_and_client
-        jt = factories.job_template()
+        inventory = factories.inventory()
+        host = inventory.add_host()
+        jt = factories.job_template(inventory=inventory)
         jt.add_instance_group(container_group)
         # TODO use sleep playbook and confirm a pod spins up on gke using client
         job = jt.launch().wait_until_completed()
         job.assert_successful()
         assert job.instance_group == container_group.id
+        assert host.name in job.result_stdout
 
     def test_launch_project_update(self, container_group_and_client, factories):
         container_group, client = container_group_and_client
@@ -104,6 +122,8 @@ class TestContainerGroups(APITest):
         adhoc.wait_until_completed(timeout=90)
         adhoc.assert_successful()
         adhoc.summary_fields.instance_group.id == container_group.id
+        assert host.name in adhoc.result_stdout
+
 
     def test_launch_cloud_inventory_update(self, container_group_and_client, aws_inventory_source):
         container_group, client = container_group_and_client
