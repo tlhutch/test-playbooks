e93551f8dd8d700cf87468021c60fc55a2587991
Daniel Sami <dsami@redhat.com>
2019-10-23 17:05:38 -0400

    awx-pf pipeline (#4257)
    * awx-pf pipeline added

---
+++ tools/jenkins/pipelines/awx-pf-e2e.groovy
@@ -0,0 +1,172 @@
+pipeline {
+    agent {
+
+            label 'jenkins-jnlp-agent'
+    }
+    parameters {
+        string(
+            name: 'AWX_E2E_URL',
+            description: 'dont include https:// or trailing slashes',
+            defaultValue: 'ec2-100-24-21-207.compute-1.amazonaws.com'
+        )
+        string(
+            name: 'AWX_E2E_USERNAME',
+            description: '',
+            defaultValue: 'admin'
+        )
+        password(
+            name: 'AWX_E2E_PASSWORD',
+            description: 'Ignore to use default password',
+        )
+        string(
+            name: 'UI_REPO',
+            description: 'Points to the repo that contains the awx-pf UI code. Use git protocol format.',
+            defaultValue: 'git@github.com:ansible/awx.git'
+        )
+        string(
+            name: 'UI_BRANCH',
+            description: 'Default is devel',
+            defaultValue: 'devel'
+        )
+        string(
+            name: 'E2E_TEST_REPO',
+            description: 'Points to the tower-qa repo that contains the tests',
+            defaultValue: 'git@github.com:ansible/tower-qa.git'
+        )
+        string(
+            name: 'E2E_TEST_BRANCH',
+            description: 'Default is devel',
+            defaultValue: 'devel'
+        )
+        string(
+            name: 'E2E_TEST_SELECTION',
+            description: 'Run the tests selected by this pattern',
+            defaultValue: '*'
+        )
+    }
+    options {
+        timeout(time: 2, unit: 'HOURS')
+        timestamps()
+        ansiColor('xterm')
+        parallelsAlwaysFailFast()
+    }
+    stages {
+        stage ('Clone Git repo') {
+            steps {
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [
+                        [ name: "${E2E_TEST_BRANCH}" ]
+                    ],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [[
+                        $class: 'RelativeTargetDirectory', 
+                        relativeTargetDir: 'tower-qa'
+                    ]], 
+                    submoduleCfg: [],
+                    userRemoteConfigs: [
+                        [
+                            credentialsId: 'github-ansible-jenkins-nopassphrase',
+                            url: '${E2E_TEST_REPO}'
+                        ]
+                    ]
+                ])
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [
+                        [ name: "${UI_BRANCH}" ]
+                    ],
+                    doGenerateSubmoduleConfigurations: false,
+                    extensions: [[
+                        $class: 'RelativeTargetDirectory', 
+                        relativeTargetDir: 'awx'
+                    ]], 
+                    submoduleCfg: [],
+                    userRemoteConfigs: [
+                        [
+                            credentialsId: 'github-ansible-jenkins-nopassphrase',
+                            url: '${UI_REPO}'
+                        ]
+                    ]
+                ])
+            }
+        }
+        stage ('Apply license') {
+            steps {
+                withCredentials([file(credentialsId: '86ed99e9-dad9-49e9-b0db-9257fb563bad', variable: 'JSON_KEY_FILE_PATH')]) {
+                sshagent(['github-ansible-jenkins-nopassphrase']) {
+                    sh '''#!/bin/bash -e
+                    set +x
+                    python "tower-qa/tools/jenkins/scripts/apply_license_py2.py" -u "${AWX_E2E_USERNAME}" -p "${AWX_E2E_PASSWORD}" https://"${AWX_E2E_URL}"
+                    set -x
+                    '''
+                }}
+            }
+        }
+        stage ('Allow remote UI connections') {
+            steps {
+                sshagent(credentials : ['github-ansible-jenkins-nopassphrase']) {
+                sh '''#!/bin/sh
+                     
+                     # Ansible ad-hoc command to modify /etc/tower/settings.py to allow remote UI calls.
+                     ansible all -i "${AWX_E2E_URL}," -m lineinfile -a 'path=/etc/tower/settings.py line=\"CSRF_TRUSTED_ORIGINS=['\\''{{ lookup("env", "AWX_E2E_URL") }}:443'\\'']\" state=present' --ssh-extra-args='-o "StrictHostKeyChecking no"' --become --become-user=root -u 'ec2-user'
+                     # Ansible ad-hoc command to restart tower and refresh settings.py
+                     ansible all -i "${AWX_E2E_URL}," -m shell -a 'ansible-tower-service restart' --become --become-user=root -u 'ec2-user'
+                     '''
+                }
+           }
+        }
+        stage('Start awx-pf locally') {
+            steps {
+                withCredentials([file(credentialsId: '86ed99e9-dad9-49e9-b0db-9257fb563bad', variable: 'JSON_KEY_FILE_PATH')]) {
+                    sshagent(['github-ansible-jenkins-nopassphrase']) {
+                        sh '''#!/bin/bash -xe
+                        #set remote target                    
+                        export TARGET_HOST="${AWX_E2E_URL}"
+                        export TARGET_PORT='443'
+
+                        set +x
+                        docker login -u _json_key -p "$(cat "${JSON_KEY_FILE_PATH}")" https://gcr.io
+                        set -x
+
+                        cd awx/awx/ui_next
+                        docker build -t awx-ui-next .
+                        docker run --name 'ui-next' --network='default' -e TARGET_PORT='443' -e TARGET_HOST="${AWX_E2E_URL}" -p '3001:3001' -d -v $(pwd)/src:/ui_next/src awx-ui-next
+                        sleep 10
+
+                        cd ../../../tower-qa/ui-tests/awx-pf-tests
+                        docker build -t awx-pf-tests .
+                        docker run -e CYPRESS_AWX_E2E_USERNAME="${AWX_E2E_USERNAME}" -e CYPRESS_AWX_E2E_PASSWORD="${AWX_E2E_PASSWORD}" --network 'default' --link 'ui-next:ui-next' -v $PWD:/e2e -w /e2e awx-pf-tests run --project .
+                        
+                        '''
+                    }
+                }
+            }
+          
+        }  
+    }
+    post {
+        always {
+            archiveArtifacts 'tower-qa/ui-tests/awx-pf-tests/cypress-report.xml, tower-qa/ui-tests/awx-pf-tests/cypress/screenshots/*.xml'
+            junit 'tower-qa/ui-tests/awx-pf-tests/cypress-report.xml'
+
+        }
+        success {
+            slackSend(
+                color: "good",
+                teamDomain: "ansible",
+                channel: "#e2e-test-resuts",
+                message: "patternfly devel is <${env.RUN_DISPLAY_URL}|passing>"
+            )
+        }
+        failure {
+            slackSend(
+                color: "bad",
+                teamDomain: "ansible",
+                channel: "#e2e-test-resuts",
+                message: "patternfly devel is <${env.RUN_DISPLAY_URL}|not passing>"
+            )
+        }
+    }
+}
+
