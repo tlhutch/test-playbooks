1cb4b324969748b8cc81445a879e772ce4c70caa
Caleb Boylan <calebboylan@gmail.com>
2019-11-19 12:25:31 -0800

    Only sed the config/credentials-pkcs8.yml if it exists

--- tools/jenkins/pipelines/yolo.groovy
+++ tools/jenkins/pipelines/yolo.groovy
@@ -405,7 +405,7 @@ pipeline {
 
                                     // Update the credentials files before deploying the test runner so that the credentials files are copied with the random generated admin password in place
                                     sh 'sed -i "s/default: &id001 {password: fo0m4nchU,/default: \\&id001 {password: $(cat artifacts/admin_password),/" config/credentials.yml'
-                                    sh 'sed -i "s/default: &id001 {password: fo0m4nchU,/default: \\&id001 {password: $(cat artifacts/admin_password),/" config/credentials-pkcs8.yml'
+                                    sh 'if [ -e config/credentials-pkcs8.yml ]; then sed -i "s/default: &id001 {password: fo0m4nchU,/default: \\&id001 {password: $(cat artifacts/admin_password),/" config/credentials-pkcs8.yml; fi'
 
                                     sh 'ansible-playbook -v -i playbooks/inventory -e @playbooks/test_runner_vars.yml playbooks/deploy-test-runner.yml'
 
