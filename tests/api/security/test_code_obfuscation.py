68876eb9fb8674c759e0e038b3b2ea996f45ff5d
Elijah DeLee <kdelee@redhat.com>
2019-11-19 13:19:09 -0500

    Improve error message for code obfuscation test
    Previously, it was difficult to know what happened if we did not get expected exit code
    Now we get more details to allow us to know the nature of the change that caused the bug to fail.
    
    Also update to use more reliable version of ansible_runner that is easier to control what hosts it runs against.

--- tests/api/security/test_code_obfuscation.py
+++ tests/api/security/test_code_obfuscation.py
@@ -1,3 +1,5 @@
+import pprint
+
 import pytest
 
 from tests.api import APITest
@@ -6,9 +8,9 @@ from tests.api import APITest
 @pytest.mark.usefixtures('skip_if_openshift', 'authtoken')
 class TestTowerLicense(APITest):
 
-    def test_tower_license_source_is_hidden(self, ansible_runner):
-        result = ansible_runner.shell(
+    def test_tower_license_source_is_hidden(self, modified_ansible_adhoc):
+        result = modified_ansible_adhoc().tower.shell(
             """echo "import inspect, tower_license; print(inspect.getsourcelines(tower_license.TowerLicense))" | awx-manage shell"""  # noqa
         ).values()[0]
-        assert result['rc'] == 1
+        assert result['rc'] == 1, pprint.pformat(result)
         assert 'could not get source code' in result['stderr']
