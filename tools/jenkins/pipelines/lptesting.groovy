a8c5987861742322ffc8870ec60e066658357b0a
Yanis Guenane <yanis@guenane.org>
2019-12-10 09:27:19 +0100

    LP: Checkout tower-qa really early on pipeline

--- tools/jenkins/pipelines/lptesting.groovy
+++ tools/jenkins/pipelines/lptesting.groovy
@@ -47,6 +47,24 @@ Tower Version: ${_TOWER_VERSION}"""
             }
         }
 
+        stage('Checkout tower-qa') {
+            steps {
+                script {
+                    branch_name = _TOWER_VERSION == 'devel' ? 'devel' : "release_${_TOWER_VERSION}"
+                }
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: "*/${branch_name}" ]],
+                    userRemoteConfigs: [
+                        [
+                            credentialsId: 'github-ansible-jenkins-nopassphrase',
+                            url: 'git@github.com:ansible/tower-qa.git'
+                        ]
+                    ]
+                ])
+            }
+        }
+
         stage ('Setup Platform - Prereq') {
             steps {
                 withCredentials([file(credentialsId: '171764d8-e57c-4332-bff8-453670d0d99f', variable: 'PUBLIC_KEY'),
@@ -105,24 +123,6 @@ Tower Version: ${_TOWER_VERSION}"""
                         sh 'ansible-playbook -i playbooks/inventory.lptesting playbooks/lptesting.yml -e @playbooks/vars.yml --tags install'
                     }
                 }
-            }
-        }
-
-        stage('Checkout tower-qa') {
-            steps {
-                script {
-                    branch_name = _TOWER_VERSION == 'devel' ? 'devel' : "release_${_TOWER_VERSION}"
-                }
-                checkout([
-                    $class: 'GitSCM',
-                    branches: [[name: "*/${branch_name}" ]],
-                    userRemoteConfigs: [
-                        [
-                            credentialsId: 'github-ansible-jenkins-nopassphrase',
-                            url: 'git@github.com:ansible/tower-qa.git'
-                        ]
-                    ]
-                ])
             }
         }
 
