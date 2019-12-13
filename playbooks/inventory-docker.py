8fc58597aace9ca14686a8b12447c68c5d4db04b
chris meyers <chris.meyers.fsu@gmail.com>
2019-12-13 13:51:28 -0500

    dynamically find the "right" docker container

---
+++ playbooks/inventory-docker.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import subprocess
+import json
+
+p = subprocess.run("docker ps | egrep -o 'tools_awx(_run)?_([^ ]+)?'", capture_output=True, shell=True)
+docker_host = p.stdout.decode('utf-8').rstrip()
+
+out = {
+    'local': {
+        'hosts': ['127.0.0.1'],
+        'vars': {
+            'ansible_connection': 'local',
+            'ansible_python_interpreter': '/usr/bin/env python',
+        },
+    },
+    'tower': {
+        'hosts': [docker_host],
+        'vars': {
+            'ansible_connection': 'docker',
+            'ansible_user': 'awx'
+        }
+    }
+}
+print(json.dumps(out))
+
