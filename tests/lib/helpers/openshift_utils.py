87173c07262ce74afd0a0cdd9b70c625d96dfffc
Elijah DeLee <kdelee@redhat.com>
2019-12-13 16:03:11 -0500

    Fix openshift helpers to conform to NWO
    In the new world order, openshift uses a different login url and uses a dynamically created project.
    The pipelines lay this info down in artifacts/openshift_installer_vars.yml
    
    This PR updates the utils to use:
    First, any environment variables passed to allow humans to do what they want
    Two, to use any vars laid down in the openshift_installer_vars.yml since our automation does that always.
    Three, if nothing else use the credentials.yml.
    
    Additionally, if the token is set, we prefer that.
    I have an additional PR up to set token in environment, https://github.com/ansible/tower-qa/pull/4481/files

--- tests/lib/helpers/openshift_utils.py
+++ tests/lib/helpers/openshift_utils.py
@@ -1,23 +1,66 @@
+import os
 import subprocess
+import yaml
 
 from openshift.helper.openshift import OpenShiftObjectHelper
 from awxkit.config import config
 
 
+def get_openshift_credentials():
+    creds = {'username': '', 'password': '', 'host': '', 'project': '', 'token': ''}
+    installer_vars = None
+    vars_path = 'artifacts/openshift_installer_vars.yml'
+    if os.path.exists(vars_path):
+        with open(vars_path) as f:
+            installer_vars = yaml.safe_load(f)
+        creds['username'] = installer_vars.get('openshift_user')
+        creds['host'] = installer_vars.get('openshift_host')
+        creds['project'] = installer_vars.get('openshift_project')
+        creds['token'] = installer_vars.get('openshift_token')
+        creds['password'] = installer_vars.get('openshift_password')
+
+    # Always override with environment variables if possible
+    creds['username'] = os.getenv('OPENSHIFT_USER') if os.getenv('OPENSHIFT_USER', False) else creds['username']
+    creds['token'] = os.getenv('OPENSHIFT_TOKEN') if os.getenv('OPENSHIFT_TOKEN', False) else creds['token']
+    creds['password'] = os.getenv('OPENSHIFT_PASSWORD') if os.getenv('OPENSHIFT_PASSWORD', False) else creds['password']
+    creds['host'] = os.getenv('OPENSHIFT_HOST') if os.getenv('OPENSHIFT_HOST', False) else creds['host']
+    creds['project'] = os.getenv('OPENSHIFT_PROJECT') if os.getenv('OPENSHIFT_PROJECT', False) else creds['project']
+
+    # Only if nothing else has been provided use credentials file
+    if not creds['username']:
+        creds['username'] = config.credentials.openshift.username
+    # Only use password if token and pasword is not set from other source
+    if not creds['token'] and not creds['password']:
+        creds['password'] = config.credentials.openshift.password
+    if not creds['host']:
+        creds['host'] = config.credentials.openshift.host
+    if not creds['project']:
+        creds['project'] = config.credentials.openshift.project
+
+    return creds
+
+
 def prep_environment():
-    ret = subprocess.call('oc login -u {0} -p {1} {2} --insecure-skip-tls-verify'.format(
-        config.credentials.openshift.username,
-        config.credentials.openshift.password,
-        config.credentials.openshift.url), shell=True)
+    creds = get_openshift_credentials()
+    if creds['token']:
+        ret = subprocess.call('oc login --token {0} {1} --insecure-skip-tls-verify'.format(
+            creds['token'],
+            creds['host']), shell=True)
+    else:
+        ret = subprocess.call('oc login -u {0} -p {1} {2} --insecure-skip-tls-verify'.format(
+            creds['username'],
+            creds['password'],
+            creds['host']), shell=True)
     assert ret == 0
 
-    ret = subprocess.call('oc project {}'.format(config.openshift_namespace), shell=True)
+    ret = subprocess.call('oc project {}'.format(creds['project']), shell=True)
     assert ret == 0
 
 
 def get_pods():
+    namespace = get_openshift_credentials()['project']
     client = OpenShiftObjectHelper(api_version='v1', kind='pod_list')
-    ret = client.get_object(namespace=config.openshift_namespace)
+    ret = client.get_object(namespace=namespace)
     return [i.metadata.name for i in ret.items]
 
 
