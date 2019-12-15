12bd19bc138f69adfd661440441459a9c4f3a38a
Caleb Boylan <cboylan@redhat.com>
2019-12-14 19:41:05 -0800

    Move collection tests around

--- tests/lib/fixtures/api/collections.py
+++ tests/lib/fixtures/api/collections.py
@@ -42,13 +42,6 @@ def tower_modules_collection(request, modified_ansible_adhoc, ansible_collection
     # filesystem that is receiving the API call. On the other hand, for
     # standalone deployments the target group should be `tower`
 
-    # Default to installing awx collection from ansible awx repo
-    fork = os.environ.get('TOWER_FORK', 'ansible')
-    product = os.environ.get('PRODUCT', 'awx')
-    branch = os.environ.get('TOWER_BRANCH', 'devel')
-    if product == 'tower':
-        pytest.skip("Current approach does not work because we don't have creds on tower box, skip because otherwise git command will hang.")
-
     ansible_path = ''
     # If we set the venv_group in fixture_args, override the defaults
     fixture_args = request.node.get_closest_marker('fixture_args')
@@ -73,64 +66,85 @@ def tower_modules_collection(request, modified_ansible_adhoc, ansible_collection
             _run_uninstall_tower_modules_collection, ansible_adhoc, ansible_collections_path)
         request.addfinalizer(uninstall_tower_modules_collection)
 
-    try:
-        contacted = ansible_adhoc.file(
-            dest=ansible_collections_path,
-            recurse=True,
-            state='directory',
-        )
-        for result in contacted.values():
-            assert result.get('state', None) == 'directory', result_to_str(result)
-
-        build_dir = os.path.join(ansible_collections_path, 'build')
-
-        git_result = ansible_adhoc.git(
-            repo=f'https://github.com/{fork}/{product}.git',
-            version=f'{branch}',
-            depth=1,
-            dest=build_dir,
-            force=True
-        )
-        for result in git_result.values():
-            assert (result.get('before') or result.get('after')), result_to_str(result)
-
-        # TODO: optionally build as awx or tower
-        contacted = ansible_adhoc.shell((
-            '{} -i localhost, awx_collection/template_galaxy.yml '
-            '-e collection_package=awx -e collection_namespace=awx -e collection_version=0.0.1'.format(ansible_playbook_bin)
-        ), chdir=build_dir)
-        for result in contacted.values():
-            assert result.get('rc', None) == 0, result_to_str(result)
-
-        contacted = ansible_adhoc.shell(
-            '{} collection build --force'.format(
-                ansible_galaxy_bin
-            ),
-            chdir=os.path.join(build_dir, 'awx_collection')
-        )
-        for result in contacted.values():
-            assert result.get('rc', None) == 0, result_to_str(result)
-            stdout = result['stdout']
-            assert 'Created collection' in stdout
-            package_name = re.search(r'\b([a-z0-9\.-]*\.tar\.gz)\b', stdout).group(1)
-
-        contacted = ansible_adhoc.shell(
-            '{} collection install {} -p {} --force'.format(
-                ansible_galaxy_bin,
-                os.path.join(build_dir, 'awx_collection', package_name), ansible_collections_path
+    if limit == 'local':
+        try:
+            contacted = ansible_adhoc.file(
+                dest=ansible_collections_path,
+                recurse=True,
+                state='directory',
             )
-        )
-        for result in contacted.values():
-            assert result.get('rc', None) == 0, result_to_str(result)
-        contacted = ansible_adhoc.file(
-            dest=ansible_collections_path,
-            mode=stat.S_IRUSR | stat.S_IXUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH,
-            recurse=True,
-            state='directory',
-        )
-        for result in contacted.values():
-            assert result.get('mode', None) == '0755', result_to_str(result)
-    except AssertionError as e:
-        pytest.fail(
-            'Failed to install awx.awx\n{}'.format(e)
-        )
+            for result in contacted.values():
+                assert result.get('state', None) == 'directory', result_to_str(result)
+
+            build_dir = os.path.join(ansible_collections_path, 'build')
+
+            git_result = ansible_adhoc.git(
+                repo=f'https://github.com/{fork}/{product}.git',
+                version=f'{branch}',
+                depth=1,
+                dest=build_dir,
+                force=True
+            )
+            for result in git_result.values():
+                assert (result.get('before') or result.get('after')), result_to_str(result)
+
+            # TODO: optionally build as awx or tower
+            contacted = ansible_adhoc.shell((
+                '{} -i localhost, awx_collection/template_galaxy.yml '
+                '-e collection_package=awx -e collection_namespace=awx -e collection_version=0.0.1'.format(ansible_playbook_bin)
+            ), chdir=build_dir)
+            for result in contacted.values():
+                assert result.get('rc', None) == 0, result_to_str(result)
+
+            contacted = ansible_adhoc.shell(
+                '{} collection build --force'.format(
+                    ansible_galaxy_bin
+                ),
+                chdir=os.path.join(build_dir, 'awx_collection')
+            )
+            for result in contacted.values():
+                assert result.get('rc', None) == 0, result_to_str(result)
+                stdout = result['stdout']
+                assert 'Created collection' in stdout
+                package_name = re.search(r'\b([a-z0-9\.-]*\.tar\.gz)\b', stdout).group(1)
+
+            contacted = ansible_adhoc.shell(
+                '{} collection install {} -p {} --force'.format(
+                    ansible_galaxy_bin,
+                    os.path.join(build_dir, 'awx_collection', package_name), ansible_collections_path
+                )
+            )
+            for result in contacted.values():
+                assert result.get('rc', None) == 0, result_to_str(result)
+            contacted = ansible_adhoc.file(
+                dest=ansible_collections_path,
+                mode=stat.S_IRUSR | stat.S_IXUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IXGRP | stat.S_IROTH | stat.S_IXOTH,
+                recurse=True,
+                state='directory',
+            )
+            for result in contacted.values():
+                assert result.get('mode', None) == '0755', result_to_str(result)
+        except AssertionError as e:
+            pytest.fail(
+                'Failed to install awx.awx\n{}'.format(e)
+            )
+    else:
+        try:
+            contacted = ansible_adhoc.file(
+                dest=ansible_collections_path,
+                recurse=True,
+                state='directory',
+            )
+            for result in contacted.values():
+                assert result.get('state', None) == 'directory', result_to_str(result)
+
+            contacted = ansible_adhoc.shell(
+                f'{ansible_galaxy_bin} collection install {aw_repo_url}/collection/ansible-tower-latest.tar.gz'
+            )
+            for result in contacted.values():
+                assert result.get('rc', None) == 0, result_to_str(result)
+
+        except AssertionError as e:
+            pytest.fail(
+                'Failed to install awx.awx\n{}'.format(e)
+            )
