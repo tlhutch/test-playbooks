91392f0134aaa5aef80194c1e31b0cdeb9dc31cf
Yanis Guenane <yanis@guenane.org>
2019-12-20 09:29:25 +0100

    Yolo: RHEL 8.2 is coming

--- tools/jenkins/pipelines/yolo.groovy
+++ tools/jenkins/pipelines/yolo.groovy
@@ -199,10 +199,7 @@ pipeline {
                         NIGHTLY_REPO_DIR = "tower_${params.TOWER_BRANCH}_packaging_${params.TOWER_PACKAGING_BRANCH}"
                     }
 
-                    // TODO: (Calebb) Make this a regex so with new versions of
-                    // RHEL 8 we dont have to continue updating this if
-                    // statement
-                    if (params.PLATFORM == 'rhel-8.0-x86_64' || params.PLATFORM == 'rhel-8.1-x86_64') {
+                    if (params.PLATFORM.contains('rhel-8')) {
                         target_dist = 'epel-8-x86_64'
                         mock_cfg = 'rhel-8-x86_64'
                     } else {
